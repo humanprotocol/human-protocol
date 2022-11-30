e4a64f5e8bf39e6e5e6499594dd0ab19b4048901
Eric Lee <eric@hmt.ai>
2022-11-30 12:17:57 -0500

    fix test script for sdk

--- packages/sdk/typescript/human-protocol-sdk/scripts/run-test.sh
+++ packages/sdk/typescript/human-protocol-sdk/scripts/run-test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -eux
 
+# Run hardhat node, and deploy contract locally
 yarn workspace @human-protocol/core local &
 
+# Wait for the contracts to be deployed properly
 sleep 5
 
+# Run test
 npx jest
 
+# Kill running hardhat node
 trap 'kill $(jobs -p)' EXIT
