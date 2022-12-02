02557d02357aa896a1d6d51b5533e9e9da5ad8dc
portuu3 <adrian.portugues.mas@gmail.com>
2022-12-02 10:45:47 +0100

    update fortune tests for staking and slashing

--- packages/examples/fortune/recording-oracle/src/services/fortune.test.ts
+++ packages/examples/fortune/recording-oracle/src/services/fortune.test.ts
@@ -11,12 +11,14 @@ import { addFortune } from './fortune';
 import Escrow from '@human-protocol/core/artifacts/contracts/Escrow.sol/Escrow.json';
 import HMToken from '@human-protocol/core/artifacts/contracts/HMToken.sol/HMToken.json';
 import EscrowFactory from '@human-protocol/core/artifacts/contracts/EscrowFactory.sol/EscrowFactory.json';
+import Staking from '@human-protocol/core/artifacts/contracts/Staking.sol/Staking.json';
 import { bulkPayout } from './reputationClient';
 import { getManifest } from './manifest';
 import { Contract } from 'web3-eth-contract';
 import { getEscrow, getFortunes, getWorkerResult } from './storage';
 
 let token: Contract;
+let staking: Contract;
 let escrowFactory: Contract;
 let escrowAddress: string;
 let escrow: Contract;
@@ -24,8 +26,11 @@ let escrow: Contract;
 const worker1 = '0x90F79bf6EB2c4f870365E785982E1f101E93b906';
 const worker2 = '0xcd3B766CCDd6AE721141F452C550Ca635964ce71';
 const web3 = new Web3('http://127.0.0.1:8547');
-const account = web3.eth.accounts.privateKeyToAccount(
+const owner = web3.eth.accounts.privateKeyToAccount(
   '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+);
+const launcher = web3.eth.accounts.privateKeyToAccount(
+  '0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6'
 );
 const recordingAccount = web3.eth.accounts.privateKeyToAccount(
   '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'
@@ -48,9 +53,8 @@ describe('Fortune', () => {
         ],
       })
       .send({
-        from: account.address,
+        from: owner.address,
       });
-
     const escrowFactoryContract = new web3.eth.Contract(
       EscrowFactory.abi as []
     );
@@ -60,21 +64,56 @@ describe('Fortune', () => {
         arguments: [token.options.address],
       })
       .send({
-        from: account.address,
+        from: owner.address,
       });
+
+    const stakingContract = new web3.eth.Contract(Staking.abi as []);
+    staking = await stakingContract
+      .deploy({
+        data: Staking.bytecode,
+        arguments: [
+          token.options.address,
+          escrowFactory.options.address,
+          web3.utils.toWei('1', 'ether'),
+          1,
+        ],
+      })
+      .send({
+        from: owner.address,
+      });
+
+    await escrowFactory.methods
+      .setStaking(staking.options.address)
+      .send({ from: owner.address });
+
+    await token.methods
+      .transfer(launcher.address, web3.utils.toWei('1000', 'ether'))
+      .send({ from: owner.address });
+
+    await token.methods
+      .approve(staking.options.address, web3.utils.toWei('500', 'ether'))
+      .send({ from: launcher.address });
+
+    await staking.methods
+      .setStaker(launcher.address, 1)
+      .send({ from: owner.address });
+
+    await staking.methods
+      .stake(web3.utils.toWei('500', 'ether'))
+      .send({ from: launcher.address });
   });
 
   beforeEach(async () => {
     await escrowFactory.methods
-      .createEscrow([account.address])
-      .send({ from: account.address });
+      .createEscrow([launcher.address])
+      .send({ from: launcher.address });
 
     escrowAddress = await escrowFactory.methods.lastEscrow().call();
 
     const value = web3.utils.toWei('10', 'ether');
     await token.methods
       .transfer(escrowAddress, value)
-      .send({ from: account.address });
+      .send({ from: launcher.address });
 
     escrow = new web3.eth.Contract(Escrow.abi as [], escrowAddress);
     await escrow.methods
@@ -86,7 +125,7 @@ describe('Fortune', () => {
         'manifestUrl',
         'manifestUrl'
       )
-      .send({ from: account.address });
+      .send({ from: launcher.address });
 
     jest.mocked(getManifest).mockResolvedValue({
       fortunes_requested: 2,
@@ -141,7 +180,7 @@ describe('Fortune', () => {
   });
 
   it('Invalid recording oracle address', async () => {
-    web3.eth.defaultAccount = account.address;
+    web3.eth.defaultAccount = owner.address;
     const err: any = await addFortune(
       web3,
       worker1,
@@ -157,7 +196,7 @@ describe('Fortune', () => {
 
   it('Escrow not pending', async () => {
     await escrow.methods.cancel().send({
-      from: account.address,
+      from: launcher.address,
     });
     const err: any = await addFortune(
       web3,
--- packages/examples/fortune/reputation-oracle/src/services/escrow.test.ts
+++ packages/examples/fortune/reputation-oracle/src/services/escrow.test.ts
@@ -4,20 +4,25 @@ import { describe, expect, it, beforeAll, beforeEach } from '@jest/globals';
 import Escrow from '@human-protocol/core/artifacts/contracts/Escrow.sol/Escrow.json';
 import HMToken from '@human-protocol/core/artifacts/contracts/HMToken.sol//HMToken.json';
 import EscrowFactory from '@human-protocol/core/artifacts/contracts/EscrowFactory.sol/EscrowFactory.json';
+import Staking from '@human-protocol/core/artifacts/contracts/Staking.sol/Staking.json';
 import { Contract } from 'web3-eth-contract';
 
 let token: Contract;
+let staking: Contract;
 let escrowFactory: Contract;
 let escrowAddress: string;
 let escrow: Contract;
 
-const worker1 = '0x90F79bf6EB2c4f870365E785982E1f101E93b906';
+const worker1 = '0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f';
 const worker2 = '0xcd3B766CCDd6AE721141F452C550Ca635964ce71';
 const worker3 = '0x146D35a6485DbAFF357fB48B3BbA31fCF9E9c787';
 
 const web3 = new Web3('http://127.0.0.1:8548');
-const account = web3.eth.accounts.privateKeyToAccount(
+const owner = web3.eth.accounts.privateKeyToAccount(
   '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+);
+const launcher = web3.eth.accounts.privateKeyToAccount(
+  '0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6'
 );
 const recordingAccount = web3.eth.accounts.privateKeyToAccount(
   '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'
@@ -38,7 +43,7 @@ describe('Fortune', () => {
         ],
       })
       .send({
-        from: account.address,
+        from: owner.address,
       });
 
     const escrowFactoryContract = new web3.eth.Contract(
@@ -50,21 +55,55 @@ describe('Fortune', () => {
         arguments: [token.options.address],
       })
       .send({
-        from: account.address,
+        from: owner.address,
       });
+      const stakingContract = new web3.eth.Contract(Staking.abi as []);
+      staking = await stakingContract
+        .deploy({
+          data: Staking.bytecode,
+          arguments: [
+            token.options.address,
+            escrowFactory.options.address,
+            web3.utils.toWei('1', 'ether'),
+            1,
+          ],
+        })
+        .send({
+          from: owner.address,
+        });
+  
+      await escrowFactory.methods
+        .setStaking(staking.options.address)
+        .send({ from: owner.address });
+  
+      await token.methods
+        .transfer(launcher.address, web3.utils.toWei('1000', 'ether'))
+        .send({ from: owner.address });
+  
+      await token.methods
+        .approve(staking.options.address, web3.utils.toWei('500', 'ether'))
+        .send({ from: launcher.address });
+  
+      await staking.methods
+        .setStaker(launcher.address, 1)
+        .send({ from: owner.address });
+  
+      await staking.methods
+        .stake(web3.utils.toWei('500', 'ether'))
+        .send({ from: launcher.address });
   });
 
   beforeEach(async () => {
     await escrowFactory.methods
-      .createEscrow([account.address])
-      .send({ from: account.address });
+      .createEscrow([launcher.address])
+      .send({ from: launcher.address });
 
     escrowAddress = await escrowFactory.methods.lastEscrow().call();
 
     const value = web3.utils.toWei('30', 'ether');
     await token.methods
       .transfer(escrowAddress, value)
-      .send({ from: account.address });
+      .send({ from: launcher.address });
 
     escrow = new web3.eth.Contract(Escrow.abi as [], escrowAddress);
     await escrow.methods
@@ -76,7 +115,7 @@ describe('Fortune', () => {
         'manifestUrl',
         'manifestUrl'
       )
-      .send({ from: account.address });
+      .send({ from: launcher.address });
   });
 
   it('Get escrow balance', async () => {
@@ -103,10 +142,10 @@ describe('Fortune', () => {
     expect(await token.methods.balanceOf(worker1).call()).toBe(
       web3.utils.toWei('0', 'ether')
     );
-    expect(await token.methods.balanceOf(worker1).call()).toBe(
+    expect(await token.methods.balanceOf(worker2).call()).toBe(
       web3.utils.toWei('0', 'ether')
     );
-    expect(await token.methods.balanceOf(worker1).call()).toBe(
+    expect(await token.methods.balanceOf(worker3).call()).toBe(
       web3.utils.toWei('0', 'ether')
     );
   }, 10000);
@@ -130,10 +169,10 @@ describe('Fortune', () => {
     expect(await token.methods.balanceOf(worker1).call()).toBe(
       web3.utils.toWei('8', 'ether')
     );
-    expect(await token.methods.balanceOf(worker1).call()).toBe(
+    expect(await token.methods.balanceOf(worker2).call()).toBe(
       web3.utils.toWei('8', 'ether')
     );
-    expect(await token.methods.balanceOf(worker1).call()).toBe(
+    expect(await token.methods.balanceOf(worker3).call()).toBe(
       web3.utils.toWei('8', 'ether')
     );
   }, 10000);
