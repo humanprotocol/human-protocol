66a4b1bd8694821376fc3ba559ab17c38bbd4ed0
Eric Lee <eric@hmt.ai>
2022-12-12 17:25:03 -0500

    fix deploy script

--- packages/core/scripts/deploy.ts
+++ packages/core/scripts/deploy.ts
@@ -2,7 +2,7 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  const [owner, recOracle, repOrcale, launcher] = await ethers.getSigners();
+  const [, , , launcher] = await ethers.getSigners();
   const HMToken = await ethers.getContractFactory('HMToken');
   const HMTokenContract = await HMToken.deploy(
     1000000000,
@@ -13,9 +13,15 @@ async function main() {
   await HMTokenContract.deployed();
   console.log('HMToken Address: ', HMTokenContract.address);
 
+  const Staking = await ethers.getContractFactory('Staking');
+  const stakingContract = await Staking.deploy(HMTokenContract.address, 1, 1);
+  await stakingContract.deployed();
+  console.log('Staking Contract Address:', stakingContract.address);
+
   const EscrowFactory = await ethers.getContractFactory('EscrowFactory');
   const escrowFactoryContract = await EscrowFactory.deploy(
-    HMTokenContract.address
+    HMTokenContract.address,
+    stakingContract.address
   );
   await escrowFactoryContract.deployed();
 
@@ -27,16 +33,6 @@ async function main() {
 
   console.log('KVStore Address: ', kvStoreContract.address);
 
-  const Staking = await ethers.getContractFactory('Staking');
-  const stakingContract = await Staking.deploy(
-    HMTokenContract.address,
-    escrowFactoryContract.address,
-    1,
-    1
-  );
-  await stakingContract.deployed();
-  console.log('Staking Contract Address:', stakingContract.address);
-
   const RewardPool = await ethers.getContractFactory('RewardPool');
   const rewardPoolContract = await RewardPool.deploy(
     HMTokenContract.address,
@@ -45,9 +41,6 @@ async function main() {
   );
   await rewardPoolContract.deployed();
   console.log('Reward Pool Contract Address:', rewardPoolContract.address);
-
-  // Configure Staking in EscrowFactory
-  await escrowFactoryContract.setStaking(stakingContract.address);
 
   // Configure RewardPool in Staking
   await stakingContract.setRewardPool(rewardPoolContract.address);
