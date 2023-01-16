fcbda2608a8baecd2348183bdc0217c202ca1e91
Eugene Voronov <voronovege@cy-tech.fr>
2023-01-16 12:22:21 +0300

    Added get manifest method

--- packages/examples/fortune/recording-oracle/src/services/index.ts
+++ packages/examples/fortune/recording-oracle/src/services/index.ts
@@ -1,3 +1,15 @@
+import axios from 'axios';
+
+export async function getManifestByUrl(manifestUrl: string) {
+  const response = await axios.get(manifestUrl);
+
+  if (!response.data) {
+    throw new Error("Not Found")
+  }
+
+  return response.data;
+}
+
 export async function sendFortunes(fortunes: any) {
   return { response: true };
 }
