cbc444beba1d300db0ce2cc57e57e8bc8bdd966b
m00n620 <gex6611@gmail.com>
2023-01-19 08:47:24 -0500

    merge

--- packages/apps/escrow-dashboard/src/components/SolvedTasks/index.tsx
+++ packages/apps/escrow-dashboard/src/components/SolvedTasks/index.tsx
@@ -3,7 +3,7 @@ import numeral from 'numeral';
 import * as React from 'react';
 
 import tasksSvg from 'src/assets/tasks.svg';
-import ViewTitle from 'src/components/ViewTitle';
+import { ViewTitle } from 'src/components/ViewTitle';
 
 const SOLVED_TASKS = [
   { date: '2022-07', value: 2181348 },
