402bab2785f5390627961462f33077168e52ac89
portuu3 <adrian.portugues.mas@gmail.com>
2023-02-01 18:49:17 +0100

    vercel

--- packages/examples/fortune/launcher/server/api/serverless.ts
+++ packages/examples/fortune/launcher/server/api/serverless.ts
@@ -5,6 +5,11 @@ import * as dotenv from 'dotenv';
 import server from '../src/server.js';
 dotenv.config();
 
+server.listen({ port: 3000 }, (err) => {
+  if (err) console.error(err);
+  console.log('server listening on 3000');
+});
+
 export default async (req: any, res: any) => {
   await server.ready();
   server.server.emit('request', req, res);
