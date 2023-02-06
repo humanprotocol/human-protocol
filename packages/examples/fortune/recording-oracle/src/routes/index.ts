0e8fe575e3e1142ff27381719bdb0a2d6269c8dd
portuu3 <adrian.portugues.mas@gmail.com>
2023-02-06 13:02:57 +0100

    cors

--- packages/examples/fortune/recording-oracle/src/routes/index.ts
+++ packages/examples/fortune/recording-oracle/src/routes/index.ts
@@ -50,16 +50,7 @@ const routes: FastifyPluginAsync = async (server: FastifyInstance<Server>) => {
 
   server.post('/send-fortunes', opts, async function (request: FastifyRequest, reply) {
     const fortunes = request.body as IFortuneRequest;
-    reply.headers({
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, OPTIONS, DELETE'
-    });
-    if(request.method === 'OPTIONS') {
-      return reply.status(200);
-    }
-    return processFortunes(plugins, fortunes);
+    return await processFortunes(plugins, fortunes);
   });
 };
 
--- packages/examples/fortune/recording-oracle/src/server.ts
+++ packages/examples/fortune/recording-oracle/src/server.ts
@@ -32,12 +32,6 @@ const getServer = async () => {
     .register(curses)
     .register(uniqueness)
     .register(cors)
-    // .register(cors, {
-    //   origin: true,
-    //   methods: ['GET', 'PUT', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
-    //   allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
-    //   credentials: true      
-    // })
     .register(routes)
     .ready();
 
