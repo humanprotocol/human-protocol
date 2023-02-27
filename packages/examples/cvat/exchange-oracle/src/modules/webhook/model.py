2b2a1658923ce84507c73add24e1a52cf17a2133
Sergey Dzeranov <sergey.dzeranov@gmail.com>
2023-02-27 19:15:23 +0300

    Exchange Oracle for CVAT example app init commit

---
+++ packages/examples/cvat/exchange-oracle/src/modules/webhook/model.py
@@ -0,0 +1,26 @@
+# pylint: disable=too-few-public-methods
+from sqlalchemy import Column, String, DateTime, Enum
+from sqlalchemy_utils import URLType
+from sqlalchemy.sql import func
+from sqlalchemy.dialects import postgresql
+
+
+from .constants import Networks, WebhookTypes, WebhookStatuses
+from src.db import Base
+
+
+class Webhook(Base):
+    __tablename__ = "webhooks"
+    id = Column(String, primary_key=True, index=True)
+    signature = Column(String, unique=True, index=True, nullable=False)
+    escrow_address = Column(String(42), unique=True, nullable=False)
+    url = Column(URLType, unique=True, nullable=False)
+    network_id = Column(String, Enum(Networks), nullable=False)
+    type = Column(Enum(WebhookTypes))
+    status = Column(Enum(WebhookStatuses), server_default=WebhookStatuses.pending.value)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    wait_until = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return f"Webhook. id={self.id}"
