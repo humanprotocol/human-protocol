e7f083dba210d1cdd7fc408b407eb0178f97f38d
Vlad Nedelcu <nedelcuvd@gmail.com>
2023-02-28 21:07:57 +0200

    added transaction handling for launcher

--- packages/examples/fortune/launcher/src/components/escrow/create.tsx
+++ packages/examples/fortune/launcher/src/components/escrow/create.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ESCROW_FACTORY_ADDRESS, ESCROW_FACTORY_MX_ADDRESS } from '../../constants/constants';
-import { useGetAccountInfo, useGetIsLoggedIn } from '@multiversx/sdk-dapp/hooks';
+import { useGetAccountInfo, useGetIsLoggedIn, useTrackTransactionStatus } from '@multiversx/sdk-dapp/hooks';
 import EscrowFactory from '../mx/service/escrow-factory.service';
 import { FactoryInterface } from '../escrow-interface.service';
 import { Web3EscrowFactory } from '../web3/service/escrow-factory.service';
@@ -15,6 +15,31 @@ export default function CreateEscrow() {
   const [escrowFactory, setEscrowFactory] = useState<FactoryInterface>(
     !Boolean(address) ? new Web3EscrowFactory() : new EscrowFactory()
   );
+
+  const [sessionId, setSessionId] = useState<string | null>(null);
+
+  const handleSuccessCreation = async () => {
+    const transactions = transactionStatus.transactions;
+    if (transactions !== undefined) {
+      const lastTxHash = transactions[0].hash;
+      const resp = await escrowFactory.getTxOutcome(lastTxHash);
+      setLastEscrow(resp.firstValue?.valueOf().toString());
+      setEscrow(resp.firstValue?.valueOf().toString());
+    }
+    setSessionId(null);
+  };
+
+  const handleFailureCreation = () => {
+    setSessionId(null);
+  };
+
+  const transactionStatus = useTrackTransactionStatus({
+    transactionId: sessionId,
+    onSuccess: handleSuccessCreation,
+    onFail: handleFailureCreation,
+    onCancelled: handleFailureCreation
+  });
+
 
   useEffect(() => {
     if (isMxLoggedIn) {
@@ -45,9 +70,14 @@ export default function CreateEscrow() {
       addr = null;
     }
 
-    let createdEscrow = await escrowFactory.createJob(addr);
-    setEscrow(createdEscrow);
-    setLastEscrow(createdEscrow);
+    const resp = await escrowFactory.createJob(addr);
+
+    if (escrowFactory instanceof EscrowFactory) {
+      setSessionId(resp.sessionId);
+    } else {
+      setEscrow(resp);
+      setLastEscrow(resp);
+    }
   };
 
   return (
