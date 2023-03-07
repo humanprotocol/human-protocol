948c579ac10760ef13bd6002752b7e4772406b5c
Dzeranov <sergey.dzeranov@gmail.com>
2023-03-07 17:55:27 +0300

    Changed DB initialization (#292)
    Pytest is added as a testing tool

--- packages/examples/cvat/exchange-oracle/alembic/env.py
+++ packages/examples/cvat/exchange-oracle/alembic/env.py
@@ -22,8 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from src.db import Base
-from src.modules.webhook.model import Webhook
+from src.db.base import Base
 
 target_metadata = Base.metadata
 
