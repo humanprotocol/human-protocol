6239266426837a60065d3fbab58981c6a4d3bd7d
Dzeranov <sergey.dzeranov@gmail.com>
2023-03-07 17:55:27 +0300

    Changed DB initialization (#292)
    Pytest is added as a testing tool

---
+++ packages/examples/cvat/exchange-oracle/src/db/deps.py
@@ -0,0 +1,10 @@
+from typing import Generator
+from src.db.session import SessionLocal
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
