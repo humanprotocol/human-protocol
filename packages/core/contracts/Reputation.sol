8180169d6127ae10ab434f2cf83d3263b870e6fa
Francisco López <francislopez977@gmail.com>
2023-03-13 15:23:46 +0100

    Test changed files

--- packages/core/contracts/Reputation.sol
+++ packages/core/contracts/Reputation.sol
@@ -130,4 +130,6 @@ contract Reputation is OwnableUpgradeable, UUPSUpgradeable {
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
     uint256[43] private __gap;
+
+    /** TEST */
 }
