aec793b3c7266d3e9157bc3f0d83da3c1c48aad8
Dzeranov <sergey.dzeranov@gmail.com>
2023-03-16 21:20:31 +0300

    Job creation on CVAT via `cvat-sdk` (#331)
    * Added cvat-sdk support;
    Added cron job for processing pending webhooks;
    
    * Added cvat-sdk (job creation on CVAT)
    Added route for listening webhooks from CVAT (create:task, update:job)

--- packages/examples/cvat/exchange-oracle/tests/conftest.py
+++ packages/examples/cvat/exchange-oracle/tests/conftest.py
@@ -3,22 +3,14 @@ from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
 
-from src.db.deps import get_db
-from src.db.base import Base
+from src.db import Base, engine
 from src import app
 
-from .utils.overrides import override_get_db
-from .utils.test_db import TestingSessionLocal, engine
 
-app.dependency_overrides[get_db] = override_get_db
-
-
-@pytest.fixture(scope="session")
-def db() -> Generator:
+@pytest.fixture(autouse=True)
+def db():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-
-    yield TestingSessionLocal()
 
 
 @pytest.fixture(scope="module")
--- packages/examples/cvat/exchange-oracle/tests/utils/overrides.py
+++
@@ -1,9 +0,0 @@
-from .test_db import TestingSessionLocal
-
-
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
