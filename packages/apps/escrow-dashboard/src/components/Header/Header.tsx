ced6326cb931ce950da8f8df40e5d5d6d48873eb
m00n620 <gex6611@gmail.com>
2023-03-23 22:07:31 -0400

    add warning on escrow dashboard

--- packages/apps/escrow-dashboard/src/components/Header/Header.tsx
+++ packages/apps/escrow-dashboard/src/components/Header/Header.tsx
@@ -4,8 +4,10 @@ import {
   Search as SearchIcon,
 } from '@mui/icons-material';
 import {
+  Alert,
   AppBar,
   Box,
+  Collapse,
   Drawer,
   IconButton,
   Link,
@@ -45,6 +47,8 @@ export const Header: FC = () => {
   const theme = useTheme();
   const isDownLg = useMediaQuery(theme.breakpoints.down('lg'));
   const isDownMd = useMediaQuery(theme.breakpoints.down('md'));
+
+  const [showWarning, setShowWarning] = useState(true);
 
   /**
    * @TODO: Remove the flag once it's implemented
@@ -86,82 +90,105 @@ export const Header: FC = () => {
         }}
       >
         <Toolbar disableGutters>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              width: '100%',
-              padding: {
-                xs: '30px 8px 26px',
-                sm: '30px 28px 26px',
-                md: '30px 52px 26px',
-              },
-            }}
-          >
-            {searchOpen ? (
-              <Box display="flex" alignItems="center" width="100%">
-                {showSearchBox && <SearchBox />}
-                <IconButton
-                  color="primary"
-                  sx={{ ml: 2 }}
-                  onClick={toggleSearchBox}
-                >
-                  <CloseIcon />
-                </IconButton>
-              </Box>
-            ) : (
-              <>
-                <Link
-                  href="/"
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    textDecoration: 'none',
-                  }}
-                >
-                  <img src={logoSvg} alt="logo" />
-                  <Typography variant="h6" color="primary" ml="10px">
-                    Dashboard
-                  </Typography>
-                </Link>
-                {!isDownLg && (
-                  <Box sx={{ minWidth: '400px' }}>
-                    {showSearchBox && <SearchBox />}
-                  </Box>
-                )}
-                {!isDownMd && (
-                  <Box display="flex" alignItems="center" gap={3}>
-                    {isDownLg && (
-                      <IconButton color="primary" onClick={toggleSearchBox}>
+          <Box sx={{ width: '100%' }}>
+            <Collapse in={showWarning}>
+              <Alert
+                severity="warning"
+                action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setShowWarning(false);
+                    }}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }
+                sx={{ px: 5 }}
+              >
+                Beta Dashboard: minor inaccuracies may be present
+              </Alert>
+            </Collapse>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+                boxSizing: 'border-box',
+                padding: {
+                  xs: '30px 8px 26px',
+                  sm: '30px 28px 26px',
+                  md: '30px 52px 26px',
+                },
+              }}
+            >
+              {searchOpen ? (
+                <Box display="flex" alignItems="center" width="100%">
+                  {showSearchBox && <SearchBox />}
+                  <IconButton
+                    color="primary"
+                    sx={{ ml: 2 }}
+                    onClick={toggleSearchBox}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </Box>
+              ) : (
+                <>
+                  <Link
+                    href="/"
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      textDecoration: 'none',
+                    }}
+                  >
+                    <img src={logoSvg} alt="logo" />
+                    <Typography variant="h6" color="primary" ml="10px">
+                      Dashboard
+                    </Typography>
+                  </Link>
+                  {!isDownLg && (
+                    <Box sx={{ minWidth: '400px' }}>
+                      {showSearchBox && <SearchBox />}
+                    </Box>
+                  )}
+                  {!isDownMd && (
+                    <Box display="flex" alignItems="center" gap={3}>
+                      {isDownLg && (
+                        <IconButton color="primary" onClick={toggleSearchBox}>
+                          <SearchIcon />
+                        </IconButton>
+                      )}
+                      {renderNavLinks()}
+                      <ConnectButton />
+                    </Box>
+                  )}
+                  {isDownMd && (
+                    <Box>
+                      <IconButton
+                        color="primary"
+                        sx={{ ml: 1 }}
+                        onClick={toggleSearchBox}
+                      >
                         <SearchIcon />
                       </IconButton>
-                    )}
-                    {renderNavLinks()}
-                    <ConnectButton />
-                  </Box>
-                )}
-                {isDownMd && (
-                  <Box>
-                    <IconButton
-                      color="primary"
-                      sx={{ ml: 1 }}
-                      onClick={toggleSearchBox}
-                    >
-                      <SearchIcon />
-                    </IconButton>
-                    <IconButton
-                      color="primary"
-                      sx={{ ml: 1 }}
-                      onClick={toggleDrawer}
-                    >
-                      <MenuIcon />
-                    </IconButton>
-                  </Box>
-                )}
-              </>
-            )}
+                      <IconButton
+                        color="primary"
+                        sx={{ ml: 1 }}
+                        onClick={toggleDrawer}
+                      >
+                        <MenuIcon />
+                      </IconButton>
+                    </Box>
+                  )}
+                </>
+              )}
+            </Box>
           </Box>
         </Toolbar>
       </AppBar>
