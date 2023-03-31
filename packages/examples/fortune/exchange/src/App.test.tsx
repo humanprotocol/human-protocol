d901d5c643c58c7cdca39903910c1545eb20d859
Eric Lee <eric@hmt.ai>
2023-03-31 10:13:33 -0400

    migrate CRA to vite for fortune exchange

--- packages/examples/fortune/exchange/src/App.test.tsx
+++ packages/examples/fortune/exchange/src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-test-renderer';
+import { Providers, setupClient, getSigners } from 'tests/utils';
+import { MockConnector } from 'wagmi/connectors/mock';
+
 import App from './App';
 
-test('renders metamask', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Metamask not installed/i);
+test('renders correctly', async () => {
+  const client = setupClient({
+    connectors: [
+      new MockConnector({
+        options: {
+          signer: getSigners()[0]!,
+          // Turn on `failConnect` flag to simulate connect failure
+        },
+      }),
+    ],
+  });
+
+  await act(async () => {
+    render(<App />, {
+      wrapper: ({ children }: { children: React.ReactNode }) => (
+        <Providers client={client}>{children}</Providers>
+      ),
+    });
+  });
+
+  const linkElement = screen.getByText(/Select Network/i);
   expect(linkElement).toBeInTheDocument();
 });
--- packages/examples/fortune/exchange/src/components/Escrow/Escrow.test.tsx
+++ packages/examples/fortune/exchange/src/components/Escrow/Escrow.test.tsx
@@ -1,23 +1,42 @@
-import renderer from 'react-test-renderer';
 import { screen, render } from '@testing-library/react';
+
+import { act, create } from 'react-test-renderer';
+import { Providers, setupClient, getSigners } from 'tests/utils';
+import { MockConnector } from 'wagmi/connectors/mock';
+
 import { Escrow } from './Escrow';
 
 describe('When rendered Escrow component', () => {
-
-    it('renders landing page', async () => {
-        render(<Escrow />);
-        expect(screen.getByTestId("escrowAddress")).toBeInTheDocument();
-        expect(screen.getByRole("button", { name: "Confirm" })).toBeInTheDocument();
-        expect(screen.getByRole("button", { name: "Send Fortune" })).toBeInTheDocument();
+  it('renders landing page', async () => {
+    const client = setupClient({
+      connectors: [
+        new MockConnector({
+          options: {
+            signer: getSigners()[0]!,
+            // Turn on `failConnect` flag to simulate connect failure
+          },
+        }),
+      ],
+    });
+    await act(async () => {
+      render(<Escrow />, {
+        wrapper: ({ children }: { children: React.ReactNode }) => (
+          <Providers client={client}>{children}</Providers>
+        ),
+      });
     });
 
+    expect(screen.getByTestId('escrowAddress')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Confirm' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Send Fortune' })
+    ).toBeInTheDocument();
+  });
 
-    it('should match snapshot', () => {
-        const component = renderer.create(
-            <Escrow />
-        );
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-        return;
-    });
+  it('should match snapshot', () => {
+    const component = create(<Escrow />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    return;
+  });
 });
