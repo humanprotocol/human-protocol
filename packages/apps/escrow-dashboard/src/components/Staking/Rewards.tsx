258367346bbaac76a0ea642bbf43d85a6478abfc
m00n620 <gex6611@gmail.com>
2023-04-11 09:27:16 -0400

    polish UI

--- packages/apps/escrow-dashboard/src/components/Staking/Rewards.tsx
+++ packages/apps/escrow-dashboard/src/components/Staking/Rewards.tsx
@@ -4,9 +4,11 @@ import {
   FormControl,
   Grid,
   InputLabel,
+  Link,
   NativeSelect,
   ToggleButton,
   ToggleButtonGroup,
+  Typography,
 } from '@mui/material';
 import { FC, useState } from 'react';
 import { Container } from '../Cards/Container';
@@ -18,64 +20,97 @@ type Mode = 'claim' | 'reinvest';
 
 export const Rewards: FC<RewardsProps> = () => {
   const [mode, setMode] = useState<Mode>();
+  const [isSuccess, setIsSuccess] = useState(false);
 
   return (
     <Container>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'space-between',
-          height: '100%',
-        }}
-      >
-        <Box>
-          <ToggleButtonGroup
-            value={mode}
-            exclusive
-            onChange={(e, newMode) => setMode(newMode)}
-            fullWidth
-          >
-            <ToggleButton color="primary" value="stake">
-              Claim Rewards
-            </ToggleButton>
-            <ToggleButton color="primary" value="unstake">
-              Reinvest Rewards
-            </ToggleButton>
-          </ToggleButtonGroup>
-          <Box mt={4} mb={5}>
-            <Grid container spacing={4}>
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth>
-                  <InputLabel variant="standard" htmlFor="network-selector">
-                    Network
-                  </InputLabel>
-                  <NativeSelect
-                    defaultValue={30}
-                    inputProps={{
-                      name: 'network',
-                      id: 'network-selector',
-                    }}
-                  >
-                    <option value={10}>Polygon</option>
-                    <option value={20}>BSC</option>
-                    <option value={30}>Avalanche</option>
-                  </NativeSelect>
-                </FormControl>
-              </Grid>
-            </Grid>
-          </Box>
-          <CurrencyInput placeholder="Enter amount to claim" />
-        </Box>
-        <Button
-          color="primary"
-          variant="contained"
-          fullWidth
-          sx={{ mt: 'auto' }}
+      {!isSuccess ? (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'space-between',
+            height: '100%',
+          }}
         >
-          Confirm
-        </Button>
-      </Box>
+          <Box mb={3}>
+            <ToggleButtonGroup
+              value={mode}
+              exclusive
+              onChange={(e, newMode) => setMode(newMode)}
+              fullWidth
+            >
+              <ToggleButton color="primary" value="stake">
+                Claim Rewards
+              </ToggleButton>
+              <ToggleButton color="primary" value="unstake">
+                Reinvest Rewards
+              </ToggleButton>
+            </ToggleButtonGroup>
+            <Box mt={4} mb={5}>
+              <Grid container spacing={4}>
+                <Grid item xs={12} sm={6}>
+                  <FormControl fullWidth>
+                    <InputLabel variant="standard" htmlFor="network-selector">
+                      Network
+                    </InputLabel>
+                    <NativeSelect
+                      defaultValue={30}
+                      inputProps={{
+                        name: 'network',
+                        id: 'network-selector',
+                      }}
+                    >
+                      <option value={10}>Polygon</option>
+                      <option value={20}>BSC</option>
+                      <option value={30}>Avalanche</option>
+                    </NativeSelect>
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </Box>
+            <CurrencyInput placeholder="Enter amount to claim" />
+          </Box>
+          <Button
+            color="primary"
+            variant="contained"
+            fullWidth
+            sx={{ mt: 'auto', p: 1, fontWeight: 600 }}
+            onClick={() => setIsSuccess(true)}
+          >
+            Confirm
+          </Button>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            py: 10,
+          }}
+        >
+          <Typography
+            color="primary"
+            variant="h6"
+            fontWeight={500}
+            gutterBottom
+          >
+            Success!
+          </Typography>
+          <Typography color="primary" variant="body2" textAlign="center" mt={2}>
+            Congratulations, you have successfully claimed 1000 HMT on the
+            Network 1.
+          </Typography>
+          <Typography color="primary" variant="body2" fontWeight={600} mt={4}>
+            Check transaction{' '}
+            <Link href="https://etherscan.io" target="_blank">
+              here
+            </Link>
+          </Typography>
+        </Box>
+      )}
     </Container>
   );
 };
--- packages/apps/escrow-dashboard/src/components/Staking/Stake.tsx
+++ packages/apps/escrow-dashboard/src/components/Staking/Stake.tsx
@@ -4,6 +4,7 @@ import {
   FormControl,
   Grid,
   InputLabel,
+  Link,
   NativeSelect,
   ToggleButton,
   ToggleButtonGroup,
@@ -19,96 +20,133 @@ type Mode = 'stake' | 'unstake';
 
 export const Stake: FC<StakeProps> = () => {
   const [mode, setMode] = useState<Mode>();
-
+  const [isSuccess, setIsSuccess] = useState(false);
   return (
     <Container>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'space-between',
-          height: '100%',
-        }}
-      >
-        <Box>
-          <ToggleButtonGroup
-            value={mode}
-            exclusive
-            onChange={(e, newMode) => setMode(newMode)}
+      {!isSuccess ? (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'space-between',
+            height: '100%',
+          }}
+        >
+          <Box>
+            <ToggleButtonGroup
+              value={mode}
+              exclusive
+              onChange={(e, newMode) => setMode(newMode)}
+              fullWidth
+            >
+              <ToggleButton color="primary" value="stake">
+                Stake HMT
+              </ToggleButton>
+              <ToggleButton color="primary" value="unstake">
+                Unstake HMT
+              </ToggleButton>
+            </ToggleButtonGroup>
+            <Box mt={4} mb={5}>
+              <Grid container spacing={4} sx={{ mb: 5 }}>
+                <Grid item xs={12} sm={6}>
+                  <FormControl fullWidth>
+                    <InputLabel variant="standard" htmlFor="network-selector">
+                      Network
+                    </InputLabel>
+                    <NativeSelect
+                      defaultValue={30}
+                      inputProps={{
+                        name: 'network',
+                        id: 'network-selector',
+                      }}
+                    >
+                      <option value={10}>Polygon</option>
+                      <option value={20}>BSC</option>
+                      <option value={30}>Avalanche</option>
+                    </NativeSelect>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <FormControl fullWidth>
+                    <InputLabel variant="standard" htmlFor="role-selector">
+                      Staking as:
+                    </InputLabel>
+                    <NativeSelect
+                      defaultValue={30}
+                      inputProps={{
+                        name: 'role',
+                        id: 'role-selector',
+                      }}
+                    >
+                      <option value={10}>Job Launcher</option>
+                      <option value={20}>Exchange Oracle</option>
+                      <option value={30}>Reputation Oracle</option>
+                    </NativeSelect>
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </Box>
+            <CurrencyInput placeholder="Enter amount to stake" />
+            <Box my={3}>
+              <Typography
+                color="primary"
+                textAlign="center"
+                mb={1.5}
+                variant="caption"
+                component="p"
+              >
+                Network address: 0xa9fbD31cd4EAA938Fc90A31eFe231316444a1102
+              </Typography>
+              <Typography
+                color="primary"
+                textAlign="center"
+                variant="caption"
+                component="p"
+              >
+                Staking amount: 0.00 HMT
+              </Typography>
+            </Box>
+          </Box>
+          <Button
+            color="primary"
+            variant="contained"
             fullWidth
+            sx={{ p: 1, fontWeight: 600 }}
+            onClick={() => setIsSuccess(true)}
           >
-            <ToggleButton color="primary" value="stake">
-              Stake HMT
-            </ToggleButton>
-            <ToggleButton color="primary" value="unstake">
-              Unstake HMT
-            </ToggleButton>
-          </ToggleButtonGroup>
-          <Box mt={4} mb={5}>
-            <Grid container spacing={4} sx={{ mb: 5 }}>
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth>
-                  <InputLabel variant="standard" htmlFor="network-selector">
-                    Network
-                  </InputLabel>
-                  <NativeSelect
-                    defaultValue={30}
-                    inputProps={{
-                      name: 'network',
-                      id: 'network-selector',
-                    }}
-                  >
-                    <option value={10}>Polygon</option>
-                    <option value={20}>BSC</option>
-                    <option value={30}>Avalanche</option>
-                  </NativeSelect>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth>
-                  <InputLabel variant="standard" htmlFor="role-selector">
-                    Staking as:
-                  </InputLabel>
-                  <NativeSelect
-                    defaultValue={30}
-                    inputProps={{
-                      name: 'role',
-                      id: 'role-selector',
-                    }}
-                  >
-                    <option value={10}>Job Launcher</option>
-                    <option value={20}>Exchange Oracle</option>
-                    <option value={30}>Reputation Oracle</option>
-                  </NativeSelect>
-                </FormControl>
-              </Grid>
-            </Grid>
-          </Box>
-          <CurrencyInput placeholder="Enter amount to stake" />
-          <Box my={3}>
-            <Typography
-              color="primary"
-              textAlign="center"
-              mb={1.5}
-              variant="caption"
-              component="p"
-            >
-              Network address: 0xa9fbD31cd4EAA938Fc90A31eFe231316444a1102
-            </Typography>
-            <Typography
-              color="primary"
-              textAlign="center"
-              variant="caption"
-              component="p"
-            >
-              Staking amount: 0.00 HMT
-            </Typography>
-          </Box>
+            Confirm
+          </Button>
         </Box>
-        <Button color="primary" variant="contained" fullWidth>
-          Confirm
-        </Button>
-      </Box>
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            py: 10,
+          }}
+        >
+          <Typography
+            color="primary"
+            variant="h6"
+            fontWeight={500}
+            gutterBottom
+          >
+            Success!
+          </Typography>
+          <Typography color="primary" variant="body2" textAlign="center" mt={2}>
+            Congratulations, you have successfully staked 1000 HMT on the
+            Network 1 to the role of Job Launcher.
+          </Typography>
+          <Typography color="primary" variant="body2" fontWeight={600} mt={4}>
+            Check transaction{' '}
+            <Link href="https://etherscan.io" target="_blank">
+              here
+            </Link>
+          </Typography>
+        </Box>
+      )}
     </Container>
   );
 };
