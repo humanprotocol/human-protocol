4f8df8d54c21bb09802fd627afc47c35aec8b1bb
eugenvoronov <104138627+eugenvoronov@users.noreply.github.com>
2023-04-20 11:02:31 +0300

    [WIP] typescript sdk/storage (#381)
    * Updated storage module: get client and upload files methods
    
    * Added storage client class
    
    * Added export to class
    
    * Updated job module and job tests
    
    * Added storage module test coverage
    
    * Change jest to vitest
    
    * Added mock methods
    
    * Updated tests included mock funcs
    
    * Removed unused params
    
    * Removed upload mock in download test case
    
    * Added deleted job.test file
    
    * Lint fix
    
    * Resolved PRs comments and add test case
    
    ---------
    
    Co-authored-by: Francisco López <francislopez977@gmail.com>

--- packages/sdk/typescript/human-protocol-sdk/src/constants.ts
+++ packages/sdk/typescript/human-protocol-sdk/src/constants.ts
@@ -2,14 +2,24 @@ import { ChainId } from './enums';
 import { NetworkData } from './types';
 
 /**
- * @constant Default bucket name
- */
-export const DEFAULT_BUCKET = 'escrow-results';
-
-/**
  * @constant Default public bucket name
  */
 export const DEFAULT_PUBLIC_BUCKET = 'escrow-public-results';
+
+/**
+ * @constant Default storage endpoint
+ */
+export const DEFAULT_ENDPOINT = 'localhost';
+
+/**
+ * @constant Default storage port
+ */
+export const DEFAULT_PORT = 9000;
+
+/**
+ * @constant Default storage port
+ */
+export const DEFAULT_USE_SSL = false;
 
 /**
  * @constant Default network parameters
---
+++ packages/sdk/typescript/human-protocol-sdk/test/storage.test.ts
@@ -0,0 +1,386 @@
+import { describe, test, expect, vi, beforeAll } from 'vitest';
+import crypto from 'crypto';
+import {
+  DEFAULT_ENDPOINT,
+  DEFAULT_PORT,
+  DEFAULT_PUBLIC_BUCKET,
+  DEFAULT_USE_SSL,
+  StorageCredentials,
+  StorageParams,
+} from '../src';
+import {
+  ErrorStorageClientNotInitialized,
+  ErrorStorageFileNotFound,
+  ErrorStorageFileNotUploaded,
+} from '../src/error';
+import StorageClient from '../src/storage';
+import {
+  STORAGE_FAKE_BUCKET,
+  STORAGE_TEST_ACCESS_KEY,
+  STORAGE_TEST_FILE_VALUE,
+  STORAGE_TEST_FILE_VALUE_2,
+  STORAGE_TEST_SECRET_KEY,
+} from './utils/constants';
+
+// Create a Minio.Client mock for the tests
+vi.mock('minio', () => {
+  // Define a constructor for the Minio.Client mock
+  class Client {
+    getObject = vi.fn().mockImplementation(() => {
+      const read = () => {
+        return JSON.stringify({ key: STORAGE_TEST_FILE_VALUE });
+      };
+      return Promise.resolve({ read });
+    }); // getObject mock
+    putObject = vi.fn(); // putObject mock
+    bucketExists = vi.fn().mockImplementation((bucketName) => {
+      // Add conditional logic here based on the test scenario
+      if (bucketName === STORAGE_FAKE_BUCKET) {
+        return Promise.resolve(false); // Return false for fake scenario
+      } else {
+        return Promise.resolve(true); // Return true for other scenarios
+      }
+    });
+  }
+
+  // Return Minio.Client mock
+  return { Client };
+});
+
+describe('Storage tests', () => {
+  describe('Client initialization', () => {
+    test('should set correct credentials', async () => {
+      const storageCredentials: StorageCredentials = {
+        accessKey: STORAGE_TEST_ACCESS_KEY,
+        secretKey: STORAGE_TEST_SECRET_KEY,
+      };
+
+      expect(storageCredentials.accessKey).toEqual(STORAGE_TEST_ACCESS_KEY);
+      expect(storageCredentials.secretKey).toEqual(STORAGE_TEST_SECRET_KEY);
+    });
+
+    test('should set correct params', async () => {
+      const storageParams: StorageParams = {
+        endPoint: DEFAULT_ENDPOINT,
+        port: DEFAULT_PORT,
+        useSSL: DEFAULT_USE_SSL,
+      };
+
+      expect(storageParams.endPoint).toEqual(DEFAULT_ENDPOINT);
+      expect(storageParams.port).toEqual(DEFAULT_PORT);
+      expect(storageParams.useSSL).toEqual(false);
+    });
+
+    test('should init client with empty credentials', async () => {
+      const storageCredentials: StorageCredentials = {
+        accessKey: '',
+        secretKey: '',
+      };
+
+      const storageParams: StorageParams = {
+        endPoint: DEFAULT_ENDPOINT,
+        port: DEFAULT_PORT,
+        useSSL: DEFAULT_USE_SSL,
+      };
+
+      const storageClient = new StorageClient(
+        storageCredentials,
+        storageParams
+      );
+
+      expect(storageClient).toBeInstanceOf(StorageClient);
+    });
+
+    test('should not init client with an error', async () => {
+      // TODO: Adapt it for particular test case
+      /* vi.mock('../src/storage', () => {
+        const StorageClient = vi.fn().mockImplementation(() => {
+          throw ErrorStorageClientNotInitialized;
+        });
+      
+        return {
+          default: StorageClient
+        }
+      }) */
+
+      const storageCredentials: StorageCredentials = {
+        accessKey: '',
+        secretKey: '',
+      };
+
+      const storageParams: StorageParams = {
+        endPoint: DEFAULT_ENDPOINT,
+        port: DEFAULT_PORT,
+        useSSL: DEFAULT_USE_SSL,
+      };
+
+      // expect(() => new StorageClient(storageCredentials, storageParams)).toThrow(ErrorStorageClientNotInitialized);
+    });
+  });
+
+  describe('Client anonymous access', () => {
+    let storageClient: StorageClient;
+
+    beforeAll(async () => {
+      const storageCredentials: StorageCredentials = {
+        accessKey: '',
+        secretKey: '',
+      };
+
+      const storageParams: StorageParams = {
+        endPoint: DEFAULT_ENDPOINT,
+        port: DEFAULT_PORT,
+        useSSL: DEFAULT_USE_SSL,
+      };
+
+      storageClient = new StorageClient(storageCredentials, storageParams);
+    });
+
+    test('should return the bucket exists', async () => {
+      const isExists = await storageClient.bucketExists(DEFAULT_PUBLIC_BUCKET);
+      expect(isExists).toEqual(true);
+    });
+
+    test('should return the bucket does not exist', async () => {
+      const isExists = await storageClient.bucketExists(STORAGE_FAKE_BUCKET);
+      expect(isExists).toEqual(false);
+    });
+
+    test('should upload the file with success', async () => {
+      const file = { key: STORAGE_TEST_FILE_VALUE };
+
+      const uploadedResults = await storageClient.uploadFiles(
+        [file],
+        DEFAULT_PUBLIC_BUCKET
+      );
+
+      const hash = crypto
+        .createHash('sha1')
+        .update(JSON.stringify(file))
+        .digest('hex');
+      const key = hash;
+
+      expect(storageClient['client'].putObject).toHaveBeenCalledWith(
+        DEFAULT_PUBLIC_BUCKET,
+        key,
+        JSON.stringify(file),
+        {
+          'Content-Type': 'application/json',
+        }
+      );
+      expect(uploadedResults[0].key).toEqual(key);
+      expect(uploadedResults[0].hash).toEqual(hash);
+    });
+
+    test('should not upload the file with an error', async () => {
+      const file = { key: STORAGE_TEST_FILE_VALUE };
+      vi.spyOn(storageClient, 'uploadFiles').mockImplementation(() => {
+        throw ErrorStorageFileNotUploaded;
+      });
+      expect(() =>
+        storageClient.uploadFiles([file], DEFAULT_PUBLIC_BUCKET)
+      ).toThrow(ErrorStorageFileNotUploaded);
+    });
+
+    test('should download the file with success', async () => {
+      const file = { key: STORAGE_TEST_FILE_VALUE };
+
+      const hash = crypto
+        .createHash('sha1')
+        .update(JSON.stringify(file))
+        .digest('hex');
+      const key = hash;
+
+      const downloadedResults = await storageClient.downloadFiles(
+        [key],
+        DEFAULT_PUBLIC_BUCKET
+      );
+
+      expect(storageClient['client'].getObject).toHaveBeenCalledWith(
+        DEFAULT_PUBLIC_BUCKET,
+        key
+      );
+      expect(downloadedResults[0].key).toEqual(key);
+      expect(downloadedResults[0].content).toEqual(file);
+    });
+
+    test('should not download the file with an error', async () => {
+      vi.spyOn(storageClient, 'downloadFiles').mockImplementation(() => {
+        throw ErrorStorageFileNotFound;
+      });
+      expect(() =>
+        storageClient.downloadFiles(
+          [STORAGE_TEST_FILE_VALUE],
+          DEFAULT_PUBLIC_BUCKET
+        )
+      ).toThrow(ErrorStorageFileNotFound);
+    });
+
+    test('should return a list of objects with success', async () => {
+      const file1 = { key: STORAGE_TEST_FILE_VALUE };
+      const hash1 = crypto
+        .createHash('sha1')
+        .update(JSON.stringify(file1))
+        .digest('hex');
+      const key1 = hash1;
+
+      const file2 = { key: STORAGE_TEST_FILE_VALUE_2 };
+      const hash2 = crypto
+        .createHash('sha1')
+        .update(JSON.stringify(file2))
+        .digest('hex');
+      const key2 = hash2;
+
+      vi.spyOn(storageClient, 'listObjects').mockImplementation(() =>
+        Promise.resolve([key1, key2])
+      );
+
+      const results = await storageClient.listObjects(DEFAULT_PUBLIC_BUCKET);
+
+      expect(results[0]).toEqual(key1);
+      expect(results[1]).toEqual(key2);
+    });
+
+    test('should not return a list of objects with an error', async () => {
+      vi.spyOn(storageClient, 'listObjects').mockImplementation(() => {
+        throw new Error();
+      });
+      expect(() => storageClient.listObjects(DEFAULT_PUBLIC_BUCKET)).toThrow(
+        new Error()
+      );
+    });
+  });
+
+  describe('Client with credentials', () => {
+    let storageClient: StorageClient;
+
+    beforeAll(async () => {
+      const storageCredentials: StorageCredentials = {
+        accessKey: STORAGE_TEST_ACCESS_KEY,
+        secretKey: STORAGE_TEST_SECRET_KEY,
+      };
+
+      const storageParams: StorageParams = {
+        endPoint: DEFAULT_ENDPOINT,
+        port: DEFAULT_PORT,
+        useSSL: DEFAULT_USE_SSL,
+      };
+
+      storageClient = new StorageClient(storageCredentials, storageParams);
+    });
+
+    test('should return the bucket exists', async () => {
+      const isExists = await storageClient.bucketExists(DEFAULT_PUBLIC_BUCKET);
+      expect(isExists).toEqual(true);
+    });
+
+    test('should return the bucket does not exist', async () => {
+      const isExists = await storageClient.bucketExists(STORAGE_FAKE_BUCKET);
+      expect(isExists).toEqual(false);
+    });
+
+    test('should upload the file with success', async () => {
+      const file = { key: STORAGE_TEST_FILE_VALUE };
+
+      const uploadedResults = await storageClient.uploadFiles(
+        [file],
+        DEFAULT_PUBLIC_BUCKET
+      );
+
+      const hash = crypto
+        .createHash('sha1')
+        .update(JSON.stringify(file))
+        .digest('hex');
+      const key = hash;
+
+      expect(storageClient['client'].putObject).toHaveBeenCalledWith(
+        DEFAULT_PUBLIC_BUCKET,
+        key,
+        JSON.stringify(file),
+        {
+          'Content-Type': 'application/json',
+        }
+      );
+      expect(uploadedResults[0].key).toEqual(key);
+      expect(uploadedResults[0].hash).toEqual(hash);
+    });
+
+    test('should not upload the file with an error', async () => {
+      const file = { key: STORAGE_TEST_FILE_VALUE };
+      vi.spyOn(storageClient, 'uploadFiles').mockImplementation(() => {
+        throw ErrorStorageFileNotUploaded;
+      });
+      expect(() =>
+        storageClient.uploadFiles([file], DEFAULT_PUBLIC_BUCKET)
+      ).toThrow(ErrorStorageFileNotUploaded);
+    });
+
+    test('should download the file with success', async () => {
+      const file = { key: STORAGE_TEST_FILE_VALUE };
+
+      const hash = crypto
+        .createHash('sha1')
+        .update(JSON.stringify(file))
+        .digest('hex');
+      const key = hash;
+
+      const downloadedResults = await storageClient.downloadFiles(
+        [key],
+        DEFAULT_PUBLIC_BUCKET
+      );
+
+      expect(storageClient['client'].getObject).toHaveBeenCalledWith(
+        DEFAULT_PUBLIC_BUCKET,
+        key
+      );
+      expect(downloadedResults[0].key).toEqual(key);
+      expect(downloadedResults[0].content).toEqual(file);
+    });
+
+    test('should not download the file with an error', async () => {
+      vi.spyOn(storageClient, 'downloadFiles').mockImplementation(() => {
+        throw ErrorStorageFileNotFound;
+      });
+      expect(() =>
+        storageClient.downloadFiles(
+          [STORAGE_TEST_FILE_VALUE],
+          DEFAULT_PUBLIC_BUCKET
+        )
+      ).toThrow(ErrorStorageFileNotFound);
+    });
+
+    test('should return a list of objects with success', async () => {
+      const file1 = { key: STORAGE_TEST_FILE_VALUE };
+      const hash1 = crypto
+        .createHash('sha1')
+        .update(JSON.stringify(file1))
+        .digest('hex');
+      const key1 = hash1;
+
+      const file2 = { key: STORAGE_TEST_FILE_VALUE_2 };
+      const hash2 = crypto
+        .createHash('sha1')
+        .update(JSON.stringify(file2))
+        .digest('hex');
+      const key2 = hash2;
+
+      vi.spyOn(storageClient, 'listObjects').mockImplementation(() =>
+        Promise.resolve([key1, key2])
+      );
+
+      const results = await storageClient.listObjects(DEFAULT_PUBLIC_BUCKET);
+
+      expect(results[0]).toEqual(key1);
+      expect(results[1]).toEqual(key2);
+    });
+
+    test('should not return a list of objects with an error', async () => {
+      vi.spyOn(storageClient, 'listObjects').mockImplementation(() => {
+        throw new Error();
+      });
+      expect(() => storageClient.listObjects(DEFAULT_PUBLIC_BUCKET)).toThrow(
+        new Error()
+      );
+    });
+  });
+});
