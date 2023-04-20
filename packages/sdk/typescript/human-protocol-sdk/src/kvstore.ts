4a039e7ebe8a8ad13172fc5a0ad2cddc9263baa5
Francisco López <francislopez977@gmail.com>
2023-04-20 17:16:31 +0200

    Add KVStore tests

--- packages/sdk/typescript/human-protocol-sdk/src/kvstore.ts
+++ packages/sdk/typescript/human-protocol-sdk/src/kvstore.ts
@@ -28,7 +28,7 @@ export default class KVStoreClient {
       else chainId = (await this.signerOrProvider.getNetwork()).chainId;
       const kvstoreAddress = NETWORKS[chainId]?.kvstoreAddress;
 
-      if (!kvstoreAddress) throw new Error('ChainId not supported');
+      if (!kvstoreAddress) return Error('ChainId not supported');
       this.contract = KVStore__factory.connect(
         kvstoreAddress,
         this.signerOrProvider
@@ -37,14 +37,16 @@ export default class KVStoreClient {
   }
 
   public async set(key: string, value: string) {
-    if (key === '' || value === '') return Error('Values can not be empty');
+    if (key === '' || value === '')
+      return Error('Key and value can not be empty');
     await this.init();
 
     try {
-      this.contract?.set(key, value);
+      await this.contract?.set(key, value);
       return null;
     } catch (e) {
-      return Error(e);
+      if (e instanceof Error)
+        return Error(`Failed to set value for key ${key}: ${e.message}`);
     }
   }
 
@@ -52,26 +54,30 @@ export default class KVStoreClient {
     if (keys.length !== values.length)
       return Error('Arrays must have the same length');
     if (keys.includes('') || values.includes(''))
-      return Error('Values can not be empty');
+      return Error('Keys and values can not be empty');
     await this.init();
 
     try {
-      this.contract?.setBulk(keys, values);
+      await this.contract?.setBulk(keys, values);
       return null;
     } catch (e) {
-      return Error(e);
+      if (e instanceof Error)
+        return Error(`Failed to set bulk values: ${e.message}`);
     }
   }
 
   public async get(address: string, key: string) {
     if (key === '') return Error('Key can not be empty');
-    if (!ethers.utils.isAddress(address)) return Error('Address not valid');
+    if (!ethers.utils.isAddress(address)) return Error('Invalid address');
     await this.init();
 
     try {
-      return this.contract?.get(address, key);
+      const result = await this.contract?.get(address, key);
+      if (result === '') return Error(`Value not found for key ${key}`);
+      return result;
     } catch (e) {
-      return Error(e);
+      if (e instanceof Error)
+        return Error(`Failed to get value for key ${key}: ${e.message}`);
     }
   }
 }
