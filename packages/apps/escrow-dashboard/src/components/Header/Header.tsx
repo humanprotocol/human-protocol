4a6f9b8629500bad6075be64b0c00ac54d5eee22
m00n620 <gex6611@gmail.com>
2023-05-09 11:18:40 -0400

    remove box shadow from the header

--- packages/apps/escrow-dashboard/src/components/Header/Header.tsx
+++ packages/apps/escrow-dashboard/src/components/Header/Header.tsx
@@ -121,7 +121,7 @@ export const Header: FC = () => {
         position="fixed"
         sx={{
           background: 'rgba(255, 255, 255, 0.8)',
-          boxShadow: '0px 4px 120px rgba(218, 222, 240, 0.8)',
+          boxShadow: 'none',
           backdropFilter: 'blur(9px)',
         }}
       >
