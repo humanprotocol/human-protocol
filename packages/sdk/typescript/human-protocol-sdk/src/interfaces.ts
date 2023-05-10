91b8ba200613c1cfe5000ee52978cfee88a166d7
eugenvoronov <104138627+eugenvoronov@users.noreply.github.com>
2023-05-10 13:56:31 +0300

    sdk/staking (#425)
    * Added init parameters class implementation
    
    * Updated names
    
    * Added approve stake implementation
    
    * Added all methods implementation and stake method test coverage
    
    * Mock staking and token contracts
    
    * Added test for staking module
    
    * Updated annotations on the methods
    
    * Resolved PR's comments
    
    * Added error handling, added gql call to get rewards
    
    * Added queries file
    
    * Removed unused error messages
    
    * Updated IStaker interface and get staker method
    
    * Added toThrow method and additional error handling cases
    
    * Implemented escrow factory contact and has escrow method. Updated tests
    
    * Fix comments
    
    * Removed toBigNumber method from utils
    
    ---------
    
    Co-authored-by: Francisco López <francislopez977@gmail.com>

--- packages/sdk/typescript/human-protocol-sdk/src/interfaces.ts
+++ packages/sdk/typescript/human-protocol-sdk/src/interfaces.ts
@@ -1,8 +1,29 @@
-import { Signer } from 'ethers';
+import { BigNumber, Signer } from 'ethers';
 import { Provider } from '@ethersproject/abstract-provider';
 import { NetworkData } from './types';
 
 export interface IClientParams {
   signerOrProvider: Signer | Provider;
   network: NetworkData;
+}
+
+export interface IAllocation {
+  escrowAddress: string;
+  staker: string;
+  tokens: BigNumber;
+  createdAt: BigNumber;
+  closedAt: BigNumber;
+}
+
+export interface IReward {
+  escrowAddress: string;
+  amount: BigNumber;
+}
+
+export interface IStaker {
+  tokensStaked: BigNumber;
+  tokensAllocated: BigNumber;
+  tokensLocked: BigNumber;
+  tokensLockedUntil: BigNumber;
+  tokensAvailable: BigNumber;
 }
