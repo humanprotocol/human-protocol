15cbff0fda11bf3d1663cc9d2be479b8422488cb
Francisco López <francislopez977@gmail.com>
2023-05-18 12:13:50 +0200

    Remove unused code

--- packages/sdk/typescript/human-protocol-sdk/src/constants.ts
+++ packages/sdk/typescript/human-protocol-sdk/src/constants.ts
@@ -2,26 +2,6 @@ import { ChainId } from './enums';
 import { NetworkData } from './types';
 
 /**
- * @constant Default public bucket name
- */
-export const DEFAULT_PUBLIC_BUCKET = 'escrow-public-results';
-
-/**
- * @constant Default storage endpoint
- */
-export const DEFAULT_ENDPOINT = 'localhost';
-
-/**
- * @constant Default storage port
- */
-export const DEFAULT_PORT = 9000;
-
-/**
- * @constant Default storage port
- */
-export const DEFAULT_USE_SSL = false;
-
-/**
  * @constant Default network parameters
  */
 export const NETWORKS: {
--- packages/sdk/typescript/human-protocol-sdk/src/index.ts
+++ packages/sdk/typescript/human-protocol-sdk/src/index.ts
@@ -1,4 +1,8 @@
-export { Job } from './job';
+export * from './init';
+export * from './staking';
+export * from './storage';
+export * from './kvstore';
 
 export * from './constants';
 export * from './types';
+export * from './enums';
