9a659a5b4e9494f0b8b44adb3e4035878fbcf93c
Francisco López <50665615+flopez7@users.noreply.github.com>
2023-06-01 12:26:39 +0200

    `human-protocol-sdk` integration (#531)
    * Integrate SDK
    
    * convert manifest to dict

--- packages/examples/cvat/exchange-oracle/src/modules/webhook/api.py
+++ packages/examples/cvat/exchange-oracle/src/modules/webhook/api.py
@@ -1,6 +1,10 @@
 from typing import Union
 from fastapi import APIRouter, Header
+from web3 import Web3
 from src.db import SessionLocal
+
+from src.utils.helpers import get_web3
+from src.utils.escrow import check_escrow
 
 from .service import create_webhook
 from .api_schema import JLWebhook, JLWebhookResponse
@@ -16,7 +20,10 @@ def save_webhook(
     jl_webhook: JLWebhook,
     human_signature: Union[str, None] = Header(default=None),
 ):
-    # TODO: Add escrow checks
+    check_escrow(
+        get_web3(jl_webhook.network), Web3.toChecksumAddress(jl_webhook.escrow_address)
+    )
+
     with SessionLocal.begin() as session:
         webhook_id = create_webhook(session, jl_webhook, human_signature)
 
