70a541b4a9bdbe5cb8f54301cbe6eab122cf7f99
Francisco López <50665615+flopez7@users.noreply.github.com>
2023-06-01 12:26:39 +0200

    `human-protocol-sdk` integration (#531)
    * Integrate SDK
    
    * convert manifest to dict

--- packages/examples/cvat/exchange-oracle/src/utils/helpers.py
+++ packages/examples/cvat/exchange-oracle/src/utils/helpers.py
@@ -1,3 +1,9 @@
+from web3 import Web3
+from web3.providers.rpc import HTTPProvider
+from src.modules.webhook.constants import Networks
+from src.config import Config
+
+
 def parse_manifest(manifest: dict):
     bucket_name = "example_bucket_name"
     region = "example_region"
@@ -12,3 +18,10 @@ def parse_manifest(manifest: dict):
         },
     ]
     return bucket_name, region, labels
+
+
+def get_web3(network_id: Networks):
+    if network_id == Config.polygon_mainnet.network_id:
+        return Web3(HTTPProvider(Config.polygon_mainnet.rpc_api))
+    else:
+        return Web3(HTTPProvider(Config.polygon_mumbai.rpc_api))
