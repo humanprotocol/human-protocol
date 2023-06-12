b5ad39f5f3bba160845cdc6572b820ae30642a58
Eric Lee <eric@hmt.ai>
2023-06-12 09:20:57 -0400

    add workflow for fortune v2 test

--- packages/examples/fortune-v2/recording-oracle/src/common/validators/ethers.ts
+++ packages/examples/fortune-v2/recording-oracle/src/common/validators/ethers.ts
@@ -20,6 +20,7 @@ class ValidateEthereumAddress implements ValidatorConstraintInterface {
 }
 
 export function IsValidEthereumAddress(validationOptions?: ValidationOptions) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (object: any, propertyName: string): void => {
     registerDecorator({
       name: "IsValidEthereumAddress",
