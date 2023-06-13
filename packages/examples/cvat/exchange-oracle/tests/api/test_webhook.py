3ed71b8afc52dd7c5863daa237f4323c877413cf
Francisco López <50665615+flopez7@users.noreply.github.com>
2023-06-13 12:26:57 +0200

    [CVAT][Exchange Oracle] Mock tests (#555)
    * Mock api tests
    
    * Change Exception to ValueError

--- packages/examples/cvat/exchange-oracle/tests/api/test_webhook.py
+++ packages/examples/cvat/exchange-oracle/tests/api/test_webhook.py
@@ -1,4 +1,7 @@
 from fastapi.testclient import TestClient
+from unittest.mock import MagicMock, patch
+from human_protocol_sdk.constants import Status
+from src.modules.api_schema import JLWebhook
 
 
 def test_incoming_webhook_200(client: TestClient) -> None:
@@ -6,33 +9,143 @@ def test_incoming_webhook_200(client: TestClient) -> None:
         "escrow_address": "0xFE776895f6b00AA53969b20119a4777Ed920676a",
         "chain_id": 80001,
     }
-    response = client.post(
-        f"/webhook/job-launcher",
-        headers={"human-signature": "mocked signature"},
-        json=data,
-    )
-    assert response.status_code == 200
+
+    with patch("src.modules.api.SessionLocal.begin") as mock_session_local, patch(
+        "src.modules.api.create_webhook"
+    ) as mock_create_webhook, patch(
+        "src.modules.chain.escrow.get_web3"
+    ) as mock_get_web3, patch(
+        "src.modules.chain.escrow.EscrowClient"
+    ) as mock_escrow_client:
+        mock_session = MagicMock()
+        mock_session_local.return_value.__enter__.return_value = mock_session
+        mock_create_webhook.return_value = "mocked_webhook_id"
+
+        mock_web3_instance = MagicMock()
+        mock_get_web3.return_value = mock_web3_instance
+
+        mock_escrow_instance = MagicMock()
+        mock_escrow_instance.get_balance.return_value = 100
+        mock_escrow_instance.get_status.return_value = Status.Pending
+        mock_escrow_client.return_value = mock_escrow_instance
+
+        response = client.post(
+            "/webhook/job-launcher",
+            headers={"human-signature": "mocked signature"},
+            json=data,
+        )
+
+        assert response.status_code == 200
+        assert response.json() == {"id": "mocked_webhook_id"}
+        mock_get_web3.assert_called_once()
+        mock_escrow_client.assert_called_once_with(mock_web3_instance)
+        mock_escrow_instance.get_balance.assert_called_once_with(
+            "0xFE776895f6b00AA53969b20119a4777Ed920676a"
+        )
+        mock_escrow_instance.get_status.assert_called_once_with(
+            "0xFE776895f6b00AA53969b20119a4777Ed920676a"
+        )
+        mock_session_local.assert_called_once()
+        mock_create_webhook.assert_called_once_with(
+            mock_session, JLWebhook(**data), "mocked signature"
+        )
 
 
 def test_incoming_webhook_400(client: TestClient) -> None:
+    # Invalid Address
     data = {
         "escrow_address": "bad_address",
         "chain_id": 80001,
     }
+
     response = client.post(
-        f"/webhook/job-launcher",
+        "/webhook/job-launcher",
         headers={"human-signature": "mocked signature"},
         json=data,
     )
-    assert response.status_code == 400
 
+    assert response.status_code == 400
+    assert response.json() == {
+        "errors": [
+            {
+                "field": "escrow_address",
+                "message": "bad_address is not a correct Web3 address",
+            }
+        ]
+    }
+
+    # Invalid Chain Id
     data = {
         "escrow_address": "0xFE776895f6b00AA53969b20119a4777Ed920676a",
-        "chain_id": 1370,
+        "chain_id": 1234,
     }
+
     response = client.post(
-        f"/webhook/job-launcher",
+        "/webhook/job-launcher",
         headers={"human-signature": "mocked signature"},
         json=data,
     )
+
     assert response.status_code == 400
+    assert response.json() == {
+        "errors": [
+            {
+                "field": "chain_id",
+                "message": "value is not a valid enumeration member; permitted: 137, 80001",
+            }
+        ]
+    }
+
+    # Invalid balance
+    data = {
+        "escrow_address": "0xFE776895f6b00AA53969b20119a4777Ed920676a",
+        "chain_id": 80001,
+    }
+
+    with patch("src.modules.chain.escrow.get_web3") as mock_get_web3, patch(
+        "src.modules.chain.escrow.EscrowClient"
+    ) as mock_escrow_client:
+        mock_web3_instance = MagicMock()
+        mock_get_web3.return_value = mock_web3_instance
+
+        mock_escrow_instance = MagicMock()
+        mock_escrow_instance.get_balance.return_value = 0
+        mock_escrow_instance.get_status.return_value = Status.Pending
+        mock_escrow_client.return_value = mock_escrow_instance
+
+        response = client.post(
+            "/webhook/job-launcher",
+            headers={"human-signature": "mocked signature"},
+            json=data,
+        )
+        print(response.json())
+        assert response.status_code == 400
+        assert response.json() == {"message": "Escrow doesn't have funds"}
+
+    # Invalid status
+    data = {
+        "escrow_address": "0xFE776895f6b00AA53969b20119a4777Ed920676a",
+        "chain_id": 80001,
+    }
+
+    with patch("src.modules.chain.escrow.get_web3") as mock_get_web3, patch(
+        "src.modules.chain.escrow.EscrowClient"
+    ) as mock_escrow_client:
+        mock_web3_instance = MagicMock()
+        mock_get_web3.return_value = mock_web3_instance
+
+        mock_escrow_instance = MagicMock()
+        mock_escrow_instance.get_balance.return_value = 100
+        mock_escrow_instance.get_status.return_value = Status.Complete
+        mock_escrow_client.return_value = mock_escrow_instance
+
+        response = client.post(
+            "/webhook/job-launcher",
+            headers={"human-signature": "mocked signature"},
+            json=data,
+        )
+        print(response.json())
+        assert response.status_code == 400
+        assert response.json() == {
+            "message": "Escrow is not in a Pending state. Current state: Complete"
+        }
