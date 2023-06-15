e0e573d3dc6e27e86a7b87a220397e9e6da17013
Eric Lee <eric@hmt.ai>
2023-06-15 13:12:58 -0400

    fix fortune reputation oracle unit test

--- packages/examples/fortune/reputation-oracle/src/services/escrow.test.ts
+++ packages/examples/fortune/reputation-oracle/src/services/escrow.test.ts
@@ -25,10 +25,10 @@ const owner = web3.eth.accounts.privateKeyToAccount(
 const launcher = web3.eth.accounts.privateKeyToAccount(
   '0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6'
 );
-const recordingAccount = web3.eth.accounts.privateKeyToAccount(
-  '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'
+const reputationAccount = web3.eth.accounts.privateKeyToAccount(
+  '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a'
 );
-web3.eth.defaultAccount = recordingAccount.address;
+web3.eth.defaultAccount = reputationAccount.address;
 
 describe('Fortune', () => {
   beforeAll(async () => {
