dcd0961451537b53b5235d930123a9b29a4f20ad
eugenvoronov <104138627+eugenvoronov@users.noreply.github.com>
2023-06-30 17:25:22 +0200

    Fix/migration scripts (#613)
    * Updated readme and package.json file
    
    * Added docker compose file
    
    * Added docker up commands to package.json file and readme file updated
    
    * Added env validation scheme
    
    * Updated typeorm config

--- packages/apps/job-launcher/server/src/app.module.ts
+++ packages/apps/job-launcher/server/src/app.module.ts
@@ -35,6 +35,7 @@ import { envValidator } from './common/config';
       envFilePath: process.env.NODE_ENV
         ? `.env.${process.env.NODE_ENV as string}`
         : '.env',
+      validationSchema: envValidator,
     }),
     DatabaseModule,
     HealthModule,
