5f179eb5bb66d51e8e0e616828389fa621a9c7e4
Francisco López <francislopez977@gmail.com>
2023-07-03 17:14:12 +0200

    Add python encryption module to SDK

---
+++ packages/sdk/python/human-protocol-sdk/human_protocol_sdk/encryption.py
@@ -0,0 +1,112 @@
+from pgpy import PGPKey, PGPMessage, PGPUID
+from pgpy.constants import (
+    SymmetricKeyAlgorithm,
+    HashAlgorithm,
+    KeyFlags,
+    PubKeyAlgorithm,
+)
+from pgpy.errors import PGPError
+
+
+class Encryption:
+    def __init__(self, private_key_armored, passphrase=None):
+        try:
+            self.private_key, _ = PGPKey.from_blob(private_key_armored)
+        except PGPError as e:
+            raise ValueError("Invalid private key: {}".format(str(e)))
+
+        if not self.private_key.is_unlocked:
+            if passphrase:
+                try:
+                    with self.private_key.unlock(passphrase):
+                        self.passphrase = passphrase
+
+                except PGPError as e:
+                    raise ValueError("Failed to unlock private key: {}".format(str(e)))
+            else:
+                raise ValueError("Private key locked. Passphrase needed")
+
+    def encrypt(self, message, public_keys):
+        pgp_message = PGPMessage.new(message)
+        if not self.private_key.is_unlocked:
+            try:
+                with self.private_key.unlock(self.passphrase):
+                    pgp_message |= self.private_key.sign(pgp_message)
+            except PGPError as e:
+                raise ValueError("Failed to unlock private key: {}".format(str(e)))
+        else:
+            pgp_message |= self.private_key.sign(pgp_message)
+
+        cipher = SymmetricKeyAlgorithm.AES256
+        sessionkey = cipher.gen_key()
+
+        for public_key in public_keys:
+            recipient_key, _ = PGPKey.from_blob(public_key)
+            pgp_message = recipient_key.encrypt(pgp_message, sessionkey)
+
+        del sessionkey
+        return pgp_message.__str__()
+
+    def decrypt(self, message, public_key=None):
+        pgp_message = PGPMessage.from_blob(message)
+        decrypted_message = ""
+        try:
+            if not self.private_key.is_unlocked:
+                try:
+                    with self.private_key.unlock(self.passphrase):
+                        decrypted_message = self.private_key.decrypt(pgp_message)
+                except PGPError as e:
+                    raise ValueError("Failed to unlock private key: {}".format(str(e)))
+            else:
+                decrypted_message = self.private_key.decrypt(pgp_message)
+            if public_key:
+                public_key, _ = PGPKey.from_blob(public_key)
+                public_key.verify(decrypted_message)
+
+            return decrypted_message.message.__str__()
+        except PGPError as e:
+            if (
+                decrypted_message
+                and len(decrypted_message.signatures) > 0
+                and str(e) == "No signatures to verify"
+            ):
+                raise ValueError(
+                    "Failed to decrypt message: Could not find signature with this public key"
+                )
+            raise ValueError("Failed to decrypt message: {}".format(str(e)))
+
+    def sign(self, message):
+        message = PGPMessage.new(message, cleartext=True)
+        if not self.private_key.is_unlocked:
+            try:
+                with self.private_key.unlock(self.passphrase):
+                    message |= self.private_key.sign(message)
+            except PGPError as e:
+                raise ValueError("Failed to unlock private key: {}".format(str(e)))
+        else:
+            message |= self.private_key.sign(message)
+        return message.__str__()
+
+
+class EncryptionUtils:
+    @staticmethod
+    def verify(message, public_key):
+        try:
+            signed_message = (
+                PGPMessage().from_blob(message) if isinstance(message, str) else message
+            )
+            public_key, _ = PGPKey.from_blob(public_key)
+            public_key.verify(signed_message)
+            return True
+        except PGPError as e:
+            return False
+
+    @staticmethod
+    def get_signed_data(message):
+        try:
+            signed_message = (
+                PGPMessage().from_blob(message) if isinstance(message, str) else message
+            )
+            return signed_message.message.__str__()
+        except PGPError as e:
+            return False
--- packages/sdk/python/human-protocol-sdk/test/human_protocol_sdk/utils/__init__.py
+++ packages/sdk/python/human-protocol-sdk/test/human_protocol_sdk/utils/__init__.py
@@ -1,1 +1,2 @@
+from .encryption import *
 from .job import DEFAULT_GAS_PAYER, DEFAULT_GAS_PAYER_PRIV
