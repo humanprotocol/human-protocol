15636c5f1bf6afe599d3e877ee8cc103c9550e74
m00n620 <50647994+m00n620@users.noreply.github.com>
2023-07-04 20:50:07 +0800

    Feat/admin api integration (#627)
    * integrate admin api
    
    * fix height
    
    * add loading

--- packages/apps/escrow-dashboard/src/vite-env.d.ts
+++ packages/apps/escrow-dashboard/src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_APP_NFT_STORAGE_API: string;
   readonly VITE_APP_FAUCET_SERVER_URL: string;
   readonly VITE_APP_WALLETCONNECT_PROJECT_ID: string;
+  readonly VITE_APP_ADMIN_API_URL: string;
+  readonly VITE_APP_BANNER_API_TOKEN: string;
 }
 
 interface ImportMeta {
