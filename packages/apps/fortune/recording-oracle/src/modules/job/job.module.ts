3037cc99a5ceaaec41c03a16f41c3eb3afa3aa48
CryptoRush <98655210+leric7@users.noreply.github.com>
2023-07-06 09:20:45 -0400

    update reputation oracle url for recording oracle (#642)

--- packages/apps/fortune/recording-oracle/src/modules/job/job.module.ts
+++ packages/apps/fortune/recording-oracle/src/modules/job/job.module.ts
@@ -2,13 +2,13 @@ import { HttpModule } from "@nestjs/axios";
 import { Logger, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
-import { storageConfig } from "@/common/config";
+import { serverConfig, storageConfig } from "@/common/config";
 import { JobController } from "./job.controller";
 import { JobService } from "./job.service";
 import { Web3Module } from "../web3/web3.module";
 
 @Module({
-  imports: [ConfigModule.forFeature(storageConfig), HttpModule, Web3Module],
+  imports: [ConfigModule.forFeature(storageConfig), ConfigModule.forFeature(serverConfig), HttpModule, Web3Module],
   controllers: [JobController],
   providers: [Logger, JobService],
   exports: [JobService],
