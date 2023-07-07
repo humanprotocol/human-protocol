5a0fe0a9bfa6191f96467643b232493bc5f71a54
m00n620 <50647994+m00n620@users.noreply.github.com>
2023-07-07 15:19:09 +0800

    fix solved tasks numbers for June (#647)

--- packages/apps/escrow-dashboard/src/components/SolvedTasks/SolvedTasksView.tsx
+++ packages/apps/escrow-dashboard/src/components/SolvedTasks/SolvedTasksView.tsx
@@ -10,21 +10,8 @@ import {
   XAxis,
 } from 'recharts';
 
+import SOLVED_TASKS from '../../history-data/sovled_tasks.json';
 import { CardContainer } from '../Cards';
-
-const SOLVED_TASKS = [
-  { date: '2022-07-31', value: 2181348 },
-  { date: '2022-08-31', value: 2537442 },
-  { date: '2022-09-30', value: 7014852 },
-  { date: '2022-10-31', value: 42615432 },
-  { date: '2022-11-30', value: 88708986 },
-  { date: '2022-12-31', value: 203793543 },
-  { date: '2023-01-31', value: 209093427 },
-  { date: '2023-02-28', value: 237012318 },
-  { date: '2023-03-31', value: 212012559 },
-  { date: '2023-04-30', value: 182462076 },
-  { date: '2023-05-31', value: 148126905 },
-];
 
 const CustomTooltip = ({ active, payload, label }: any) => {
   if (active && payload && payload.length) {
