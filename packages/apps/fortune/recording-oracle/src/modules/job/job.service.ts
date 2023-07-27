69129bf68f16dbec6c40c5154487d623dece9d9d
eugenvoronov <104138627+eugenvoronov@users.noreply.github.com>
2023-07-27 10:16:45 +0200

    [Recording Oracle] Job type validation (#730)
    * Added validation for only one solution by worker in fortune
    
    * Updated error mesages and implemented error job enum
    
    * Fixed lint errors
    
    * Added errors file

--- packages/apps/fortune/recording-oracle/src/modules/job/job.service.ts
+++ packages/apps/fortune/recording-oracle/src/modules/job/job.service.ts
@@ -1,14 +1,17 @@
 import { HttpService } from "@nestjs/axios";
-import { BadRequestException, ConflictException, Inject, Injectable } from "@nestjs/common";
+import { BadRequestException, ConflictException, Inject, Injectable, Logger } from "@nestjs/common";
 import { EscrowClient, EscrowStatus, StorageClient } from "@human-protocol/sdk";
 import { ethers } from "ethers";
 
 import { serverConfigKey, ServerConfigType, storageConfigKey, StorageConfigType } from "@/common/config";
-import { JobSolutionRequestDto } from "./job.dto";
+import { JobRequestType, JobSolutionRequestDto } from "./job.dto";
 import { Web3Service } from "../web3/web3.service";
+import { ErrorJob } from "../../common/constants/errors";
 
 @Injectable()
 export class JobService {
+  public readonly logger = new Logger(JobService.name);
+
   constructor(
     @Inject(storageConfigKey)
     private storageConfig: StorageConfigType,
@@ -27,25 +30,33 @@ export class JobService {
     const recordingOracleAddress = await escrowClient.getRecordingOracleAddress(jobSolution.escrowAddress);
 
     if (ethers.utils.getAddress(recordingOracleAddress) !== (await signer.getAddress())) {
-      throw new Error("Escrow Recording Oracle address mismatches the current one");
+      this.logger.log(ErrorJob.AddressMismatches, JobService.name);
+      throw new BadRequestException(ErrorJob.AddressMismatches);
     }
 
     // Validate if the escrow is in the correct state
     const escrowStatus = await escrowClient.getStatus(jobSolution.escrowAddress);
     if (escrowStatus !== EscrowStatus.Pending) {
-      throw new Error("Escrow is not in the Pending status");
+      this.logger.log(ErrorJob.InvalidStatus, JobService.name);
+      throw new BadRequestException(ErrorJob.InvalidStatus);
     }
 
     // Validate if the escrow has the correct manifest
     const manifestUrl = await escrowClient.getManifestUrl(jobSolution.escrowAddress);
-    const { submissionsRequired } = (await StorageClient.downloadFileFromUrl(manifestUrl)) as Record<
+    const { submissionsRequired, requestType } = (await StorageClient.downloadFileFromUrl(manifestUrl)) as Record<
       string,
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       any
     >;
 
-    if (!submissionsRequired) {
-      throw new BadRequestException("Manifest does not contain the required data");
+    if (!submissionsRequired && !requestType) {
+      this.logger.log(ErrorJob.InvalidManifest, JobService.name);
+      throw new BadRequestException(ErrorJob.InvalidManifest);
+    }
+
+    if (requestType !== JobRequestType.FORTUNE) {
+      this.logger.log(ErrorJob.InvalidJobType, JobService.name);
+      throw new BadRequestException(ErrorJob.InvalidJobType);
     }
 
     // Initialize Storage Client
@@ -66,12 +77,15 @@ export class JobService {
     try {
       existingJobSolutionsURL = await escrowClient.getIntermediateResultsUrl(jobSolution.escrowAddress);
     } catch (e) {
-      throw new BadRequestException("Error while getting intermediate results url from escrow contract");
+      console.log(e);
+      this.logger.log(ErrorJob.NotFoundIntermediateResultsUrl, JobService.name);
+      throw new BadRequestException(ErrorJob.NotFoundIntermediateResultsUrl);
     }
 
     const existingJobSolutions = await StorageClient.downloadFileFromUrl(existingJobSolutionsURL).catch(() => []);
     if (existingJobSolutions.find(({ solution }: { solution: string }) => solution === jobSolution.solution)) {
-      throw new ConflictException("Solution already exists");
+      this.logger.log(ErrorJob.SolutionAlreadyExists, JobService.name);
+      throw new BadRequestException(ErrorJob.SolutionAlreadyExists);
     }
 
     // Save new solution to S3
@@ -105,7 +119,8 @@ export class JobService {
 
       return "The requested job is completed.";
     } else if (newJobSolutions.length > submissionsRequired) {
-      throw new ConflictException("All solutions have already been sent.");
+      this.logger.log(ErrorJob.AllSolutionsHaveAlreadyBeenSent, JobService.name);
+      throw new ConflictException(ErrorJob.AllSolutionsHaveAlreadyBeenSent);
     }
 
     return "Solution is recorded.";
