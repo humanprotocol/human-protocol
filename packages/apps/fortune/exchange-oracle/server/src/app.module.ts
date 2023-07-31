8cda9995f9253423d11e29e8e1dfa19291b8a771
Eugene Voronov <voronovege@cy-tech.fr>
2023-07-31 12:49:35 +0300

    Added migrations and database module

--- packages/apps/fortune/exchange-oracle/server/src/app.module.ts
+++ packages/apps/fortune/exchange-oracle/server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { JobModule } from './modules/job/job.module';
 import { ConfigModule } from '@nestjs/config';
 import { envValidator } from './common/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { envValidator } from './common/config';
         : '.env',
       validationSchema: envValidator,
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
 })
