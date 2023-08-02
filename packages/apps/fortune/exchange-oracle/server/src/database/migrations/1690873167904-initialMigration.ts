e074e1e59e985595f86fe3bf83163ba75b001a22
Eugene Voronov <voronovege@cy-tech.fr>
2023-08-02 07:47:30 +0300

    Removed unused migrations

--- packages/apps/fortune/exchange-oracle/server/src/database/migrations/1690873167904-initialMigration.ts
+++ packages/apps/fortune/exchange-oracle/server/src/database/migrations/1690873167904-initialMigration.ts
@@ -1,9 +1,11 @@
+import { NS } from "src/common/constants";
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class InitialMigration1690873167904 implements MigrationInterface {
     name = 'InitialMigration1690873167904'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createSchema(NS, true);
         await queryRunner.query(`CREATE TYPE "hmt"."user_type_enum" AS ENUM('OPERATOR', 'REQUESTER')`);
         await queryRunner.query(`CREATE TYPE "hmt"."user_status_enum" AS ENUM('ACTIVE', 'INACTIVE', 'PENDING')`);
         await queryRunner.query(`CREATE TABLE "hmt"."user" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, "password" character varying NOT NULL, "email" character varying, "stripeCustomerId" character varying, "type" "hmt"."user_type_enum" NOT NULL, "status" "hmt"."user_status_enum" NOT NULL, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "UQ_0bfe583759eb0305b60117be840" UNIQUE ("stripeCustomerId"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
@@ -16,6 +18,7 @@ export class InitialMigration1690873167904 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropSchema(NS);
         await queryRunner.query(`ALTER TABLE "hmt"."auth" DROP CONSTRAINT "FK_373ead146f110f04dad60848154"`);
         await queryRunner.query(`ALTER TABLE "hmt"."token" DROP CONSTRAINT "FK_94f168faad896c0786646fa3d4a"`);
         await queryRunner.query(`DROP TABLE "hmt"."auth"`);
