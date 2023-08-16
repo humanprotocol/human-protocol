7b7fa21c5ec505b6ec79487a15d4e19a784069bd
Marius Hamacher <marius.hamacher@outlook.de>
2023-08-16 12:12:53 +0200

    remove pytest-resourcepath package.

--- packages/examples/cvat/recording-oracle/tests/unit/modules/webhook/conftest.py
+++ packages/examples/cvat/recording-oracle/tests/unit/modules/webhook/conftest.py
@@ -1,8 +1,11 @@
 import pytest
 import json
+from pathlib import Path
+
+resource_path_root = Path(__file__).parent.parent.parent.parent / "testresources"
 
 
 @pytest.fixture
-def intermediate_results(resource_path, resource_path_root):
+def intermediate_results():
     with open(resource_path_root / "recording_oracle_dummy_input.json") as f:
         return json.load(f)
