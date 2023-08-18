671042f845b9a1a5ed0ca34c675ff5a885b3379d
Marius Hamacher <marius.hamacher@outlook.de>
2023-08-18 12:08:46 +0200

    add validation to test.

--- packages/examples/cvat/recording-oracle/tests/unit/modules/webhook/test_process_intermediate_results.py
+++ packages/examples/cvat/recording-oracle/tests/unit/modules/webhook/test_process_intermediate_results.py
@@ -2,6 +2,9 @@ from src.modules.webhook.process_intermediate_results import (
     process_image_label_binary_intermediate_results,
 )
 
+from src.modules.webhook.api_schema import ImageLabelBinaryJobResults
+
 
 def test_process_image_label_binary_intermediate_results(intermediate_results):
     results = process_image_label_binary_intermediate_results(intermediate_results)
+    ImageLabelBinaryJobResults.validate(results)
