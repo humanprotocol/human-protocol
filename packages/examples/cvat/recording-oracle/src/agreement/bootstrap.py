14fd68e7826139235ebb52ee8a6b030f0abb8c87
Marius Hamacher <marius.hamacher@outlook.de>
2023-08-23 10:23:09 +0200

    add docstrings.

--- packages/examples/cvat/recording-oracle/src/agreement/bootstrap.py
+++ packages/examples/cvat/recording-oracle/src/agreement/bootstrap.py
@@ -1,25 +1,59 @@
 import numpy as np
 from typing import Sequence, Callable, Optional
 from pyerf import erf, erfinv
+from warnings import warn
 
 
 class NormalDistribution:
+    """Continuous Normal Distribution.
+
+    See: https://en.wikipedia.org/wiki/Normal_distribution
+    """
+
     def __init__(self, location: float = 0.0, scale: float = 1.0):
+        """Creates a NormalDistribution from the given parameters.
+        Args:
+            location: Location of the distribution.
+            scale: Scale of the distribution. Must be positive.
+        """
         if scale < 0.0:
             raise ValueError(f"scale parameter needs to be positive, but was {scale}")
 
         self.location = location
         self.scale = scale
 
-    def cdf(self, x: float):
+    def cdf(self, x: float) -> float:
+        """Cumulative Distribution Function of the Normal Distribution. Returns
+        the probability that a random sample will be less than the given
+        point.
+
+        Args:
+            x: Point within the distribution's domain.
+        """
         return (1 + erf((x - self.location) / (self.scale * 2**0.5))) / 2
 
-    def pdf(self, x: float):
+    def pdf(self, x: float) -> float:
+        """Probability Density Function of the Normal Distribution. Returns the
+        probability for observing the given sample in the distribution.
+
+        Args:
+            x: Point within the distribution's domain.
+        """
         return np.exp(-0.5 * (x - self.location / self.scale) ** 2) / (
             self.scale * (2 * np.pi) ** 0.5
         )
 
-    def ppf(self, p: float):
+    def ppf(self, p: float) -> float:
+        """Probability Point function of the Normal Distribution. Returns
+        the maximum point to which cumulated probabilities equal the given
+        probability. Also called quantile. Inverse of the cdf.
+
+        Args:
+              p: Percentile of the distribution to be covered by the ppf.
+        """
+        if not (0.0 <= p <= 1.0):
+            raise ValueError(f"p must be a float within [0.0, 1.0], but was {p}")
+
         return self.location + self.scale * 2**0.5 * erfinv(2 * p - 1.0)
 
 
@@ -31,12 +65,43 @@ def bootstrap_ci(
     ci=0.95,
     algorithm="bca",
 ) -> tuple:
+    """Returns the confidence interval for the boostrap estimate of the given
+    statistic.
+
+    Args:
+        data: Data to estimate the statistic.
+        statistic_fn: Function to calculate the statistic. `f(data)` must
+            return the statistic.
+        n_iterations: Number of bootstrap samples to use for the estimate.
+        n_sample: If provided, determines the size of each bootstrap sample
+            drawn from the data. If omitted, is equal to the length of the
+            data.
+        ci: Size of the confidence interval.
+        algorithm: Which algorithm to use for the confidence interval
+            estimation. "bca" uses the "Bias Corrected Bootstrap with
+            Acceleration", "percentile" simply takes the appropriate
+            percentiles from the bootstrap distribution.
+    """
     data = np.asarray(data)
 
+    if n_iterations < 1:
+        raise ValueError(
+            f"n_iterations must be a positive integer, but were {n_iterations}"
+        )
+
     n_data = len(data)
+    if n_data < 30:
+        warn(
+            "Dataset size is low, bootstrap estimate might be inaccurate. For accurate results, make sure to provide at least 30 data points."
+        )
 
     if n_sample is None:
         n_sample = n_data
+    elif n_sample < 1:
+        raise ValueError(f"n_sample must be a positive integer, but was {n_sample}")
+
+    if not (0.0 <= ci <= 1.0):
+        raise ValueError(f"ci must be a float within [0.0, 1.0], but was {ci}")
 
     # bootstrap estimates
     theta_b = np.empty(n_iterations, dtype=float)
@@ -60,8 +125,7 @@ def bootstrap_ci(
                     theta_hat - statistic_fn(data[jn_idxs[i]])
                 )
 
-            a_hat = (np.sum(theta_jn**3) / np.sum(theta_jn**2, axis=-1) ** 1.5) / 6
-            print(a_hat)
+            a = (np.sum(theta_jn**3) / np.sum(theta_jn**2, axis=-1) ** 1.5) / 6
 
             alpha = 1.0 - ci
             alpha /= 2
@@ -76,8 +140,7 @@ def bootstrap_ci(
             z_u = ppf(q)
             z_diff = z_0 + z_u
 
-            q = cdf(z_0 + (z_diff / (1 - a_hat * z_diff)))
-            print(q)
+            q = cdf(z_0 + (z_diff / (1 - a * z_diff)))
         case _:
             raise ValueError(f"Algorithm '{algorithm}' is not available!")
 
