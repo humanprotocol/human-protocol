4a133f2e3409b42c1fc66ee36acd614f879532f2
eugenvoronov <104138627+eugenvoronov@users.noreply.github.com>
2023-08-28 14:39:56 +0300

    [Job Launcher] Implemented signature verification mechanism (#800)
    * Added signature unility and unit tests
    
    * Made signature algorithm compatable with python version
    
    * Updated unit tests
    
    * Updated constants in unit tests
    
    * Added object compatibility
    
    * Added unit test cases

---
+++ packages/apps/job-launcher/server/src/common/utils/signature.ts
@@ -0,0 +1,39 @@
+import { ConflictException } from "@nestjs/common";
+import { ethers } from "ethers";
+import { ErrorSignature } from "../constants/errors";
+import { toUtf8Bytes } from "ethers/lib/utils";
+
+export function verifySignature(
+  message: object | string,
+  signature: string,
+  address: string,
+): boolean {
+  const signer = recoverSigner(message, signature);
+
+  if (signer.toLowerCase() !== address.toLowerCase()) {
+    throw new ConflictException(
+      ErrorSignature.SignatureNotVerified);
+  }
+
+  return true;
+}
+
+export async function signMessage(message: object | string, privateKey: string): Promise<string> {
+  if (typeof(message) !== "string") { message = JSON.stringify(message); }
+  
+  const wallet = new ethers.Wallet(privateKey);
+  const signature = await wallet.signMessage(message);
+
+  return signature;
+}
+
+export function recoverSigner(message: object | string, signature: string): string {  
+  if (typeof(message) !== "string") { message = JSON.stringify(message); }
+
+  try {  
+    return ethers.utils.verifyMessage(message, signature);
+  } catch (e) {
+    throw new ConflictException(
+      ErrorSignature.InvalidSignature);
+  }
+}
