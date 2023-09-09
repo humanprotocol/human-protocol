7ca7a39f6c7d22226735b7e6a376892d5007e5a4
Eugene Voronov <voronovege@cy-tech.fr>
2023-09-09 12:19:50 +0300

    Updated migrations

--- packages/apps/reputation-oracle/server/src/database/migrations/1678011588667-addWebhookIncomingTable.ts
+++ packages/apps/reputation-oracle/server/src/database/migrations/1678011588667-addWebhookIncomingTable.ts
@@ -27,6 +27,11 @@ export class addWebhookIncomingTable1678011588667
           type: 'int',
         },
         {
+          name: 'oracle_address',
+          type: 'varchar',
+          isNullable: true,
+        },
+        {
           name: 'escrow_address',
           type: 'varchar',
         },
