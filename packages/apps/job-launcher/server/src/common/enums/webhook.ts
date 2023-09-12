29bf58478475ce9e14d9d69c0a8cb4d35cef35e5
Eugene Voronov <voronovege@cy-tech.fr>
2023-09-12 14:36:19 +0300

    Updated eventType

--- packages/apps/job-launcher/server/src/common/enums/webhook.ts
+++ packages/apps/job-launcher/server/src/common/enums/webhook.ts
@@ -1,7 +1,10 @@
 export enum EventType {
   ESCROW_CREATED = 'escrow_created',
   ESCROW_CANCELED = 'escrow_canceled',
+  TASK_REJECTED = 'task_rejected',
   TASK_CREATION_FAILED = 'task_creation_failed',
+  TASK_COMPLETED = 'task_completed',
+  TASK_FINISHED = 'task_finished'
 }
 
 export enum OracleType {
