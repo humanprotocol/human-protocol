457828d6b5c29b743c8a0657fe4a262f8a6ff895
Eugene Voronov <voronovege@cy-tech.fr>
2023-09-14 13:01:01 +0300

    Added oracle type parameter to swagger

--- packages/apps/reputation-oracle/server/src/modules/webhook/webhook.controller.ts
+++ packages/apps/reputation-oracle/server/src/modules/webhook/webhook.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Headers, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Param, Post, Request, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Public } from '../../common/decorators';
 import { WebhookService } from './webhook.service';
 import { WebhookIncomingDto } from './webhook.dto';
 import { SignatureAuthGuard } from 'src/common/guards';
 import { HEADER_SIGNATURE_KEY } from 'src/common/constants';
+import { OracleType } from 'src/common/enums';
 
 @Public()
 @ApiTags('Webhook')
@@ -17,6 +18,7 @@ export class WebhookController {
   @Post('/:oracleType')
   public async createIncomingWebhook(
     @Headers(HEADER_SIGNATURE_KEY) _: string,
+    @Param('oracleType') oracleType: OracleType,
     @Request() req: any,
     @Body() data: WebhookIncomingDto,
   ): Promise<boolean> {
