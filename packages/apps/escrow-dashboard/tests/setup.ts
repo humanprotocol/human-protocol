4e999f161694257220620285aeab44f07c3eeaf5
Eric Lee <eric@hmt.ai>
2023-09-19 00:29:16 -0400

    fix vitest config

--- packages/apps/escrow-dashboard/tests/setup.ts
+++ packages/apps/escrow-dashboard/tests/setup.ts
@@ -1,4 +1,4 @@
-import matchers from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 import { expect, afterEach } from 'vitest';
 
