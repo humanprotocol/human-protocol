cf3f6df4a18a6a293672d60bd25d9b4767a3f933
Marius Hamacher <marius.hamacher@outlook.de>
2023-09-25 12:02:18 +0200

    add data annotations example.

--- packages/sdk/python/human-protocol-sdk/example.py
+++ packages/sdk/python/human-protocol-sdk/example.py
@@ -1,8 +1,11 @@
 import datetime
+import json
 from web3 import Web3
 
 from human_protocol_sdk.escrow import EscrowClient, EscrowFilter, Status
 from human_protocol_sdk.statistics import StatisticsClient, StatisticsParam
+from human_protocol_sdk.storage import StorageClient
+from human_protocol_sdk.agreement import agreement
 
 if __name__ == "__main__":
     alchemy_url = (
@@ -62,3 +65,20 @@ if __name__ == "__main__":
             )
         )
     )
+
+    # process annotation data and get quality estimates
+    url = "https://raw.githubusercontent.com/humanprotocol/human-protocol/efa8d3789ac35915b42435011cd0a8d36507564c/packages/sdk/python/human-protocol-sdk/example_annotations.json"
+    annotations = json.loads(StorageClient.download_file_from_url(url))
+    print(annotations)
+
+    report = agreement(
+        data=annotations["data"],
+        data_format=annotations["data_format"],
+        labels=annotations["labels"],
+        nan_values=annotations["nan_values"],
+        measure="fleiss_kappa",
+        bootstrap_method="bca",
+        bootstrap_kwargs={"seed": 42},
+    )
+    print(report["results"])
+    print(report["config"])
