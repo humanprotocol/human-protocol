73c78dcb0f0153217040b1231c59d363e66454cd
Eugene Voronov <voronovege@cy-tech.fr>
2023-10-03 14:27:27 +0300

    Updated migration

--- packages/apps/job-launcher/server/src/database/migrations/1696331060184-AddFailedReasonToJobs.ts
+++ packages/apps/job-launcher/server/src/database/migrations/1696331060184-AddFailedReasonToJobs.ts
@@ -5,14 +5,14 @@ export class AddFailedReasonToJobs1696331060184 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             ALTER TABLE "hmt"."jobs"
-            ADD COLUMN failedReason character varying;
+            ADD COLUMN failed_reason character varying;
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             ALTER TABLE "hmt"."jobs"
-            DROP COLUMN failedReason;
+            DROP COLUMN failed_reason;
         `);
     }
 
