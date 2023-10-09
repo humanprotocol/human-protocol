c423414f9a6b3b11bb306f9ee4c50179833b69bf
CryptoRush <98655210+leric7@users.noreply.github.com>
2023-10-09 09:38:37 -0400

    [SDK] fix: remove IM integration from sdk and fix worker statistics on subgraph (#1010)
    * remove IM integration from sdk and fix worker statistics on subgraph
    
    * remove temporary comments
    
    * add class to python statistics
    
    * fix datetime import
    
    * fix python sdk test failing

--- packages/sdk/python/human-protocol-sdk/human_protocol_sdk/statistics.py
+++ packages/sdk/python/human-protocol-sdk/human_protocol_sdk/statistics.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
-import datetime
+from datetime import datetime
 import logging
 import os
 
-from typing import Optional
+from typing import List, Optional
 
-import requests
 from web3 import Web3
 
 from human_protocol_sdk.constants import ChainId, NETWORKS
-from human_protocol_sdk.filter import PayoutFilter
 from human_protocol_sdk.gql.hmtoken import get_holders_query
 
 from human_protocol_sdk.utils import get_data_from_subgraph
@@ -35,16 +33,16 @@ class StatisticsParam:
 
     def __init__(
         self,
-        date_from: Optional[datetime.datetime] = None,
-        date_to: Optional[datetime.datetime] = None,
+        date_from: Optional[datetime] = None,
+        date_to: Optional[datetime] = None,
         limit: Optional[int] = None,
     ):
         """
         Initializes a StatisticsParam instance.
 
         Args:
-            date_from (Optional[datetime.datetime]): Statistical data from date
-            date_to (Optional[datetime.datetime]): Statistical data to date
+            date_from (Optional[datetime]): Statistical data from date
+            date_to (Optional[datetime]): Statistical data to date
             limit (Optional[int]): Limit of statistical data
         """
 
@@ -53,13 +51,235 @@ class StatisticsParam:
         self.limit = limit
 
 
+class DailyEscrowData:
+    """
+    A class used to specify daily escrow data.
+    """
+
+    def __init__(
+        self,
+        timestamp: datetime,
+        escrows_total: int,
+        escrows_pending: int,
+        escrows_solved: int,
+        escrows_paid: int,
+        escrows_cancelled: int,
+    ):
+        """
+        Initializes a DailyEscrowData instance.
+
+        Args:
+            timestamp (datetime): Timestamp
+            escrows_total (int): Total escrows
+            escrows_pending (int): Pending escrows
+            escrows_solved (int): Solved escrows
+            escrows_paid (int): Paid escrows
+            escrows_cancelled (int): Cancelled escrows
+        """
+
+        self.timestamp = timestamp
+        self.escrows_total = escrows_total
+        self.escrows_pending = escrows_pending
+        self.escrows_solved = escrows_solved
+        self.escrows_paid = escrows_paid
+        self.escrows_cancelled = escrows_cancelled
+
+
+class EscrowStatistics:
+    """
+    A class used to specify escrow statistics.
+    """
+
+    def __init__(
+        self,
+        total_escrows: int,
+        daily_escrows_data: List[DailyEscrowData],
+    ):
+        """
+        Initializes a EscrowStatistics instance.
+
+        Args:
+            total_escrows (int): Total escrows
+            daily_escrows_data (List[DailyEscrowData]): Daily escrows data
+        """
+
+        self.total_escrows = total_escrows
+        self.daily_escrows_data = daily_escrows_data
+
+
+class DailyWorkerData:
+    """
+    A class used to specify daily worker data.
+    """
+
+    def __init__(
+        self,
+        timestamp: datetime,
+        active_workers: int,
+    ):
+        """
+        Initializes a DailyWorkerData instance.
+
+        Args:
+            timestamp (datetime): Timestamp
+            active_workers (int): Active workers
+        """
+
+        self.timestamp = timestamp
+        self.active_workers = active_workers
+
+
+class WorkerStatistics:
+    """
+    A class used to specify worker statistics.
+    """
+
+    def __init__(
+        self,
+        daily_workers_data: List[DailyWorkerData],
+    ):
+        """
+        Initializes a WorkerStatistics instance.
+
+        Args:
+            daily_workers_data (List[DailyWorkerData]): Daily workers data
+        """
+
+        self.daily_workers_data = daily_workers_data
+
+
+class DailyPaymentData:
+    """
+    A class used to specify daily payment data.
+    """
+
+    def __init__(
+        self,
+        timestamp: datetime,
+        total_amount_paid: int,
+        total_count: int,
+        average_amount_per_worker: int,
+    ):
+        """
+        Initializes a DailyPaymentData instance.
+
+        Args:
+            timestamp (datetime): Timestamp
+            total_amount_paid (int): Total amount paid
+            total_count (int): Total count
+            average_amount_per_worker (int): Average amount per worker
+        """
+
+        self.timestamp = timestamp
+        self.total_amount_paid = total_amount_paid
+        self.total_count = total_count
+        self.average_amount_per_worker = average_amount_per_worker
+
+
+class PaymentStatistics:
+    """
+    A class used to specify payment statistics.
+    """
+
+    def __init__(
+        self,
+        daily_payments_data: List[DailyPaymentData],
+    ):
+        """
+        Initializes a PaymentStatistics instance.
+
+        Args:
+            daily_payments_data (List[DailyPaymentData]): Daily payments data
+        """
+
+        self.daily_payments_data = daily_payments_data
+
+
+class HMTHolder:
+    """
+    A class used to specify HMT holder.
+    """
+
+    def __init__(
+        self,
+        address: str,
+        balance: int,
+    ):
+        """
+        Initializes a HMTHolder instance.
+
+        Args:
+            address (str): Address
+            balance (int): Balance
+        """
+
+        self.address = address
+        self.balance = balance
+
+
+class DailyHMTData:
+    """
+    A class used to specify daily HMT data.
+    """
+
+    def __init__(
+        self,
+        timestamp: datetime,
+        total_transaction_amount: int,
+        total_transaction_count: int,
+    ):
+        """
+        Initializes a DailyHMTData instance.
+
+        Args:
+            timestamp (datetime): Timestamp
+            total_transaction_amount (int): Total transaction amount
+            total_transaction_count (int): Total transaction count
+        """
+
+        self.timestamp = timestamp
+        self.total_transaction_amount = total_transaction_amount
+        self.total_transaction_count = total_transaction_count
+
+
+class HMTStatistics:
+    """
+    A class used to specify HMT statistics.
+    """
+
+    def __init__(
+        self,
+        total_transfer_amount: int,
+        total_transfer_count: int,
+        total_holders: int,
+        holders: List[HMTHolder],
+        daily_hmt_data: List[DailyHMTData],
+    ):
+        """
+        Initializes a HMTStatistics instance.
+
+        Args:
+            total_transfer_amount (int): Total transfer amount
+            total_transfer_count (int): Total transfer count
+            total_holders (int): Total holders
+            holders (List[HMTHolder]): Holders
+            daily_hmt_data (List[DailyHMTData]): Daily HMT data
+        """
+
+        self.total_transfer_amount = total_transfer_amount
+        self.total_transfer_count = total_transfer_count
+        self.total_holders = total_holders
+        self.holders = holders
+        self.daily_hmt_data = daily_hmt_data
+
+
 class StatisticsClient:
     """
     A client used to get statistical data.
     """
 
-    def __init__(self, w3: Web3, im_api_key: str):
-        """Initializes a Staking instance
+    def __init__(self, w3: Web3):
+        """Initializes a Statistics instance
 
         Args:
 
@@ -82,97 +302,9 @@ class StatisticsClient:
         if not self.network:
             raise StatisticsClientError("Empty network configuration")
 
-        # Keep IM API key
-        self.im_api_key = im_api_key
-
-    def get_data_from_im_api(self, param: StatisticsParam):
-        # TODO: Remove this when IM API supports other networks
-        if self.network["name"] != "Polygon":
-            return {}
-
-        if param.date_from:
-            date_from = datetime.datetime.strftime(param.date_from, "%Y-%m-%d")
-        else:
-            date_from = (
-                datetime.datetime.now() - datetime.timedelta(days=60)
-            ).strftime("%Y-%m-%d")
-
-        if param.date_to:
-            date_to = datetime.datetime.strftime(param.date_to, "%Y-%m-%d")
-        else:
-            date_to = datetime.datetime.now().strftime("%Y-%m-%d")
-
-        # IM API now limits the date range to 60 days, so we need to make multiple requests
-        # if the date range is greater than 60 days
-        date_from = datetime.datetime.strptime(date_from, "%Y-%m-%d")
-        date_to = datetime.datetime.strptime(date_to, "%Y-%m-%d")
-        date_range = date_to - date_from
-        date_chunks = []
-        chunk_size = 60
-        for i in range(0, date_range.days, chunk_size):
-            date_chunks.append(
-                {
-                    "date_from": (date_from + datetime.timedelta(days=i)).strftime(
-                        "%Y-%m-%d"
-                    ),
-                    "date_to": (
-                        date_from + datetime.timedelta(days=i + chunk_size)
-                    ).strftime("%Y-%m-%d"),
-                }
-            )
-        if date_range.days % chunk_size != 0:
-            date_chunks.append(
-                {
-                    "date_from": (
-                        date_from
-                        + datetime.timedelta(
-                            days=date_range.days - (date_range.days % chunk_size)
-                        )
-                    ).strftime("%Y-%m-%d"),
-                    "date_to": date_to.strftime("%Y-%m-%d"),
-                }
-            )
-
-        im_data = {}
-        for date_chunk in date_chunks:
-            data = requests.get(
-                "https://foundation-accounts.hmt.ai//support/summary-stats",
-                params={
-                    "start_date": date_chunk["date_from"],
-                    "end_date": date_chunk["date_to"],
-                    "api_key": self.im_api_key,
-                },
-            ).json()
-
-            for key in data:
-                if key != "total":
-                    im_data[key] = data[key]
-
-        return im_data
-
-    def get_task_statistics(self, param: StatisticsParam = StatisticsParam()) -> dict:
-        """Get task statistics data for the given date range.
-
-        Args:
-            param (StatisticsParam): Object containing the date range
-
-        Returns:
-            dict: Task statistics data
-        """
-        im_data = self.get_data_from_im_api(param)
-
-        return {
-            "daily_tasks_data": [
-                {
-                    "timestamp": datetime.datetime.strptime(key, "%Y-%m-%d"),
-                    "tasks_total": im_data[key]["served"],
-                    "tasks_solved": im_data[key]["solved"],
-                }
-                for key in im_data
-            ],
-        }
-
-    def get_escrow_statistics(self, param: StatisticsParam = StatisticsParam()) -> dict:
+    def get_escrow_statistics(
+        self, param: StatisticsParam = StatisticsParam()
+    ) -> EscrowStatistics:
         """Get escrow statistics data for the given date range.
 
         Args:
@@ -202,30 +334,28 @@ class StatisticsClient:
         )
         event_day_datas = event_day_datas_data["data"]["eventDayDatas"]
 
-        return {
-            "total_escrows": int(escrow_statistics["totalEscrowCount"]),
-            "daily_escrows_data": [
-                {
-                    "timestamp": datetime.datetime.fromtimestamp(
-                        int(event_day_data["timestamp"])
-                    ),
-                    "escrows_total": int(event_day_data["dailyEscrowCount"]),
-                    "escrows_pending": int(
-                        event_day_data["dailyPendingStatusEventCount"]
-                    ),
-                    "escrows_solved": int(
+        return EscrowStatistics(
+            total_escrows=int(escrow_statistics["totalEscrowCount"]),
+            daily_escrows_data=[
+                DailyEscrowData(
+                    timestamp=datetime.fromtimestamp(int(event_day_data["timestamp"])),
+                    escrows_total=int(event_day_data["dailyEscrowCount"]),
+                    escrows_pending=int(event_day_data["dailyPendingStatusEventCount"]),
+                    escrows_solved=int(
                         event_day_data["dailyCompletedStatusEventCount"]
                     ),
-                    "escrows_paid": int(event_day_data["dailyPaidStatusEventCount"]),
-                    "escrows_cancelled": int(
+                    escrows_paid=int(event_day_data["dailyPaidStatusEventCount"]),
+                    escrows_cancelled=int(
                         event_day_data["dailyCancelledStatusEventCount"]
                     ),
-                }
+                )
                 for event_day_data in event_day_datas
             ],
-        }
+        )
 
-    def get_worker_statistics(self, param: StatisticsParam = StatisticsParam()) -> dict:
+    def get_worker_statistics(
+        self, param: StatisticsParam = StatisticsParam()
+    ) -> WorkerStatistics:
         """Get worker statistics data for the given date range.
 
         Args:
@@ -234,47 +364,33 @@ class StatisticsClient:
         Returns:
             dict: Worker statistics data
         """
+        from human_protocol_sdk.gql.statistics import (
+            get_event_day_data_query,
+        )
 
-        from human_protocol_sdk.gql.payout import get_payouts_query
+        event_day_datas_data = get_data_from_subgraph(
+            self.network["subgraph_url"],
+            query=get_event_day_data_query(param),
+            params={
+                "from": int(param.date_from.timestamp()) if param.date_from else None,
+                "to": int(param.date_to.timestamp()) if param.date_to else None,
+            },
+        )
+        event_day_datas = event_day_datas_data["data"]["eventDayDatas"]
 
-        im_data = self.get_data_from_im_api(param)
-
-        daily_workers_data = []
-        for key in im_data:
-            date_from = datetime.datetime.strptime(key, "%Y-%m-%d")
-            date_to = date_from + datetime.timedelta(days=1)
-
-            payouts_data = get_data_from_subgraph(
-                self.network["subgraph_url"],
-                query=get_payouts_query(
-                    PayoutFilter(date_from=date_from, date_to=date_to)
-                ),
-                params={
-                    "from": int(date_from.timestamp()),
-                    "to": int(date_to.timestamp()),
-                },
-            )
-            payouts = payouts_data["data"]["payouts"]
-
-            active_workers = len(set([payout["recipient"] for payout in payouts]))
-
-            daily_workers_data.append(
-                {
-                    "timestamp": date_from,
-                    "active_workers": active_workers,
-                    "average_jobs_solved": im_data[key]["solved"] / active_workers
-                    if active_workers != 0
-                    else 0,
-                }
-            )
-
-        return {
-            "daily_workers_data": daily_workers_data,
-        }
+        return WorkerStatistics(
+            daily_workers_data=[
+                DailyWorkerData(
+                    timestamp=datetime.fromtimestamp(int(event_day_data["timestamp"])),
+                    active_workers=int(event_day_data["dailyWorkerCount"]),
+                )
+                for event_day_data in event_day_datas
+            ],
+        )
 
     def get_payment_statistics(
         self, param: StatisticsParam = StatisticsParam()
-    ) -> dict:
+    ) -> PaymentStatistics:
         """Get payment statistics data for the given date range.
 
         Args:
@@ -299,30 +415,24 @@ class StatisticsClient:
         )
         event_day_datas = event_day_datas_data["data"]["eventDayDatas"]
 
-        return {
-            "daily_payments_data": [
-                {
-                    "timestamp": datetime.datetime.fromtimestamp(
-                        int(event_day_data["timestamp"])
-                    ),
-                    "total_amount_paid": int(event_day_data["dailyPayoutAmount"]),
-                    "total_count": int(event_day_data["dailyPayoutCount"]),
-                    "average_amount_per_job": int(event_day_data["dailyPayoutAmount"])
-                    / int(event_day_data["dailyBulkPayoutEventCount"])
-                    if event_day_data["dailyBulkPayoutEventCount"] != "0"
-                    else 0,
-                    "average_amount_per_worker": int(
-                        event_day_data["dailyPayoutAmount"]
-                    )
+        return PaymentStatistics(
+            daily_payments_data=[
+                DailyPaymentData(
+                    timestamp=datetime.fromtimestamp(int(event_day_data["timestamp"])),
+                    total_amount_paid=int(event_day_data["dailyPayoutAmount"]),
+                    total_count=int(event_day_data["dailyPayoutCount"]),
+                    average_amount_per_worker=int(event_day_data["dailyPayoutAmount"])
                     / int(event_day_data["dailyWorkerCount"])
                     if event_day_data["dailyWorkerCount"] != "0"
                     else 0,
-                }
+                )
                 for event_day_data in event_day_datas
             ],
-        }
+        )
 
-    def get_hmt_statistics(self, param: StatisticsParam = StatisticsParam()) -> dict:
+    def get_hmt_statistics(
+        self, param: StatisticsParam = StatisticsParam()
+    ) -> HMTStatistics:
         """Get HMT statistics data for the given date range.
 
         Args:
@@ -358,29 +468,27 @@ class StatisticsClient:
         )
         event_day_datas = event_day_datas_data["data"]["eventDayDatas"]
 
-        return {
-            "total_transfer_amount": int(hmtoken_statistics["totalValueTransfered"]),
-            "total_transfer_count": int(hmtoken_statistics["totalTransferEventCount"]),
-            "total_holders": int(hmtoken_statistics["holders"]),
-            "holders": [
-                {
-                    "address": holder["address"],
-                    "balance": int(holder["balance"]),
-                }
+        return HMTStatistics(
+            total_transfer_amount=int(hmtoken_statistics["totalValueTransfered"]),
+            total_transfer_count=int(hmtoken_statistics["totalTransferEventCount"]),
+            total_holders=int(hmtoken_statistics["holders"]),
+            holders=[
+                HMTHolder(
+                    address=holder["address"],
+                    balance=int(holder["balance"]),
+                )
                 for holder in holders
             ],
-            "daily_hmt_data": [
-                {
-                    "timestamp": datetime.datetime.fromtimestamp(
-                        int(event_day_data["timestamp"])
-                    ),
-                    "total_transaction_amount": int(
+            daily_hmt_data=[
+                DailyHMTData(
+                    timestamp=datetime.fromtimestamp(int(event_day_data["timestamp"])),
+                    total_transaction_amount=int(
                         event_day_data["dailyHMTTransferAmount"]
                     ),
-                    "total_transaction_count": int(
+                    total_transaction_count=int(
                         event_day_data["dailyHMTTransferCount"]
                     ),
-                }
+                )
                 for event_day_data in event_day_datas
             ],
-        }
+        )
