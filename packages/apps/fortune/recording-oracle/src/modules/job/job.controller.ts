ba3e889fe87e50964ae489f6a886e90aebc3b7e2
Francisco López <francislopez977@gmail.com>
2023-10-13 10:43:35 +0200

    Add header to controller params

--- packages/apps/fortune/recording-oracle/src/modules/job/job.controller.ts
+++ packages/apps/fortune/recording-oracle/src/modules/job/job.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { JobSolutionsRequestDto } from './job.dto';
 import { JobService } from './job.service';
 import { SignatureAuthGuard } from '../../common/guards';
 import { Role } from '../../common/enums/role';
+import { HEADER_SIGNATURE_KEY } from '../../common/constants';
 
 @Controller('/job')
 @ApiTags('Job')
@@ -13,7 +14,10 @@ export class JobController {
 
   @UseGuards(new SignatureAuthGuard([Role.Exchange]))
   @Post('/solve')
-  public async solve(@Body() fortune: JobSolutionsRequestDto): Promise<string> {
+  public async solve(
+    @Headers(HEADER_SIGNATURE_KEY) _: string,
+    @Body() fortune: JobSolutionsRequestDto,
+  ): Promise<string> {
     return await this.jobService.processJobSolution(fortune);
   }
 }
