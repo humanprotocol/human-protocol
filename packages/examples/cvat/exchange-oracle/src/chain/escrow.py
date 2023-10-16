c1ec859937f5853a32045f7f95a9e5e49a9f6a79
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2023-10-16 11:44:48 +0300

    Update exchange oracle to use newer humanprotocol sdk

--- packages/examples/cvat/exchange-oracle/src/chain/escrow.py
+++ packages/examples/cvat/exchange-oracle/src/chain/escrow.py
@@ -2,10 +2,8 @@ import json
 from typing import List
 
 from human_protocol_sdk.constants import Status
-from human_protocol_sdk.escrow import EscrowClient
+from human_protocol_sdk.escrow import EscrowUtils
 from human_protocol_sdk.storage import StorageClient
-
-from src.chain.web3 import get_web3
 
 
 def validate_escrow(
@@ -17,44 +15,29 @@ def validate_escrow(
 ) -> None:
     assert accepted_states
 
-    web3 = get_web3(chain_id)
-    escrow_client = EscrowClient(web3)
+    escrow = EscrowUtils.get_escrow(chain_id, escrow_address)
 
-    escrow_status = escrow_client.get_status(escrow_address)
-    if escrow_status not in accepted_states:
+    if escrow.status not in accepted_states:
         raise ValueError(
             "Escrow is not in any of the accepted states ({}). Current state: {}".format(
-                ", ".join(s.name for s in accepted_states), escrow_status.name
+                ", ".join(s.name for s in accepted_states), escrow.status.name
             )
         )
 
-    if escrow_status == Status.Pending and not allow_no_funds:
-        if escrow_client.get_balance(escrow_address) == 0:
+    if escrow.status == Status.Pending and not allow_no_funds:
+        if escrow.balance == 0:
             raise ValueError("Escrow doesn't have funds")
 
 
 def get_escrow_manifest(chain_id: int, escrow_address: str) -> dict:
-    web3 = get_web3(chain_id)
-    escrow_client = EscrowClient(web3)
-
-    manifest_url = escrow_client.get_manifest_url(escrow_address)
-
-    return json.loads((StorageClient.download_file_from_url(manifest_url)).decode("utf-8"))
+    escrow = EscrowUtils.get_escrow(chain_id, escrow_address)
+    manifest_content = StorageClient.download_file_from_url(escrow.manifestUrl)
+    return json.loads(manifest_content.decode("utf-8"))
 
 
 def get_job_launcher_address(chain_id: int, escrow_address: str) -> str:
-    web3 = get_web3(chain_id)
-    escrow_client = EscrowClient(web3)
-
-    job_launcher_address = escrow_client.get_job_launcher_address(escrow_address)
-
-    return job_launcher_address
+    return EscrowUtils.get_escrow(chain_id, escrow_address).launcher
 
 
 def get_recording_oracle_address(chain_id: int, escrow_address: str) -> str:
-    web3 = get_web3(chain_id)
-    escrow_client = EscrowClient(web3)
-
-    recording_oracle_address = escrow_client.get_recording_oracle_address(escrow_address)
-
-    return recording_oracle_address
+    return EscrowUtils.get_escrow(chain_id, escrow_address).recordingOracle
--- packages/examples/cvat/exchange-oracle/src/chain/kvstore.py
+++ packages/examples/cvat/exchange-oracle/src/chain/kvstore.py
@@ -1,5 +1,5 @@
-from human_protocol_sdk.escrow import EscrowClient
-from human_protocol_sdk.kvstore import KVStoreClient
+from human_protocol_sdk.escrow import EscrowUtils
+from human_protocol_sdk.staking import StakingClient
 
 from src.chain.web3 import get_web3
 from src.core.config import Config
@@ -9,25 +9,19 @@ def get_recording_oracle_url(chain_id: int, escrow_address: str) -> str:
     if url := Config.localhost.recording_oracle_url:
         return url
 
+    escrow = EscrowUtils.get_escrow(chain_id, escrow_address)
+
     web3 = get_web3(chain_id)
-    escrow_client = EscrowClient(web3)
-    kvstore_client = KVStoreClient(web3)
-
-    recording_oracle_address = escrow_client.get_recording_oracle_address(escrow_address)
-    url = kvstore_client.get(recording_oracle_address, "webhook_url")
-
-    return url
+    staking_client = StakingClient(web3)
+    return staking_client.get_leader(escrow.recordingOracle)["webhook_url"]
 
 
 def get_job_launcher_url(chain_id: int, escrow_address: str) -> str:
     if url := Config.localhost.job_launcher_url:
         return url
 
+    escrow = EscrowUtils.get_escrow(chain_id, escrow_address)
+
     web3 = get_web3(chain_id)
-    escrow_client = EscrowClient(web3)
-    kvstore_client = KVStoreClient(web3)
-
-    job_launcher_address = escrow_client.get_job_launcher_address(escrow_address)
-    url = kvstore_client.get(job_launcher_address, "webhook_url")
-
-    return url
+    staking_client = StakingClient(web3)
+    return staking_client.get_leader(escrow.launcher)["webhook_url"]
