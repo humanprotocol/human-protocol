91fc3f92edebdcca61dbcffadae9b1b7907c947b
Francisco López <50665615+flopez7@users.noreply.github.com>
2023-10-17 11:14:51 +0200

    Add signature to all webhook calls in Reputation Oracle (#1049)

--- packages/apps/reputation-oracle/server/src/common/guards/signature.auth.ts
+++ packages/apps/reputation-oracle/server/src/common/guards/signature.auth.ts
@@ -1,32 +1,37 @@
-
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { verifySignature } from '../utils/signature';
 import { HEADER_SIGNATURE_KEY } from '../constants';
-import { ConfigService } from '@nestjs/config';
-import { ConfigNames } from '../config';
+import { EscrowUtils } from '@human-protocol/sdk';
+import { Role } from '../enums/role';
 
 @Injectable()
 export class SignatureAuthGuard implements CanActivate {
-  constructor(
-    public readonly configService: ConfigService
-  ) {}
+  constructor(private role: Role[]) {}
 
   public async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    
+
     const data = request.body;
-    const signature = request.headers[HEADER_SIGNATURE_KEY]; 
-    const oracleAdresses = [
-      this.configService.get<string>(
-        ConfigNames.FORTUNE_RECORDING_ORACLE_ADDRESS,
-      )!,
-      this.configService.get<string>(
-        ConfigNames.CVAT_RECORDING_ORACLE_ADDRESS,
-      )!
-    ]
-    
+    const signature = request.headers[HEADER_SIGNATURE_KEY];
+    const oracleAdresses: string[] = [];
     try {
-      const isVerified = verifySignature(data, signature, oracleAdresses)
+      const escrowData = await EscrowUtils.getEscrow(
+        data.chainId,
+        data.escrowAddress,
+      );
+      if (this.role.includes(Role.JobLaucher))
+        oracleAdresses.push(escrowData.launcher);
+      if (this.role.includes(Role.Exchange))
+        oracleAdresses.push(escrowData.exchangeOracle!);
+      if (this.role.includes(Role.Recording))
+        oracleAdresses.push(escrowData.recordingOracle!);
+
+      const isVerified = verifySignature(data, signature, oracleAdresses);
 
       if (isVerified) {
         return true;
