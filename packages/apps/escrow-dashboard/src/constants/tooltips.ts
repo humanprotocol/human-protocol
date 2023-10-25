3a5c6d7145951358bc9c11e1d6d45c1629532e52
m00n620 <50647994+m00n620@users.noreply.github.com>
2023-10-25 09:09:17 -0400

    [Escrow Dashboard] All network (#1112)
    * add all network option to escrow-dashboard
    
    * fix UI bugs

--- packages/apps/escrow-dashboard/src/constants/tooltips.ts
+++ packages/apps/escrow-dashboard/src/constants/tooltips.ts
@@ -5,6 +5,7 @@ export const TOOLTIPS = {
   TRANSACTIONS: 'Daily on-chain HMT transaction count',
   SOLVED_TASKS: 'Number of tasks solved by workers',
   TOTAL_TRANSFERS: 'Total HMT transfer count',
-  HOLDERS: 'Number of HMT holders',
+  HOLDERS:
+    'Number of HMT holders, this number is all time number and not for the time period selected',
   TOTAL_SUPPLY: 'Total supply of HMT',
 };
