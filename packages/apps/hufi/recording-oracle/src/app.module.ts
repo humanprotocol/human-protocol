f827d2ad8cb5daa5bfb09ba6c0de795cabbe2270
portuu3 <adrian.portugues.mas@gmail.com>
2023-11-03 11:31:01 +0100

    fix swagger files route

--- packages/apps/hufi/recording-oracle/src/app.module.ts
+++ packages/apps/hufi/recording-oracle/src/app.module.ts
@@ -33,7 +33,7 @@ import { join } from 'path';
     ServeStaticModule.forRoot({
       rootPath: join(
         __dirname,
-        '../../../../../../',
+        '../../../../../',
         'node_modules/swagger-ui-dist',
       ),
     }),
