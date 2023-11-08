295e9286ec78574fa04f9b21b086d37a3e4210e6
Sarthak Vijayvergiya <18403281+sarthakvijayvergiya@users.noreply.github.com>
2023-11-08 10:18:01 +0100

    Fix JwtAuthGuard to handle isPublic, isApiKey & Jwt auth token

--- packages/apps/job-launcher/server/src/common/guards/jwt.auth.ts
+++ packages/apps/job-launcher/server/src/common/guards/jwt.auth.ts
@@ -18,29 +18,41 @@ export class JwtAuthGuard extends AuthGuard('jwt-http') implements CanActivate {
   }
 
   public async canActivate(context: ExecutionContext): Promise<boolean> {
-    const useApiKey = this.reflector.getAllAndOverride<boolean>('isApiKey', [
+    // Check for public routes first
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
       context.getHandler(),
       context.getClass(),
     ]);
-    if (useApiKey) {
-      // Use the ApiKeyGuard for API key authentication
-      const apiKeyGuard = await this.moduleRef.create(ApiKeyGuard);
-      return await apiKeyGuard.canActivate(context);
+
+    if (isPublic) {
+      return true;
     }
-    // `super` has to be called to set `user` on `request`
-    // see https://github.com/nestjs/passport/blob/master/lib/auth.guard.ts
-    return (super.canActivate(context) as Promise<boolean>).catch((e) => {
-      const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+
+    // Try to authenticate with JWT
+    try {
+      // `super` has to be called to set `user` on `request`
+      // see https://github.com/nestjs/passport/blob/master/lib/auth.guard.ts
+      return (await super.canActivate(context)) as boolean;
+    } catch (jwtError) {
+      // If JWT fails, try API key authentication if it's allowed for this route
+      const useApiKey = this.reflector.getAllAndOverride<boolean>('isApiKey', [
         context.getHandler(),
         context.getClass(),
       ]);
 
-      if (isPublic) {
-        return true;
+      if (useApiKey) {
+        const apiKeyGuard = await this.moduleRef.create(ApiKeyGuard);
+        try {
+          return await apiKeyGuard.canActivate(context);
+        } catch (apiKeyError) {
+          // If API key also fails, log the error and throw an UnauthorizedException
+          console.error('API key auth failed:', apiKeyError);
+        }
       }
 
-      console.error(e);
+      // If both JWT and API key authentication failed, log the JWT error and throw an UnauthorizedException
+      console.error('JWT auth failed:', jwtError);
       throw new UnauthorizedException('Unauthorized');
-    });
+    }
   }
 }
