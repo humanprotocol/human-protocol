57a6a27c2c08629008ad85b5e47f10db695390a6
Marius Hamacher <marius.hamacher@outlook.de>
2023-11-20 16:01:29 +0100

    fix exo cron tests.

--- packages/examples/cvat/recording-oracle/tests/integration/cron/test_process_exchange_oracle_webhooks.py
+++ packages/examples/cvat/recording-oracle/tests/integration/cron/test_process_exchange_oracle_webhooks.py
@@ -25,7 +25,7 @@ from src.models.webhook import Webhook
 from src.services.webhook import OracleWebhookDirectionTag
 from src.utils.logging import get_function_logger
 
-from tests.utils.constants import DEFAULT_GAS_PAYER_PRIV, SIGNATURE
+from tests.utils.constants import DEFAULT_GAS_PAYER_PRIV, RECORDING_ORACLE_FEE, SIGNATURE
 from tests.utils.setup_escrow import create_escrow, fund_escrow
 
 
@@ -44,11 +44,8 @@ class ServiceIntegrationTest(unittest.TestCase):
     def tearDown(self):
         self.session.close()
 
-    def test_process_exchange_oracle_webhook(self):
-        escrow_address = create_escrow(self.w3)
-        fund_escrow(self.w3, escrow_address)
-
-        webhook = Webhook(
+    def make_webhook(self, escrow_address):
+        return Webhook(
             id=str(uuid.uuid4()),
             direction=OracleWebhookDirectionTag.incoming.value,
             signature=SIGNATURE,
@@ -59,34 +56,26 @@ class ServiceIntegrationTest(unittest.TestCase):
             event_type=ExchangeOracleEventType.task_finished.value,
         )
 
+    def test_process_exchange_oracle_webhook(self):
+        escrow_address = create_escrow(self.w3)
+        fund_escrow(self.w3, escrow_address)
+
+        webhook = self.make_webhook(escrow_address)
         self.session.add(webhook)
         self.session.commit()
 
-        with patch(
-            "src.crons.process_exchange_oracle_webhooks.handle_exchange_oracle_event"
-        ):
+        with patch("src.crons.process_exchange_oracle_webhooks.handle_exchange_oracle_event"):
             process_incoming_exchange_oracle_webhooks()
 
         updated_webhook = (
-            self.session.execute(select(Webhook).where(Webhook.id == webhook.id))
-            .scalars()
-            .first()
+            self.session.execute(select(Webhook).where(Webhook.id == webhook.id)).scalars().first()
         )
         self.assertEqual(updated_webhook.status, OracleWebhookStatuses.completed)
         self.assertEqual(updated_webhook.attempts, 1)
 
     def test_process_recording_oracle_webhooks_invalid_escrow_address(self):
-        webhook = Webhook(
-            id=str(uuid.uuid4()),
-            direction=OracleWebhookDirectionTag.incoming.value,
-            signature=SIGNATURE,
-            escrow_address="invalid_address",
-            chain_id=Networks.localhost.value,
-            type=OracleWebhookTypes.exchange_oracle.value,
-            status=OracleWebhookStatuses.pending.value,
-            event_type=ExchangeOracleEventType.task_finished.value,
-        )
-
+        escrow_address = "invalid_address"
+        webhook = self.make_webhook(escrow_address)
         self.session.add(webhook)
         self.session.commit()
 
@@ -95,164 +84,55 @@ class ServiceIntegrationTest(unittest.TestCase):
                 "src.crons.process_exchange_oracle_webhooks.handle_exchange_oracle_event"
             ) as mock_handler
         ):
-            mock_handler.side_effect = ValueError()
+            mock_handler.side_effect = Exception(f"Can't find escrow {escrow_address}")
             process_incoming_exchange_oracle_webhooks()
             mock_handler.assert_called_once()
 
         updated_webhook = (
-            self.session.execute(select(Webhook).where(Webhook.id == webhook.id))
-            .scalars()
-            .first()
+            self.session.execute(select(Webhook).where(Webhook.id == webhook.id)).scalars().first()
         )
 
         self.assertEqual(updated_webhook.status, OracleWebhookStatuses.pending.value)
         self.assertEqual(updated_webhook.attempts, 1)
 
     def test_process_recording_oracle_webhooks_invalid_escrow_balance(self):
-        chain_id = Networks.localhost.value
         escrow_address = create_escrow(self.w3)
 
-        webhok_id = str(uuid.uuid4())
-        webhook = Webhook(
-            id=webhok_id,
-            signature="signature",
-            s3_url="http://host.docker.internal:9000/results/intermediate-results.json",
-            escrow_address=escrow_address,
-            chain_id=chain_id,
-            type=OracleWebhookTypes.exchange_oracle.value,
-            status=OracleWebhookStatuses.pending.value,
-        )
+        webhook = self.make_webhook(escrow_address)
 
         self.session.add(webhook)
         self.session.commit()
-
-        with self.assertLogs(level="ERROR") as cm:
-            process_exchange_oracle_webhooks()
+        with (
+            patch(
+                "src.crons.process_exchange_oracle_webhooks.handle_exchange_oracle_event"
+            ) as mock_handler
+        ):
+            mock_handler.side_effect = ValueError("Escrow doesn't have funds")
+            process_incoming_exchange_oracle_webhooks()
 
         updated_webhook = (
-            self.session.execute(select(Webhook).where(Webhook.id == webhok_id))
-            .scalars()
-            .first()
+            self.session.execute(select(Webhook).where(Webhook.id == webhook.id)).scalars().first()
         )
 
         self.assertEqual(updated_webhook.status, OracleWebhookStatuses.pending.value)
         self.assertEqual(updated_webhook.attempts, 1)
-        self.assertEqual(
-            cm.output,
-            [
-                f"ERROR:app:Webhook: {webhok_id} failed during execution. Error Escrow doesn't have funds"
-            ],
-        )
 
     @patch("src.chain.escrow.EscrowClient.get_manifest_url")
-    def test_process_job_launcher_webhooks_invalid_manifest_url(
-        self, mock_manifest_url
-    ):
+    def test_process_job_launcher_webhooks_invalid_manifest_url(self, mock_manifest_url):
         mock_manifest_url.return_value = "invalid_url"
-        chain_id = Networks.localhost.value
         escrow_address = create_escrow(self.w3)
         fund_escrow(self.w3, escrow_address)
 
-        webhok_id = str(uuid.uuid4())
-        webhook = Webhook(
-            id=webhok_id,
-            signature="signature",
-            s3_url="http://host.docker.internal:9000/results/intermediate-results.json",
-            escrow_address=escrow_address,
-            chain_id=chain_id,
-            type=OracleWebhookTypes.exchange_oracle.value,
-            status=OracleWebhookStatuses.pending.value,
-        )
+        webhook = self.make_webhook(escrow_address)
 
         self.session.add(webhook)
         self.session.commit()
 
-        with self.assertLogs(level="ERROR") as cm:
-            process_exchange_oracle_webhooks()
+        process_incoming_exchange_oracle_webhooks()
 
         updated_webhook = (
-            self.session.execute(select(Webhook).where(Webhook.id == webhok_id))
-            .scalars()
-            .first()
+            self.session.execute(select(Webhook).where(Webhook.id == webhook.id)).scalars().first()
         )
 
         self.assertEqual(updated_webhook.status, OracleWebhookStatuses.pending.value)
         self.assertEqual(updated_webhook.attempts, 1)
-        self.assertTrue(
-            f"ERROR:app:Webhook: {webhok_id} failed during execution. Error Invalid URL: invalid_url"
-            in cm.output[0],
-        )
-
-    def test_process_job_launcher_webhooks_invalid_intermediate_results_url(self):
-        chain_id = Networks.localhost.value
-        escrow_address = create_escrow(self.w3)
-        fund_escrow(self.w3, escrow_address)
-
-        webhok_id = str(uuid.uuid4())
-        webhook = Webhook(
-            id=webhok_id,
-            signature="signature",
-            s3_url="invalid_url",
-            escrow_address=escrow_address,
-            chain_id=chain_id,
-            type=OracleWebhookTypes.exchange_oracle.value,
-            status=OracleWebhookStatuses.pending.value,
-        )
-
-        self.session.add(webhook)
-        self.session.commit()
-
-        with self.assertLogs(level="ERROR") as cm:
-            process_exchange_oracle_webhooks()
-
-        updated_webhook = (
-            self.session.execute(select(Webhook).where(Webhook.id == webhok_id))
-            .scalars()
-            .first()
-        )
-
-        self.assertEqual(updated_webhook.status, OracleWebhookStatuses.pending.value)
-        self.assertEqual(updated_webhook.attempts, 1)
-        self.assertTrue(
-            f"ERROR:app:Webhook: {webhok_id} failed during execution. Error Invalid URL: invalid_url"
-            in cm.output[0],
-        )
-
-    @patch("src.core.config.StorageConfig.secure")
-    def test_process_job_launcher_webhooks_error_uploading_files(
-        self, mock_storage_config
-    ):
-        mock_storage_config.return_value = True
-        chain_id = Networks.localhost.value
-        escrow_address = create_escrow(self.w3)
-        fund_escrow(self.w3, escrow_address)
-
-        webhok_id = str(uuid.uuid4())
-        webhook = Webhook(
-            id=webhok_id,
-            signature="signature",
-            s3_url="http://host.docker.internal:9000/results/intermediate-results.json",
-            escrow_address=escrow_address,
-            chain_id=chain_id,
-            type=OracleWebhookTypes.exchange_oracle.value,
-            status=OracleWebhookStatuses.pending.value,
-        )
-
-        self.session.add(webhook)
-        self.session.commit()
-
-        with self.assertLogs(level="ERROR") as cm:
-            process_exchange_oracle_webhooks()
-
-        updated_webhook = (
-            self.session.execute(select(Webhook).where(Webhook.id == webhok_id))
-            .scalars()
-            .first()
-        )
-
-        self.assertEqual(updated_webhook.status, OracleWebhookStatuses.pending.value)
-        self.assertEqual(updated_webhook.attempts, 1)
-        self.assertNotEqual(
-            cm.output,
-            [],
-        )
