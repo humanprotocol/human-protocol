62828de512616288cbba5ad7dd7c50ffa620af1a
Marius Hamacher <marius.hamacher@outlook.de>
2023-11-21 13:56:54 +0100

    add tests for client service.

---
+++ packages/examples/cvat/recording-oracle/tests/integration/services/cloud/test_client_service.py
@@ -0,0 +1,42 @@
+import unittest
+
+import boto3
+
+from src.services.cloud import S3Client
+
+
+class ServiceIntegrationTest(unittest.TestCase):
+    def setUp(self):
+        self.url = "http://minio:9000"
+        self.bucket_name = "test-bucket"
+        self.access_key = "dev"
+        self.secret = "devdevdev"
+        self.client = boto3.resource(
+            "s3",
+            aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret,
+            endpoint_url=self.url,
+        ).meta.client
+        self.client.create_bucket(Bucket=self.bucket_name)
+
+    def tearDown(self):
+        self.client.delete_bucket(Bucket=self.bucket_name)
+
+    def test_file_operations(self):
+        client = S3Client(self.url, access_key=self.access_key, secret_key=self.secret)
+
+        assert len(client.list_files(self.bucket_name)) == 0
+
+        file_name = "test_file"
+        data = "this is a test".encode("utf-8")
+
+        assert not client.file_exists(self.bucket_name, file_name)
+        client.create_file(self.bucket_name, file_name, data)
+        assert client.file_exists(self.bucket_name, file_name)
+        assert len(client.list_files(self.bucket_name)) == 1
+
+        file_content = client.download_fileobj(bucket=self.bucket_name, key=file_name)
+        assert file_content == data
+
+        client.remove_file(self.bucket_name, file_name)
+        assert not client.file_exists(self.bucket_name, file_name)
