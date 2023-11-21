7d06a8aa3125d5a7ae6ea5832c99aa4cb57b45ce
Marius Hamacher <marius.hamacher@outlook.de>
2023-11-21 14:21:01 +0100

    add degenerate tests for client service.

--- packages/examples/cvat/recording-oracle/tests/integration/services/cloud/test_client_service.py
+++ packages/examples/cvat/recording-oracle/tests/integration/services/cloud/test_client_service.py
@@ -1,6 +1,8 @@
 import unittest
 
 import boto3
+import pytest
+from botocore.exceptions import ClientError, EndpointConnectionError
 
 from src.services.cloud import S3Client
 
@@ -40,3 +42,32 @@ class ServiceIntegrationTest(unittest.TestCase):
 
         client.remove_file(self.bucket_name, file_name)
         assert not client.file_exists(self.bucket_name, file_name)
+
+    def test_degenerate_file_operations(self):
+        client = S3Client(self.url, access_key=self.access_key, secret_key=self.secret)
+        invalid_bucket = "non-existent-bucket"
+        invalid_file = "non-existent-file"
+
+        with pytest.raises(ClientError):
+            client.download_fileobj(bucket=invalid_bucket, key=invalid_file)
+
+        with pytest.raises(ClientError):
+            client.download_fileobj(bucket=self.bucket_name, key=invalid_file)
+
+        with pytest.raises(ClientError):
+            client.create_file(bucket=invalid_bucket, filename=invalid_file)
+
+        with pytest.raises(ClientError):
+            client.list_files(bucket=invalid_bucket)
+
+        client.remove_file(bucket=self.bucket_name, filename=invalid_file)
+
+    def test_degenerate_client(self):
+        with pytest.raises(EndpointConnectionError):
+            invalid_client = S3Client(
+                "http://not.an.url:1234", access_key=self.access_key, secret_key=self.secret
+            )
+            invalid_client.create_file(self.bucket_name, "test.txt")
+
+        with pytest.raises(ValueError):
+            S3Client("nonsense-stuff")
