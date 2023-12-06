75df94b0765b9053d9b21899d13759bf35cc66da
m00n620 <gex6611@gmail.com>
2023-12-06 10:11:43 -0500

    add dashobard admin project

---
+++ packages/apps/dashboard/admin/src/api/network-data-item/routes/network-data-item.ts
@@ -0,0 +1,7 @@
+/**
+ * network-data-item router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::network-data-item.network-data-item');
---
+++ packages/apps/dashboard/admin/src/api/network-data-item/services/network-data-item.ts
@@ -0,0 +1,7 @@
+/**
+ * network-data-item service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::network-data-item.network-data-item');
