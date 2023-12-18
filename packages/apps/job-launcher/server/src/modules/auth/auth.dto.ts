18b8b1680603e40e4bbd72bbc08a9ca31ce23514
Eugene Voronov <voronovege@cy-tech.fr>
2023-12-18 12:14:33 +0300

    Updated regex

--- packages/apps/job-launcher/server/src/modules/auth/auth.dto.ts
+++ packages/apps/job-launcher/server/src/modules/auth/auth.dto.ts
@@ -24,7 +24,7 @@ export class SignInDto {
 }
 
 export class ValidatePasswordDto {
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*+])(?=.{8,})/, {
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\|'"/`[\]:;<>,.?~\\-]).*$/, {
     message:
       'Password is not strong enough. Password must be at least eight characters long and contain 1 upper, 1 lowercase, 1 number and 1 special character.',
   })
