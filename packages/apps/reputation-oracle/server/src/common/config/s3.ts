d736514cf6611ab6c94a5ecc8fe04b18827aa176
Eric Lee <eric@hmt.ai>
2023-12-18 12:10:06 -0500

    fix test

--- packages/apps/reputation-oracle/server/src/common/config/s3.ts
+++ packages/apps/reputation-oracle/server/src/common/config/s3.ts
@@ -3,9 +3,9 @@ import { ConfigType, registerAs } from '@nestjs/config';
 export const s3Config = registerAs('s3', () => ({
   endPoint: process.env.S3_ENDPOINT || '127.0.0.1',
   port: +(process.env.S3_PORT || 9000),
-  accessKey: process.env.S3_ACCESS_KEY,
-  secretKey: process.env.S3_SECRET_KEY,
-  bucket: process.env.S3_BUCKET,
+  accessKey: process.env.S3_ACCESS_KEY || '',
+  secretKey: process.env.S3_SECRET_KEY || '',
+  bucket: process.env.S3_BUCKET || '',
   useSSL: process.env.S3_USE_SSL === 'true',
 }));
 
