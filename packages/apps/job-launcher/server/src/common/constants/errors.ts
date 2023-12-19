eca6267727b4230c430eb5926f31fba4da687415
portuu3 <adrian.portugues.mas@gmail.com>
2023-12-19 13:59:39 +0100

    remove duplicate

--- packages/apps/job-launcher/server/src/common/constants/errors.ts
+++ packages/apps/job-launcher/server/src/common/constants/errors.ts
@@ -30,16 +30,6 @@ export enum ErrorWebhook {
 }
 
 /**
- * Represents error messages associated to webhook.
- */
-export enum ErrorWebhook {
-  NotSent = 'Webhook was not sent',
-  NotFound = 'Webhook not found',
-  UrlNotFound = 'Webhook URL not found',
-  NotCreated = 'Webhook has not been created',
-}
-
-/**
  * Represents error messages related to escrow.
  */
 export enum ErrorEscrow {
