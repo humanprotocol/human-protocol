2633572f4ab8aa9028c93decfd54c25c80f03720
m00n620 <gex6611@gmail.com>
2023-12-21 09:11:50 -0500

    fix ui bugs and tasks data on dashboard ui

--- packages/apps/dashboard/ui/src/components/HumanAppData/views/Transactions.tsx
+++ packages/apps/dashboard/ui/src/components/HumanAppData/views/Transactions.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { ChartContainer } from './Container';
 import { TooltipIcon } from 'src/components/TooltipIcon';
+import { TOOLTIPS } from 'src/constants/tooltips';
 
 export const TransactionsView = ({
   isLoading,
@@ -12,7 +13,7 @@ export const TransactionsView = ({
 }) => {
   return (
     <ChartContainer isLoading={isLoading} data={data} title="Transactions">
-      <TooltipIcon title="Sorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim." />
+      <TooltipIcon title={TOOLTIPS.TRANSACTIONS} />
     </ChartContainer>
   );
 };
