fdfad7f5281c209666bd4b955231d03139ee7d65
m00n620 <50647994+m00n620@users.noreply.github.com>
2024-01-08 10:36:54 -0500

    [Job Launcher] CVAT S3 buckets (#1384)
    * support s3 providers on cvat
    
    * disable GCS
    
    ---------
    
    Co-authored-by: portuu3 <adrian.portugues.mas@gmail.com>

--- packages/apps/job-launcher/client/src/components/Jobs/Create/FortuneJobRequestForm.tsx
+++ packages/apps/job-launcher/client/src/components/Jobs/Create/FortuneJobRequestForm.tsx
@@ -1,18 +1,37 @@
-import { Box, Button, FormControl, Grid, TextField } from '@mui/material';
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { Formik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from '../../../components/Accordion';
+import { CollectionsFilledIcon } from '../../../components/Icons/CollectionsFilledIcon';
 import { useCreateJobPageUI } from '../../../providers/CreateJobPageUIProvider';
 import { FortuneJobRequestValidationSchema } from './schema';
 
 export const FortuneJobRequestForm = () => {
   const { jobRequest, updateJobRequest, goToPrevStep, goToNextStep } =
     useCreateJobPageUI();
+  const [expanded, setExpanded] = useState<string | false>('panel1');
 
   const initialValues = {
     title: '',
     fortunesRequested: undefined,
     description: '',
   };
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
 
   const handleNext = ({ title, fortunesRequested, description }: any) => {
     updateJobRequest?.({
@@ -27,7 +46,7 @@ export const FortuneJobRequestForm = () => {
   };
 
   return (
-    <Box>
+    <Box sx={{ mt: '42px' }}>
       <Formik
         initialValues={initialValues}
         validationSchema={FortuneJobRequestValidationSchema}
@@ -44,59 +63,77 @@ export const FortuneJobRequestForm = () => {
           setFieldValue,
         }) => (
           <form>
-            <Grid container spacing={2} mb={4}>
-              <Grid item xs={12} sm={12} md={6}>
-                <FormControl fullWidth>
-                  <TextField
-                    name="title"
-                    value={values.title}
-                    onChange={(e) => setFieldValue('title', e.target.value)}
-                    onBlur={handleBlur}
-                    placeholder="Title"
-                    label="Title"
-                    error={touched.title && Boolean(errors.title)}
-                    helperText={errors.title}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12} sm={12} md={6}>
-                <FormControl fullWidth>
-                  <TextField
-                    name="fortunesRequested"
-                    value={values.fortunesRequested}
-                    onChange={(e) =>
-                      setFieldValue('fortunesRequested', e.target.value)
-                    }
-                    onBlur={handleBlur}
-                    placeholder="Fortunes Requested"
-                    label="Fortunes Requested"
-                    type="number"
-                    inputProps={{ min: 0, step: 1 }}
-                    error={
-                      touched.fortunesRequested &&
-                      Boolean(errors.fortunesRequested)
-                    }
-                    helperText={errors.fortunesRequested}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField
-                    name="description"
-                    value={values.description}
-                    onChange={(e) =>
-                      setFieldValue('description', e.target.value)
-                    }
-                    onBlur={handleBlur}
-                    placeholder="Description"
-                    label="Description"
-                    error={touched.description && Boolean(errors.description)}
-                    helperText={errors.description}
-                  />
-                </FormControl>
-              </Grid>
-            </Grid>
+            <Accordion
+              expanded={expanded === 'panel1'}
+              onChange={handleChange('panel1')}
+            >
+              <AccordionSummary
+                aria-controls="panel1d-content"
+                id="panel1d-header"
+              >
+                <CollectionsFilledIcon />
+                <Typography variant="body2" fontWeight={700} ml={2}>
+                  General
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Grid container spacing={2} mb={4}>
+                  <Grid item xs={12} sm={12} md={6}>
+                    <FormControl fullWidth>
+                      <TextField
+                        name="title"
+                        value={values.title}
+                        onChange={(e) => setFieldValue('title', e.target.value)}
+                        onBlur={handleBlur}
+                        placeholder="Title"
+                        label="Title"
+                        error={touched.title && Boolean(errors.title)}
+                        helperText={errors.title}
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={6}>
+                    <FormControl fullWidth>
+                      <TextField
+                        name="fortunesRequested"
+                        value={values.fortunesRequested}
+                        onChange={(e) =>
+                          setFieldValue('fortunesRequested', e.target.value)
+                        }
+                        onBlur={handleBlur}
+                        placeholder="Fortunes Requested"
+                        label="Fortunes Requested"
+                        type="number"
+                        inputProps={{ min: 0, step: 1 }}
+                        error={
+                          touched.fortunesRequested &&
+                          Boolean(errors.fortunesRequested)
+                        }
+                        helperText={errors.fortunesRequested}
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth>
+                      <TextField
+                        name="description"
+                        value={values.description}
+                        onChange={(e) =>
+                          setFieldValue('description', e.target.value)
+                        }
+                        onBlur={handleBlur}
+                        placeholder="Description"
+                        label="Description"
+                        error={
+                          touched.description && Boolean(errors.description)
+                        }
+                        helperText={errors.description}
+                      />
+                    </FormControl>
+                  </Grid>
+                </Grid>
+              </AccordionDetails>
+            </Accordion>
             <Box
               sx={{
                 display: 'flex',
