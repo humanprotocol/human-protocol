f00836d724ebe86ea2c8c7cd57e14b4e09c81256
Mehdi <rhouzm@gmail.com>
2024-01-19 05:35:02 +0100

    feat: init handlers

--- packages/core-foundry/test/CoreUtils.t.sol
+++ packages/core-foundry/test/CoreUtils.t.sol
@@ -1,8 +1,13 @@
 pragma solidity 0.8.20;
 
 import "forge-std/test.sol";
+import "../src/Escrow.sol";
+import "../src/HMToken.sol";
 
-contract CoreUtils is Test {
+
+contract CoreUtils is Test {    
+
+
     address owner = vm.addr(1);
     address launcher = vm.addr(2);
     address reputationOracle = vm.addr(3);
@@ -10,7 +15,7 @@ contract CoreUtils is Test {
     address exchangeOracle = vm.addr(5);
     address externalAddress = vm.addr(6);
     address[] restAccounts = new address[](2);
-    address[] trustedHandlers = [vm.addr(9)];
+    address[] trustedHandlers = new address[](2);
 
     /**
      * @dev fund accounts with Ether
@@ -24,5 +29,13 @@ contract CoreUtils is Test {
         vm.deal(externalAddress, 100 ether);
         vm.deal(restAccounts[0], 100 ether);
         vm.deal(trustedHandlers[0], 100 ether);
+    }
+
+    function _initTrustedHandlers() internal {
+        trustedHandlers[0] = vm.addr(12);
+        trustedHandlers[1] = vm.addr(13);
+        // vm.prank(owner);
+        // escrow = new Escrow(address(0), launcher, payable(owner), 100, trustedHandlers);
+        // escrow.addTrustedHandlers(trustedHandlers);
     }
 }
