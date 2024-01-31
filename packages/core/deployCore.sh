264f7a50c94c9e1404bdac1238c2aab2c19ece1c
mrhouzlane <rhouzm@gmail.com>
2024-01-31 08:48:16 +0100

    fix: remove forge-std in core-foundry

--- packages/core/deployCore.sh
+++ packages/core/deployCore.sh
@@ -3,7 +3,6 @@
 # Run forge build
 forge build --silent
 
-# Extract the contract name from the path (adjust the path pattern as needed)
 for file in ./out/*.sol/*.json; do
     if [[ -f "$file" ]]; then
         echo "Extracting ABI from $file"
