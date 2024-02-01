72b933da750d754dd1c26f0c5deb66cf60eda1a5
mrhouzlane <rhouzm@gmail.com>
2024-02-01 08:06:24 +0100

    add: forge fmt to format contracts

--- packages/core/setup-foundry.sh
+++ packages/core/setup-foundry.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-if command -v forge &> /dev/null; then
-    echo "Forge is already installed."
-    echo "Forge version is $(forge --version)"
-else
+# Check if forge is already installed
+if ! command -v forge &> /dev/null; then
     echo "Forge is not installed, installing Foundry..."
     curl -L https://foundry.paradigm.xyz | bash
+    source $HOME/.profile || source $HOME/.bashrc || echo "Note: If forge command is not found, you may need to manually update your PATH or start a new shell session."
+fi
 
-    sleep 10
+sleep 20
 
-    if command -v foundryup &> /dev/null; then
-        foundryup || echo "Failed to run foundryup."
-        if command -v forge &> /dev/null; then
-            echo "Forge installation was successful."
-            echo "Forge version is $(forge --version)"
-        else
-            echo "Forge installation failed. Please check your PATH or Foundry installation."
-            exit 1
-        fi
-    else
-        echo "foundryup command not found. Please ensure the Foundry installation script ran successfully and your PATH is correctly updated."
-        exit 1
-    fi
+if command -v forge &> /dev/null; then
+    echo "Forge version is $(forge --version)"
+else
+    echo "Forge installation failed or Forge is not in your PATH."
+    exit 1
+fi
+
+echo "Running forge fmt --check to verify format..."
+if forge fmt --check; then
+    echo "Format check passed."
+else
+    echo "Format check failed. Please format your Solidity files."
+    exit 1
 fi
