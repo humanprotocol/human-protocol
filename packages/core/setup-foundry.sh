89d8600e5dab48f99668cf3ef24b18b319e02833
mrhouzlane <rhouzm@gmail.com>
2024-02-01 07:59:47 +0100

    fix: setup-foundry.sh file

--- packages/core/setup-foundry.sh
+++ packages/core/setup-foundry.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Check if forge is already installed
-if command -v forge &> /dev/null; then
-    echo "Forge is already installed."
-    
+if command -v foundryup &> /dev/null; then
+    echo "Updating Foundry tools..."
+    foundryup
 else
-    echo "Forge is not installed, installing Foundry..."
+    echo "Installing Foundry..."
     curl -L https://foundry.paradigm.xyz | bash
-    forge --version
+    source $HOME/.profile || source $HOME/.bashrc || echo "Unable to source profile files. You may need to manually update your PATH or start a new shell session."
+    foundryup || echo "Failed to run foundryup. Please ensure it's installed and in your PATH."
+fi
+
+if command -v forge &> /dev/null; then
+    echo "Forge installation verified."
     echo "Forge version is $(forge --version)"
-
-    source $HOME/.profile || source $HOME/.bashrc || echo "Note: Unable to source profile files. You may need to manually update your PATH or start a new shell session."
-
-    sleep 10
-
-    if command -v foundryup &> /dev/null; then
-        foundryup || echo "Failed to run foundryup."
-    else
-        echo "foundryup
+else
+    echo "Forge installation failed or Forge is not in your PATH."
+    exit 1
+fi
