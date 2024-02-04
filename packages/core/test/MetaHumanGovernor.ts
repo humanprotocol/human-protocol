e6f131706853ec12c531aeaffd89273e1d46162e
mrhouzlane <rhouzm@gmail.com>
2024-02-04 23:18:53 +0100

    feat: governor tests

--- packages/core/test/MetaHumanGovernor.ts
+++ packages/core/test/MetaHumanGovernor.ts
@@ -1,42 +1,245 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { EventLog, Signer } from 'ethers';
+import { Signer } from 'ethers';
 import {
+  MetaHumanGovernor,
+  VHMToken,
+  HMToken,
   TimelockController,
   DAOSpokeContract,
-  MetaHumanGovernor,
-  HMToken,
-  VHMToken,
 } from '../typechain-types';
 
-let metaHumanGovernor: MetaHumanGovernor,
-  vhmToken: VHMToken,
-  hmToken: HMToken,
-  daoSpokeContract: DAOSpokeContract,
-  timelockController: TimelockController;
-let owner: Signer, addr1: Signer, addr2: Signer, trustedHandlers: Signer[];
+describe('MetaHumanGovernor', function () {
+  let owner: Signer;
+  let proposers: string[];
+  const executors: string[] = [ethers.ZeroAddress];
+  let governor: MetaHumanGovernor;
+  let voteToken: VHMToken;
+  let token: HMToken;
+  let timelockController: TimelockController;
+  let daoSpoke: DAOSpokeContract;
+  let newDAOSpoke: DAOSpokeContract;
+  let wormholeRelayerAddress: Signer;
 
-async function deployMetaHumanGovernor() {
-  const MetaHumanGovernor = await ethers.getContractFactory(
-    'contracts/governance/MetaHumanGovernor.sol:MetaHumanGovernor'
-  );
-  const VHMToken = await ethers.getContractFactory(
-    'contracts/governance/vhm-token/VHMToken.sol:VHMToken'
-  );
-  const HMToken = await ethers.getContractFactory(
-    'contracts/HMToken.sol:HMToken'
-  );
-  const DAOSpokeContract = await ethers.getContractFactory(
-    'contracts/governance/DAOSpokeContract.sol:DAOSpokeContract'
-  );
-  const TimelockController =
-    await ethers.getContractFactory('TimelockController');
+  this.beforeEach(async () => {
+    const [deployer] = await ethers.getSigners();
+    owner = deployer;
 
-  hmToken = (await HMToken.deploy(100000, 'HMToken', 18, 'HMT')) as HMToken;
-  vhmToken = (await VHMToken.deploy(hmToken.getAddress())) as VHMToken;
+    // Deploy HMToken
+    const HMToken = await ethers.getContractFactory(
+      'contracts/HMToken.sol:HMToken'
+    );
+    token = (await HMToken.deploy(
+      1000000000,
+      'Human Token',
+      18,
+      'HMT'
+    )) as HMToken;
 
-  await hmToken.approve(vhmToken.getAddress(), 10);
-  await vhmToken.depositFor(owner.getAddress(), 10);
-}
+    // Deploy VHMToken
+    const VHMToken = await ethers.getContractFactory(
+      'contracts/governance/vhm-token/VHMToken.sol:VHMToken'
+    );
+    voteToken = (await VHMToken.deploy(await token.getAddress())) as VHMToken;
+
+    // Deposit vhmTokens
+    await token.approve(voteToken.getAddress(), 10);
+    await voteToken.depositFor(owner.getAddress(), 10);
+
+    // Deploy TimelockController
+    proposers = [await owner.getAddress()];
+    const TimelockController =
+      await ethers.getContractFactory('TimelockController');
+    timelockController = await TimelockController.deploy(
+      1,
+      proposers,
+      executors,
+      owner.getAddress()
+    );
+    await timelockController.waitForDeployment();
+
+    // Deploy MetaHumanGovernor
+    const MetaHumanContract = await ethers.getContractFactory(
+      'contracts/governance/MetaHumanGovernor.sol:MetaHumanGovernor'
+    );
+    governor = (await MetaHumanContract.deploy(
+      voteToken.getAddress(),
+      timelockController.getAddress(),
+      [],
+      5, // voting delay
+      '0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0',
+      owner.getAddress(),
+      12
+    )) as MetaHumanGovernor;
+
+    // Deploy DAOSpokeContract
+    const DAOSpokeContract = await ethers.getContractFactory(
+      'contracts/governance/DAOSpokeContract.sol:DAOSpokeContract'
+    );
+    const DAOSpoke = (await DAOSpokeContract.deploy(
+      ethers.zeroPadBytes(await governor.getAddress(), 32),
+      5, // hubChainId
+      voteToken.getAddress(),
+      12, // voting period
+      6, // spokeChainId
+      '0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0',
+      owner.getAddress() // admin address
+    )) as DAOSpokeContract;
+  });
+
+  it('Should update spoke contracts correctly', async function () {
+    // Deploy new Spoke
+    const DAOSpokeContract = await ethers.getContractFactory(
+      'contracts/governance/DAOSpokeContract.sol:DAOSpokeContract'
+    );
+
+    newDAOSpoke = (await DAOSpokeContract.deploy(
+      ethers.zeroPadBytes(await governor.getAddress(), 32),
+      5,
+      await voteToken.getAddress(),
+      12,
+      6,
+      '0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0',
+      await owner.getAddress() // admin address
+    )) as DAOSpokeContract;
+
+    const spokeContractsX = [
+      {
+        contractAddress: ethers.zeroPadBytes(
+          await newDAOSpoke.getAddress(),
+          32
+        ),
+        chainId: 6,
+      },
+    ];
+
+    await expect(governor.updateSpokeContracts(spokeContractsX)).to.emit(
+      governor,
+      'SpokesUpdated'
+    );
+
+    const updated = await governor.spokeContractsMapping(
+      ethers.zeroPadBytes(await newDAOSpoke.getAddress(), 32),
+      6
+    );
+    expect(updated).to.be.true;
+  });
+
+  it('Should revert when updating spoke contracts with duplicates', async function () {
+    const DAOSpokeContract = await ethers.getContractFactory(
+      'contracts/governance/DAOSpokeContract.sol:DAOSpokeContract'
+    );
+
+    const newDAOSpoke = (await DAOSpokeContract.deploy(
+      ethers.zeroPadBytes(await governor.getAddress(), 32),
+      5,
+      voteToken.getAddress(),
+      12,
+      6,
+      '0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0',
+      owner.getAddress()
+    )) as DAOSpokeContract;
+
+    const spokeContracts = [
+      {
+        contractAddress: ethers.zeroPadBytes(
+          await newDAOSpoke.getAddress(),
+          32
+        ),
+        chainId: 6,
+      },
+      {
+        contractAddress: ethers.zeroPadBytes(
+          await newDAOSpoke.getAddress(),
+          32
+        ),
+        chainId: 6,
+      },
+    ];
+    await expect(
+      governor.updateSpokeContracts(spokeContracts)
+    ).to.be.revertedWith('Duplicates are not allowed');
+  });
+
+  it('Should update spoke contracts with unique entries successfully', async function () {
+    const DAOSpokeContract = await ethers.getContractFactory(
+      'contracts/governance/DAOSpokeContract.sol:DAOSpokeContract'
+    );
+    const newlyDeployedSpoke = await DAOSpokeContract.deploy(
+      ethers.zeroPadBytes(await governor.getAddress(), 32),
+      5,
+      await voteToken.getAddress(),
+      12,
+      6,
+      '0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0',
+      await owner.getAddress()
+    );
+
+    const spokeContracts = [
+      {
+        contractAddress: ethers.zeroPadBytes(
+          await newlyDeployedSpoke.getAddress(),
+          32
+        ),
+        chainId: 6,
+      },
+      {
+        contractAddress: ethers.zeroPadBytes(
+          await newlyDeployedSpoke.getAddress(),
+          32
+        ),
+        chainId: 7,
+      },
+    ];
+
+    await governor.updateSpokeContracts(spokeContracts);
+
+    const updated1 = await governor.spokeContractsMapping(
+      ethers.zeroPadBytes(await newlyDeployedSpoke.getAddress(), 32),
+      6
+    );
+    expect(updated1).to.be.true;
+
+    const updated2 = await governor.spokeContractsMapping(
+      ethers.zeroPadBytes(await newlyDeployedSpoke.getAddress(), 32),
+      7
+    );
+    expect(updated2).to.be.true;
+  });
+
+  it('cannot update spoke contracts after transferring ownership', async function () {
+    const DAOSpokeContract = await ethers.getContractFactory(
+      'contracts/governance/DAOSpokeContract.sol:DAOSpokeContract'
+    );
+    const newlyDeployedSpoke = (await DAOSpokeContract.deploy(
+      ethers.zeroPadBytes(await governor.getAddress(), 32),
+      5,
+      await voteToken.getAddress(),
+      12,
+      6,
+      '0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0',
+      await owner.getAddress()
+    )) as DAOSpokeContract;
+    const spokeChainId = 6;
+
+    const spokeContracts = [
+      {
+        contractAddress: ethers.zeroPadBytes(
+          await newlyDeployedSpoke.getAddress(),
+          32
+        ),
+        chainId: spokeChainId,
+      },
+    ];
+
+    // Transfer ownership to the timelockController
+    await governor
+      .connect(owner)
+      .transferOwnership(await timelockController.getAddress());
+
+    await expect(
+      governor.connect(owner).updateSpokeContracts(spokeContracts)
+    ).to.be.revertedWith('Ownable: caller is not the owner');
+  });
+});
