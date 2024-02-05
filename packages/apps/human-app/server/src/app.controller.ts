3a56fc84beea8832a526339ac2098659892c0591
Michał Walczak <michal.walczak@blockydevs.com>
2024-02-05 12:58:17 +0100

    feat: redirection to swagger endpoint

--- packages/apps/human-app/server/src/app.controller.ts
+++ packages/apps/human-app/server/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  @Redirect('/swagger', 301)
+  public swagger(): string {
+    return 'OK';
   }
 }
