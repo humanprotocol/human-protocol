f9f48b614014636050529deae84be48696ead5b0
Michał Walczak <michal.walczak@blockydevs.com>
2024-02-05 12:33:07 +0100

    feat: add swagger setup

--- packages/apps/human-app/server/src/main.ts
+++ packages/apps/human-app/server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Human APP API')
+    .setDescription('Swagger Human APP API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
