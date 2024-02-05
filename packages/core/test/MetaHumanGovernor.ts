27b859da797fea4fef504ee7591cc6ac4b90ef5c
mrhouzlane <rhouzm@gmail.com>
2024-02-05 02:08:14 +0100

    add: function helpers for governor tests

--- packages/core/test/MetaHumanGovernor.ts
+++ packages/core/test/MetaHumanGovernor.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { Signer } from 'ethers';
+import { Signer, BigNumberish } from 'ethers';
 import {
   MetaHumanGovernor,
   VHMToken,
@@ -9,6 +9,124 @@ import {
   TimelockController,
   DAOSpokeContract,
 } from '../typechain-types';
+
+interface IWormholeSignature {
+  r: string;
+  s: string;
+  v: number;
+  guardianIndex: number;
+}
+
+interface IWormholeVM {
+  version: number;
+  timestamp: number;
+  nonce: number;
+  emitterChainId: number;
+  emitterAddress: string;
+  sequence: number;
+  consistencyLevel: number;
+  payload: string;
+  guardianSetIndex: number;
+  signatures: IWormholeSignature[];
+  hash: string;
+}
+
+async function createMessageWithPayload(
+  payload: string,
+  emitterChainId: number,
+  emitterAddress: string
+): Promise<IWormholeVM> {
+  const signatures: Signature[] = [];
+  const mockVM: IWormholeVM = {
+    version: 1,
+    timestamp: 0,
+    nonce: 0,
+    emitterChainId: emitterChainId,
+    emitterAddress: emitterAddress,
+    sequence: 0,
+    consistencyLevel: 200,
+    payload: payload,
+    guardianSetIndex: 0,
+    signatures: signatures,
+    hash: ethers.keccak256(payload),
+  };
+
+  return mockVM;
+}
+
+async function callReceiveMessageOnSpokeWithMock(
+  daoSpoke: DAOSpokeContract,
+  result: IWormholeVM
+): Promise<void> {
+  const vaas: string[] = [];
+
+  await daoSpoke.receiveWormholeMessages(
+    result.payload,
+    vaas,
+    result.emitterAddress,
+    result.emitterChainId,
+    result.hash
+  );
+}
+
+async function createProposalOnSpoke(
+  daoSpoke: DAOSpokeContract,
+  proposalId: number,
+  governorAddress: string
+): Promise<number> {
+  const spokeChainId = 6;
+  const hubChainId = 5;
+
+  const latestBlock = await ethers.provider.getBlock('latest');
+  if (!latestBlock) {
+    throw new Error('Failed to fetch the latest block');
+  }
+  const currentBlockTimestamp = latestBlock.timestamp;
+  const futureTimestamp = currentBlockTimestamp + 1000;
+
+  // Encode the message
+  const message = ethers.solidityPacked(
+    ['uint256', 'uint256', 'uint256', 'uint256'],
+    [0, proposalId, currentBlockTimestamp, futureTimestamp]
+  );
+
+  const payload = ethers.solidityPacked(
+    ['address', 'uint256', 'address', 'bytes'],
+    [daoSpoke.getAddress(), spokeChainId, governorAddress, message]
+  );
+
+  const mockResult = await createMessageWithPayload(
+    payload,
+    hubChainId,
+    governorAddress
+  );
+
+  await callReceiveMessageOnSpokeWithMock(daoSpoke, mockResult);
+  return proposalId;
+}
+
+async function createBasicProposal(
+  voteToken: VHMToken,
+  governor: MetaHumanGovernor,
+  owner: Signer
+): Promise<BigNumber> {
+  const encodedCall = voteToken.interface.encodeFunctionData('transfer', [
+    await owner.getAddress(),
+    1,
+  ]);
+  const targets = [await voteToken.getAddress()];
+  const values: BigNumberish[] = [0];
+  const calldatas = [encodedCall];
+
+  const proposalId: BigNumber = await governor.crossChainPropose(
+    targets,
+    values,
+    calldatas,
+    ''
+  );
+
+  return proposalId;
+}
 
 describe('MetaHumanGovernor', function () {
   let owner: Signer;
@@ -241,5 +359,9 @@ describe('MetaHumanGovernor', function () {
     await expect(
       governor.connect(owner).updateSpokeContracts(spokeContracts)
     ).to.be.revertedWith('Ownable: caller is not the owner');
+  });
+
+  it('Should grant proposal creation', async function () {
+    const proposalId = await createBasicProposal(voteToken, governor, owner);
   });
 });
