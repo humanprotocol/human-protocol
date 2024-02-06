a6ccd9c7e7e0209aac0ef018c18a20b820b9dae5
maciek.nabialek <maciej.nabialek@blockydevs.com>
2024-02-06 17:18:47 +0100

    Draft: Adds e2e tests

--- packages/apps/human-app/server/jest.config.ts
+++ packages/apps/human-app/server/jest.config.ts
@@ -1,15 +1,28 @@
 module.exports = {
+  preset: 'ts-jest/presets/default-esm', // Use this preset for ESM support
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1', // Add this line if you're importing ESM modules using .js extensions in your TypeScript files
+    '^uuid$': require.resolve('uuid'),
+    '^typeorm$': require.resolve('typeorm'),
+  },
+  // Specify file extensions for modules using ESM
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    // Use ts-jest for ts / tsx files
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   coverageDirectory: '../coverage',
   collectCoverageFrom: ['**/*.(t|j)s'],
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
   testEnvironment: 'node',
   testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '^uuid$': require.resolve('uuid'),
-    '^typeorm$': require.resolve('typeorm'),
-  },
+  transformIgnorePatterns: [
+    'node_modules/(?!@nestjs/config/node_modules/uuid)'
+  ],
 };
--- packages/apps/human-app/server/test/app.e2e-spec.ts
+++ packages/apps/human-app/server/test/app.e2e-spec.ts
@@ -2,16 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
+import nock from "nock";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { WorkerType } from "../src/interfaces/signup-worker-request.dto";
+import {describe} from '@jest/globals';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: ['.env'],
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    configService = moduleFixture.get<ConfigService>(ConfigService);
     await app.init();
   });
 
@@ -20,5 +32,32 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('should process the singnup worker request', async () => {
+    const registeredType =  WorkerType.WORKER.toString();
+      nock(configService.get<string>('REPUTATION_ORACLE_URL') || 'http://localhost:5678')
+        .post('/auth/signup', {
+          email: 'john_doe@example.com',
+          password: 'v3ry57r0n9P455w0r[)!',
+          type: registeredType,
+        }, {
+          reqheaders: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .reply(200, );
+
+      return request(app.getHttpServer())
+        .post('/auth/signup')
+        .send({
+          email: 'john_doe@example.com',
+          password: 'v3ry57r0n9P455w0r[)!'
+        })
+        .expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
