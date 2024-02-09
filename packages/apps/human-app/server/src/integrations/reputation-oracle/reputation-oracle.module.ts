8556d44cab64796125cbc6c5c7b4b8c1ba7af24b
Michał Walczak <michal.walczak@blockydevs.com>
2024-02-09 11:32:47 +0100

    bug fixed

--- packages/apps/human-app/server/src/integrations/reputation-oracle/reputation-oracle.module.ts
+++ packages/apps/human-app/server/src/integrations/reputation-oracle/reputation-oracle.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { ReputationOracleGateway } from './reputation-oracle.gateway';
 import { HttpModule } from '@nestjs/axios';
 import { ReputationOracleProfile } from './reputation-oracle.mapper';
-import { AppConfigModule } from '../../common/config/app-config.module';
+import { EnvironmentConfigService } from '../../common/config/environment-config.service';
+import { GatewayConfigService } from '../../common/config/gateway-config.service';
 
 @Module({
-  imports: [HttpModule, AppConfigModule],
-  providers: [ReputationOracleGateway, ReputationOracleProfile],
+  imports: [HttpModule],
+  providers: [
+    ReputationOracleGateway,
+    ReputationOracleProfile,
+    EnvironmentConfigService,
+    GatewayConfigService,
+  ],
   exports: [ReputationOracleGateway],
 })
 export class ReputationOracleModule {}
