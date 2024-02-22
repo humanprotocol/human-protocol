e6cea8e13f8a34e2471d163934f0694dd7f97291
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-02-22 21:26:43 +0200

    Add job annotation mode param into assignment links

--- packages/examples/cvat/exchange-oracle/src/utils/assignments.py
+++ packages/examples/cvat/exchange-oracle/src/utils/assignments.py
@@ -1,13 +1,18 @@
 from urllib.parse import urljoin
 
 from src.core.config import Config
-from src.core.manifest import TaskManifest
+from src.core.manifest import TaskManifest, TaskType
 from src.core.manifest import parse_manifest as _parse_manifest
+from src.models.cvat import Project
 
 
 def parse_manifest(manifest: dict) -> TaskManifest:
     return _parse_manifest(manifest)
 
 
-def compose_assignment_url(task_id, job_id) -> str:
-    return urljoin(Config.cvat_config.cvat_url, f"/tasks/{task_id}/jobs/{job_id}")
+def compose_assignment_url(task_id: int, job_id: int, *, project: Project) -> str:
+    query_params = ""
+    if project.job_type in [TaskType.image_skeletons_from_boxes, TaskType.image_boxes_from_points]:
+        query_params = "?defaultWorkspace=single_shape"
+
+    return urljoin(Config.cvat_config.cvat_url, f"/tasks/{task_id}/jobs/{job_id}{query_params}")
