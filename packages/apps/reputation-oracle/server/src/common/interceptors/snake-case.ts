94b879b5dc66b7323452fd6f885987d7790e21d5
Francisco López <50665615+flopez7@users.noreply.github.com>
2024-02-22 15:36:04 +0100

    Fix camel case converter for nested objects (#1619)

--- packages/apps/reputation-oracle/server/src/common/interceptors/snake-case.ts
+++ packages/apps/reputation-oracle/server/src/common/interceptors/snake-case.ts
@@ -23,17 +23,23 @@ export class SnakeCaseInterceptor implements NestInterceptor {
     return next.handle().pipe(map((data) => this.transformToSnakeCase(data)));
   }
 
-  private transformToCamelCase(obj: Record<string, any>): Record<string, any> {
-    const result: Record<string, any> = {};
-    for (const key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        const camelCaseKey = key.replace(/_([a-z])/g, (g) =>
-          g[1].toUpperCase(),
-        );
-        result[camelCaseKey] = obj[key];
-      }
+  private transformToCamelCase(obj: any): any {
+    if (Array.isArray(obj)) {
+      return obj.map((item) => this.transformToCamelCase(item));
+    } else if (typeof obj === 'object' && obj !== null) {
+      return Object.keys(obj).reduce(
+        (acc: Record<string, any>, key: string) => {
+          const camelCaseKey = key.replace(/_([a-z])/g, (g) =>
+            g[1].toUpperCase(),
+          );
+          acc[camelCaseKey] = this.transformToCamelCase(obj[key]);
+          return acc;
+        },
+        {},
+      );
+    } else {
+      return obj;
     }
-    return result;
   }
 
   private transformToSnakeCase(obj: any): any {
