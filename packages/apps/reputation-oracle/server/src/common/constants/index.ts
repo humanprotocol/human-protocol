ebc212e3039ed4611345e480b4c15e73b9b549fa
portuu3 <61605646+portuu3@users.noreply.github.com>
2024-02-28 10:39:41 +0100

    Undo signature header change (#1641)

--- packages/apps/reputation-oracle/server/src/common/constants/index.ts
+++ packages/apps/reputation-oracle/server/src/common/constants/index.ts
@@ -22,7 +22,7 @@ export const CVAT_JOB_TYPES = [
   JobRequestType.IMAGE_POINTS,
 ];
 
-export const HEADER_SIGNATURE_KEY = 'Human-Signature';
+export const HEADER_SIGNATURE_KEY = 'human-signature';
 
 export const CURSE_WORDS = [
   '4r5e',
