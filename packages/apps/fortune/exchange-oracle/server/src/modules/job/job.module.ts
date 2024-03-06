8f507d3f7503027c99f00d0c1097f1d3355ff661
portuu3 <adrian.portugues.mas@gmail.com>
2024-03-06 10:01:15 +0100

    remove unnecessary imports

--- packages/apps/fortune/exchange-oracle/server/src/modules/job/job.module.ts
+++ packages/apps/fortune/exchange-oracle/server/src/modules/job/job.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JobController } from './job.controller';
 import { JobService } from './job.service';
--- packages/apps/fortune/exchange-oracle/server/src/modules/webhook/webhook.module.ts
+++ packages/apps/fortune/exchange-oracle/server/src/modules/webhook/webhook.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
