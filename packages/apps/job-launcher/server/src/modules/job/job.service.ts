df92ea2220ee57d55f30f907c09a817b5ea64587
Eugene Voronov <eugene@hmt.ai>
2024-03-15 16:17:51 +0300

    Fixed get details method

--- packages/apps/job-launcher/server/src/modules/job/job.service.ts
+++ packages/apps/job-launcher/server/src/modules/job/job.service.ts
@@ -1197,8 +1197,8 @@ export class JobService {
     } else {
       manifest = manifest as CvatManifestDto;
       specificManifestDetails = {
-        requestType: manifest.annotation.type,
-        submissionsRequired: manifest.annotation.job_size,
+        requestType: manifest.annotation?.type,
+        submissionsRequired: manifest.annotation?.job_size,
       };
     }
 
