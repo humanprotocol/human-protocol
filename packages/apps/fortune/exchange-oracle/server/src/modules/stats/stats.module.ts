77585bfaeadffb2462cb5d8a10e42d3c71c70a0b
portuu3 <adrian.portugues.mas@gmail.com>
2024-03-19 22:22:54 +0100

    exchange oracle fixes

--- packages/apps/fortune/exchange-oracle/server/src/modules/stats/stats.module.ts
+++ packages/apps/fortune/exchange-oracle/server/src/modules/stats/stats.module.ts
@@ -4,9 +4,10 @@ import { AssignmentEntity } from '../assignment/assignment.entity';
 import { StatsService } from './stats.service';
 import { StatsController } from './stats.controller';
 import { AssignmentRepository } from '../assignment/assignment.repository';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AssignmentEntity])],
+  imports: [TypeOrmModule.forFeature([AssignmentEntity]), ConfigModule],
   controllers: [StatsController],
   providers: [StatsService, AssignmentRepository],
   exports: [StatsService],
