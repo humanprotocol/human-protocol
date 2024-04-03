9862e7bfc848ac428d52c4a640457e3a59ec7962
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-04-03 18:13:05 +0300

    fix default echo value

--- packages/examples/cvat/recording-oracle/src/db/__init__.py
+++ packages/examples/cvat/recording-oracle/src/db/__init__.py
@@ -8,7 +8,7 @@ from src.core.config import Config
 DATABASE_URL = Config.postgres_config.connection_url()
 engine = sqlalchemy.create_engine(
     DATABASE_URL,
-    echo="debug" if Config.loglevel <= logging.DEBUG else None,
+    echo="debug" if Config.loglevel <= logging.DEBUG else False,
     connect_args={"options": "-c lock_timeout={:d}".format(Config.postgres_config.lock_timeout)},
 )
 SessionLocal = sessionmaker(autocommit=False, bind=engine)
