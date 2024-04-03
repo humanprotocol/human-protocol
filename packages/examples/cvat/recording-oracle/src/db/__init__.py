90f0eca35488e8b0de0ba724fd4158cda27f3ef5
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-04-03 18:28:50 +0300

    [CVAT-M2] Add more logging to recording oracle (#1813)
    * Add more logs to recording oracle
    
    * fix default echo value

--- packages/examples/cvat/recording-oracle/src/db/__init__.py
+++ packages/examples/cvat/recording-oracle/src/db/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 import sqlalchemy
 from sqlalchemy.orm import declarative_base, sessionmaker
 
@@ -6,6 +8,7 @@ from src.core.config import Config
 DATABASE_URL = Config.postgres_config.connection_url()
 engine = sqlalchemy.create_engine(
     DATABASE_URL,
+    echo="debug" if Config.loglevel <= logging.DEBUG else False,
     connect_args={"options": "-c lock_timeout={:d}".format(Config.postgres_config.lock_timeout)},
 )
 SessionLocal = sessionmaker(autocommit=False, bind=engine)
