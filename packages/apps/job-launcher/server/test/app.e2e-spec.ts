5fc418e94593767c6125a400db365d7198459969
Eugene Voronov <eugene@hmt.ai>
2024-04-05 11:14:00 +0300

    Added restore password e2e workflow

--- packages/apps/job-launcher/server/test/app.e2e-spec.ts
+++ packages/apps/job-launcher/server/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -18,7 +18,7 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(301)
+      .expect('Moved Permanently. Redirecting to /swagger');
   });
 });
