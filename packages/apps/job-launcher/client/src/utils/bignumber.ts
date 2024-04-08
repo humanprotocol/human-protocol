4e3ce8aa1e62bf571608698bd889a13846af168c
Francisco López <francislopez977@gmail.com>
2024-04-08 12:49:28 +0200

    Remove bignumber.ts utils file

--- packages/apps/job-launcher/client/src/utils/bignumber.ts
+++
@@ -1,6 +0,0 @@
-import { ethers, BigNumber } from 'ethers';
-
-export const formatAmount = (amount: string) => {
-  return amount;
-  return ethers.utils.formatUnits(BigNumber.from(amount), 18);
-};
