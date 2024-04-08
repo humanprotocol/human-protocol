5b2c1bbdb495a80b097c9da189c24d325f3dd3a1
Francisco López <francislopez977@gmail.com>
2024-04-08 12:45:50 +0200

    Update jwt times from milliseconds to seconds Remove parseEthers in jl front since it is already done in server

--- packages/apps/job-launcher/client/src/utils/bignumber.ts
+++ packages/apps/job-launcher/client/src/utils/bignumber.ts
@@ -1,5 +1,6 @@
 import { ethers, BigNumber } from 'ethers';
 
 export const formatAmount = (amount: string) => {
+  return amount;
   return ethers.utils.formatUnits(BigNumber.from(amount), 18);
 };
--- packages/apps/reputation-oracle/server/src/common/config/auth-config.service.ts
+++ packages/apps/reputation-oracle/server/src/common/config/auth-config.service.ts
@@ -11,27 +11,21 @@ export class AuthConfigService {
     return this.configService.get<string>('JWT_PUBLIC_KEY', '');
   }
   get accessTokenExpiresIn(): number {
-    return this.configService.get<number>(
-      'JWT_ACCESS_TOKEN_EXPIRES_IN',
-      300000,
-    );
+    return this.configService.get<number>('JWT_ACCESS_TOKEN_EXPIRES_IN', 600);
   }
   get refreshTokenExpiresIn(): number {
-    return this.configService.get<number>(
-      'JWT_REFRESH_TOKEN_EXPIRES_IN',
-      3600000,
-    );
+    return this.configService.get<number>('JWT_REFRESH_TOKEN_EXPIRES_IN', 3600);
   }
   get verifyEmailTokenExpiresIn(): number {
     return this.configService.get<number>(
       'VERIFY_EMAIL_TOKEN_EXPIRES_IN',
-      1800000,
+      86400,
     );
   }
   get forgotPasswordExpiresIn(): number {
     return this.configService.get<number>(
       'FORGOT_PASSWORD_TOKEN_EXPIRES_IN',
-      1800000,
+      86400,
     );
   }
   get hCaptchaSiteKey(): string {
