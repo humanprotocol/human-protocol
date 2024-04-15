c9bdd0ac0877c5e9cd4280275f24b6181a02134d
MicDebBlocky <michal.debek@blockydevs.com>
2024-04-15 13:57:46 +0200

    feat(auth): add worker sign up service

---
+++ packages/apps/human-app/frontend/src/api/servieces/worker/sign-up.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { apiClient } from '@/api/api-client';
+import { apiPaths } from '@/api/api-paths';
+import { routerPaths } from '@/shared/router-paths';
+
+export const signUpDtoSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1),
+});
+
+export type SignUpDto = z.infer<typeof signUpDtoSchema>;
+
+const signUpSuccessResponseSchema = z.unknown();
+
+// function signUpMutationFn(data: SignUpDto) {
+//   return apiClient(apiPaths.worker.signUp.path, {
+//     successSchema: signUpSuccessResponseSchema,
+//   });
+// }
+
+function signUpMutationFn() {
+  return apiClient(apiPaths.test.path, {
+    successSchema: signUpSuccessResponseSchema,
+  });
+}
+
+export function useSignUpMutation() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: signUpMutationFn,
+    onSuccess: async () => {
+      // TODO add correct path
+      navigate(routerPaths.app.path);
+      await queryClient.invalidateQueries();
+    },
+    onError: async () => {
+      await queryClient.invalidateQueries();
+    },
+  });
+}
