74fa0be011daba41e6d4872085921b8efe6a210a
Maksymilian Kozak <maksymilian.kozak@blockydevs.com>
2024-04-22 13:44:23 +0200

    Jobs Mobile- useQuery

--- packages/apps/human-app/frontend/src/pages/worker/jobs/components/avaible-jobs/available-jobs-table-mobile.tsx
+++ packages/apps/human-app/frontend/src/pages/worker/jobs/components/avaible-jobs/available-jobs-table-mobile.tsx
@@ -1,13 +1,16 @@
 import { Grid, List, Paper, Stack } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { ProfileListItem } from '@/pages/operator/components/profile/profile-list-item';
 import { colorPalette } from '@/styles/color-palette';
 import { Button } from '@/components/ui/button';
 import { SearchForm } from '@/pages/playground/table-example/table-search-form';
 import { shortenEscrowAddress } from '../utils/shorten-escrow-address';
-import type { AvailableJobs } from './available-jobs-table-service';
-import { getJobsTableData } from './available-jobs-table-service';
+import {
+  getJobsTableData,
+  type AvailableJobs,
+} from './available-jobs-table-service';
 
 export function AvailableJobsTableMobile() {
   const { t } = useTranslation();
@@ -17,94 +20,84 @@ export function AvailableJobsTableMobile() {
       value: '',
     },
   ]);
-  const [data, setData] = useState<AvailableJobs[]>([]);
-  const [error, setError] = useState('');
+  const { data, isLoading } = useQuery<AvailableJobs[]>({
+    queryKey: ['example', []],
+    queryFn: () => getJobsTableData(),
+  });
+  const [filteredData, setFilteredData] = useState(data);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const jobsData = await getJobsTableData();
-        if (searchEscrowAddress[0].value === '') {
-          setData(jobsData);
-        }
-        if (searchEscrowAddress[0].value) {
-          const filtered = jobsData.filter((item) => {
-            return item.escrowAddress.includes(searchEscrowAddress[0].value);
-          });
-          setData(filtered);
-        }
-      } catch (errorMsg) {
-        setError('Error while fetching data');
-      }
-    };
-
-    fetchData().catch(() => {
-      setError('Error while fetching data');
+    const filtered = data?.filter((item) => {
+      return item.escrowAddress.includes(searchEscrowAddress[0].value);
     });
-  }, [searchEscrowAddress]);
+    setFilteredData(filtered);
+  }, [searchEscrowAddress, data]);
 
   return (
     <>
-      {error ? <>{error}</> : null}
       <SearchForm
         columnId={t('worker.jobs.escrowAddressColumnId')}
         label={t('worker.jobs.searchEscrowAddress')}
         name={t('worker.jobs.searchEscrowAddress')}
-        placeholder="Example address search"
+        placeholder={t('worker.jobs.searchEscrowAddress')}
         updater={setSearchEscrowAddress}
       />
       <Stack flexDirection="column">
-        {data.map((d: AvailableJobs) => (
-          <Paper
-            key={crypto.randomUUID()}
-            sx={{
-              px: '16px',
-              py: '32px',
-              backgroundColor: colorPalette.white,
-              marginBottom: '20px',
-            }}
-          >
-            <List>
-              <ProfileListItem
-                header={t('worker.jobs.jobDescription')}
-                paragraph={d.jobDescription}
-              />
-              <Grid container>
-                <Grid item xs={6}>
-                  <ProfileListItem
-                    header={t('worker.jobs.escrowAddress')}
-                    paragraph={shortenEscrowAddress(d.escrowAddress)}
-                  />
-                  <ProfileListItem
-                    header={t('worker.jobs.rewardAmount')}
-                    paragraph={d.rewardAmount}
-                  />
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          filteredData?.map((d) => (
+            <Paper
+              key={crypto.randomUUID()}
+              sx={{
+                px: '16px',
+                py: '32px',
+                backgroundColor: colorPalette.white,
+                marginBottom: '20px',
+              }}
+            >
+              <List>
+                <ProfileListItem
+                  header={t('worker.jobs.jobDescription')}
+                  paragraph={d.jobDescription}
+                />
+                <Grid container>
+                  <Grid item xs={6}>
+                    <ProfileListItem
+                      header={t('worker.jobs.escrowAddress')}
+                      paragraph={shortenEscrowAddress(d.escrowAddress)}
+                    />
+                    <ProfileListItem
+                      header={t('worker.jobs.rewardAmount')}
+                      paragraph={d.rewardAmount}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <ProfileListItem
+                      header={t('worker.jobs.network')}
+                      paragraph={d.network}
+                    />
+                    <ProfileListItem
+                      header={t('worker.jobs.jobType')}
+                      paragraph={d.jobType}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      color="secondary"
+                      fullWidth
+                      size="small"
+                      type="button"
+                      variant="contained"
+                    >
+                      {t('worker.jobs.selectJob')}
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                  <ProfileListItem
-                    header={t('worker.jobs.network')}
-                    paragraph={d.network}
-                  />
-                  <ProfileListItem
-                    header={t('worker.jobs.jobType')}
-                    paragraph={d.jobType}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    color="secondary"
-                    fullWidth
-                    size="small"
-                    type="button"
-                    variant="contained"
-                  >
-                    {t('worker.jobs.selectJob')}
-                  </Button>
-                </Grid>
-              </Grid>
-            </List>
-          </Paper>
-        ))}
+              </List>
+            </Paper>
+          ))
+        )}
       </Stack>
     </>
   );
