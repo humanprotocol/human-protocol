14b128687bc59be6a56a7b557446704787be043c
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-04-26 19:09:49 +0300

    [CVAT-M2] Finish escrow if only unverifiable jobs left (#1929)
    * Finish escrow if only unverifiable jobs left
    
    * Add logging

--- packages/examples/cvat/recording-oracle/src/handlers/process_intermediate_results.py
+++ packages/examples/cvat/recording-oracle/src/handlers/process_intermediate_results.py
@@ -1021,7 +1021,16 @@ def process_intermediate_results(
             )
         )
         should_complete = True
-    elif not rejected_jobs:
+    elif len(rejected_jobs) == unverifiable_jobs_count:
+        if unverifiable_jobs_count:
+            logger.info(
+                "Validation for escrow_address={}: "
+                "only unverifiable assignments left ({}), stopping annotation".format(
+                    escrow_address,
+                    unverifiable_jobs_count,
+                )
+            )
+
         should_complete = True
 
     if not should_complete:
