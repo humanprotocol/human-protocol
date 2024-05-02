ac45c72cb6c5ef162509a5e373c76a7b620342ed
mrhouzlane <rhouzm@gmail.com>
2024-05-02 19:19:16 +0100

    fix : disable networks that do not have RPC URL

--- packages/apps/job-launcher/client/src/constants/chains.ts
+++ packages/apps/job-launcher/client/src/constants/chains.ts
@@ -4,13 +4,13 @@ export const IS_MAINNET =
   import.meta.env.VITE_APP_ENVIRONMENT.toLowerCase() === 'mainnet';
 export const IS_TESTNET = !IS_MAINNET;
 
-export let SUPPORTED_CHAIN_IDS: ChainId[];
+let initialSupportedChainIds: ChainId[];
 switch (import.meta.env.VITE_APP_ENVIRONMENT.toLowerCase()) {
   case 'mainnet':
-    SUPPORTED_CHAIN_IDS = [ChainId.POLYGON];
+    initialSupportedChainIds = [ChainId.POLYGON];
     break;
   case 'testnet':
-    SUPPORTED_CHAIN_IDS = [
+    initialSupportedChainIds = [
       ChainId.BSC_TESTNET,
       ChainId.POLYGON_AMOY,
       ChainId.SEPOLIA,
@@ -18,16 +18,9 @@ switch (import.meta.env.VITE_APP_ENVIRONMENT.toLowerCase()) {
     break;
   case 'localhost':
   default:
-    SUPPORTED_CHAIN_IDS = [ChainId.LOCALHOST];
+    initialSupportedChainIds = [ChainId.LOCALHOST];
     break;
 }
-
-export const CHAIN_ID_BY_NAME: Record<string, number> = {
-  'Polygon Amoy': ChainId.POLYGON_AMOY,
-  'Binance Smart Chain': ChainId.BSC_MAINNET,
-  'Ethereum Sepolia': ChainId.SEPOLIA,
-  Localhost: ChainId.LOCALHOST,
-};
 
 export const RPC_URLS: {
   [chainId in ChainId]?: string;
@@ -48,4 +41,15 @@ export const RPC_URLS: {
   [ChainId.CELO_ALFAJORES]:
     import.meta.env.VITE_APP_RPC_URL_CELO_ALFAJORES || '',
   [ChainId.CELO]: import.meta.env.VITE_APP_RPC_URL_CELO || '',
+};
+
+export const SUPPORTED_CHAIN_IDS: ChainId[] = initialSupportedChainIds.filter(
+  (chainId) => Boolean(RPC_URLS[chainId]),
+);
+
+export const CHAIN_ID_BY_NAME: Record<string, number> = {
+  'Polygon Amoy': ChainId.POLYGON_AMOY,
+  'Binance Smart Chain': ChainId.BSC_MAINNET,
+  'Ethereum Sepolia': ChainId.SEPOLIA,
+  Localhost: ChainId.LOCALHOST,
 };
