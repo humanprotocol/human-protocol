6cb0263c3c7ddd3ef6d7a38deca3406eecdc8347
portuu3 <61605646+portuu3@users.noreply.github.com>
2024-05-13 23:37:04 +0200

    fix credentialling routes (#2001)

--- packages/apps/reputation-oracle/server/src/modules/reputation/reputation.interface.ts
+++ packages/apps/reputation-oracle/server/src/modules/reputation/reputation.interface.ts
@@ -1,5 +1,5 @@
 import { ChainId } from '@human-protocol/sdk';
-import { CvatManifestDto, FortuneManifestDto } from 'src/common/dto/manifest';
+import { CvatManifestDto, FortuneManifestDto } from '../../common/dto/manifest';
 
 export interface RequestAction {
   assessWorkerReputationScores: (
