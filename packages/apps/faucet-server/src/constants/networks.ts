8f23a707b4f1eeb8fcc201a0a78c778ea4ed82f8
portuu3 <61605646+portuu3@users.noreply.github.com>
2024-05-13 15:34:44 +0200

    [Faucet] Remove mainnet networks from faucet (#1997)
    * remove mainnet networks
    
    * add logs
    
    * remove logs

--- packages/apps/faucet-server/src/constants/networks.ts
+++ packages/apps/faucet-server/src/constants/networks.ts
@@ -8,38 +8,20 @@ export interface IFaucetData {
 export const FAUCET_NETWORKS: {
   [chainId in ChainId]?: IFaucetData;
 } = {
-  [ChainId.MAINNET]: {
-    rpcUrl: process.env.RPC_URL_MAINNET || '',
-  },
   [ChainId.SEPOLIA]: {
     rpcUrl: process.env.RPC_URL_SEPOLIA || '',
-  },
-  [ChainId.BSC_MAINNET]: {
-    rpcUrl: process.env.RPC_URL_BSC_MAINNET || '',
   },
   [ChainId.BSC_TESTNET]: {
     rpcUrl: process.env.RPC_URL_BSC_TESTNET || '',
   },
-  [ChainId.POLYGON]: {
-    rpcUrl: process.env.RPC_URL_POLYGON || '',
-  },
   [ChainId.POLYGON_AMOY]: {
     rpcUrl: process.env.RPC_URL_POLYGON_AMOY || '',
-  },
-  [ChainId.MOONBEAM]: {
-    rpcUrl: process.env.RPC_URL_MOONBEAM || '',
   },
   [ChainId.MOONBASE_ALPHA]: {
     rpcUrl: process.env.RPC_URL_MOONBASE_ALPHA || '',
   },
-  [ChainId.AVALANCHE]: {
-    rpcUrl: process.env.RPC_URL_AVALANCHE || '',
-  },
   [ChainId.AVALANCHE_TESTNET]: {
     rpcUrl: process.env.RPC_URL_AVALANCHE_TESTNET || '',
-  },
-  [ChainId.CELO]: {
-    rpcUrl: process.env.RPC_URL_CELO || '',
   },
   [ChainId.CELO_ALFAJORES]: {
     rpcUrl: process.env.RPC_URL_CELO_ALFAJORES || '',
@@ -50,9 +32,6 @@ export const FAUCET_NETWORKS: {
   [ChainId.SKALE]: {
     rpcUrl: process.env.RPC_URL_SKALE || '',
     faucetAddress: '0xb51a0E538c76C82e76757dc6D5a3938136C03c0C',
-  },
-  [ChainId.XLAYER]: {
-    rpcUrl: process.env.RPC_URL_XLAYER || '',
   },
   [ChainId.LOCALHOST]: {
     rpcUrl: process.env.RPC_PORT
