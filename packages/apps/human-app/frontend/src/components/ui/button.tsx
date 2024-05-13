18ae231a82033d0bc6ec0d1d72f265ef8eefa1f0
MicDebBlocky <michal.debek@blockydevs.com>
2024-05-13 09:42:30 +0200

    fix(haf-52): add button loading state

--- packages/apps/human-app/frontend/src/components/ui/button.tsx
+++ packages/apps/human-app/frontend/src/components/ui/button.tsx
@@ -1,19 +1,21 @@
 import type { Link, LinkProps } from 'react-router-dom';
 import { forwardRef } from 'react';
-import type { LoadingButtonProps } from '@mui/lab';
-import { LoadingButton } from '@mui/lab';
+import MuiButton from '@mui/material/Button';
+import type { ButtonProps } from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 
-export type CustomButtonProps = LoadingButtonProps &
+export type CustomButtonProps = ButtonProps &
   Partial<LinkProps> & {
     component?: typeof Link;
+    loading?: boolean;
   };
 
 export const Button = forwardRef<HTMLButtonElement, CustomButtonProps>(
-  ({ children, ...props }, ref) => {
+  ({ children, loading, disabled, ...props }, ref) => {
     return (
-      <LoadingButton ref={ref} {...props}>
-        {children}
-      </LoadingButton>
+      <MuiButton ref={ref} {...props} disabled={disabled || loading}>
+        <>{loading ? <CircularProgress size={24} /> : children}</>
+      </MuiButton>
     );
   }
 );
