5118b8f15b9a5e03d2a019d46e9377be77880306
mrhouzlane <rhouzm@gmail.com>
2024-05-14 15:39:02 +0700

    fix swagger issue

--- packages/apps/reputation-oracle/server/src/modules/credentials/credential.dto.ts
+++ packages/apps/reputation-oracle/server/src/modules/credentials/credential.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, IsOptional, IsDateString } from 'class-validator';
 
 export class CreateCredentialDto {
+  @ApiProperty()
   @IsString()
   public reference: string;
 
+  @ApiProperty()
   @IsString()
   public description: string;
 
+  @ApiProperty()
   @IsString()
   public url: string;
 
+  @ApiProperty()
   @IsDateString()
   public startsAt: Date;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsDateString()
   public expiresAt?: Date;
