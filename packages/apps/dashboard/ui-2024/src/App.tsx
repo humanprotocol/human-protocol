ad591dd1045c2e67d4f5ac171b430b53506c810c
Piotr Rodak <piotr.rodak@blockydevs.com>
2024-05-16 13:24:50 +0200

    Add routing to ui

--- packages/apps/dashboard/ui-2024/src/App.tsx
+++ packages/apps/dashboard/ui-2024/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/details/:id" element={<div>Details</div>} />
+        <Route path="/graph" element={<div>Graph</div>} />
+        <Route path="/search/:id" element={<div>Search</div>} />
+        <Route path="*" element={<div>Not find</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
