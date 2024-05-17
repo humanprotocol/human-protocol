865e2f803d77c15af7012fb1c959b474107865a9
Francisco López <francislopez977@gmail.com>
2024-05-17 17:36:50 +0200

    Add minimum fee to env-schema

--- packages/apps/job-launcher/server/src/common/config/env-schema.ts
+++ packages/apps/job-launcher/server/src/common/config/env-schema.ts
@@ -8,6 +8,7 @@ export const envValidator = Joi.object({
   FE_URL: Joi.string(),
   SESSION_SECRET: Joi.string(),
   MAX_RETRY_COUNT: Joi.number(),
+  MINIMUM_FEE_USD: Joi.number(),
   // Auth
   JWT_PRIVATE_KEY: Joi.string().required(),
   JWT_PUBLIC_KEY: Joi.string().required(),
