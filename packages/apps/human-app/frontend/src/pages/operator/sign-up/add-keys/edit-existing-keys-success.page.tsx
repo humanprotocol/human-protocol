07224d65d68e093a5c3f6efe9eaf0941f30e20d0
KacperKoza343 <kacper.koza@blockydevs.com>
2024-05-20 11:11:49 +0200

    feat(app/operator): add preapare siganture for web3 auth

--- packages/apps/human-app/frontend/src/pages/operator/sign-up/add-keys/edit-existing-keys-success.page.tsx
+++ packages/apps/human-app/frontend/src/pages/operator/sign-up/add-keys/edit-existing-keys-success.page.tsx
@@ -1,11 +1,49 @@
 import { t } from 'i18next';
 import { Grid, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { PageCard } from '@/components/ui/page-card';
+import {
+  PageCard,
+  PageCardError,
+  PageCardLoader,
+} from '@/components/ui/page-card';
 import { Button } from '@/components/ui/button';
 import { routerPaths } from '@/router/router-paths';
+import { useConnectedWallet } from '@/auth-web3/use-connected-wallet';
+import type { SignatureData } from '@/api/servieces/operator/prepare-signature';
+import {
+  PrepareSignatureType,
+  usePrepareSignature,
+} from '@/api/servieces/operator/prepare-signature';
+import { defaultErrorMessage } from '@/shared/helpers/default-error-message';
 
 export function EditExistingKeysSuccessPage() {
+  const { address, signMessage } = useConnectedWallet();
+  const {
+    data: signatureData,
+    isError: isSignatureDataError,
+    error: errorSignatureDataError,
+    isPending: isSignatureDataPending,
+  } = usePrepareSignature({
+    address,
+    type: PrepareSignatureType.SignUp,
+  });
+
+  const createSignature = async (data: SignatureData) => {
+    return signMessage(JSON.stringify(data));
+  };
+
+  if (isSignatureDataError) {
+    return (
+      <PageCardError
+        errorMessage={defaultErrorMessage(errorSignatureDataError)}
+      />
+    );
+  }
+
+  if (isSignatureDataPending) {
+    return <PageCardLoader />;
+  }
+
   return (
     <PageCard title={t('operator.editExistingKeysSuccess.title')}>
       <Grid container gap="2rem" marginTop="2rem">
@@ -20,6 +58,10 @@ export function EditExistingKeysSuccessPage() {
         <Button
           component={Link}
           fullWidth
+          loading={isSignatureDataPending}
+          onClick={() => {
+            void createSignature(signatureData);
+          }}
           to={routerPaths.homePage}
           variant="contained"
         >
