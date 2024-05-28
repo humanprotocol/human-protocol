24342876af6848650d60b5c3249b5ec42178f53f
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-05-28 15:16:17 +0300

    [CVAT] Fix escrow status checks (#2064)
    * Avoid failing the whole transaction on project lock attempts, optimize repeated escrow validation calls, skip locked
    
    * Fix repeated skeleton escrow completion checks if the escrow was partially rejected
    
    * Fix invalid locking error checks
    
    * Remove outdated test
    
    * Update tests

--- packages/examples/cvat/exchange-oracle/src/db/errors.py
+++ packages/examples/cvat/exchange-oracle/src/db/errors.py
@@ -4,3 +4,5 @@ if db_engine.driver != "psycopg2":
     raise NotImplementedError
 
 from psycopg2.errors import LockNotAvailable
+
+# These errors can be found, e.g., in the .orig field of sqlalchemy errors
