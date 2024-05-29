40fc795988f2f654f0fa3578366cca657c4879de
Mateusz Marcinkowski <mateusz.marcinkowski@blockydevs.com>
2024-05-29 10:24:06 +0200

    HAF-66: Add captcha to sign worker sign up

--- packages/apps/human-app/frontend/src/components/h-captcha.tsx
+++ packages/apps/human-app/frontend/src/components/h-captcha.tsx
@@ -1,6 +1,9 @@
 import { useRef } from 'react';
 import HCaptcha from '@hcaptcha/react-hcaptcha';
+import { useFormContext } from 'react-hook-form';
+import { Typography } from '@mui/material';
 import { env } from '@/shared/env';
+import { colorPalette } from '@/styles/color-palette';
 
 interface CaptchaProps {
   setCaptchaToken: (token: string) => void;
@@ -26,5 +29,30 @@ export function Captcha({ setCaptchaToken }: CaptchaProps) {
       ref={captchaRef}
       sitekey={env.VITE_H_CAPTCHA_SITE_KEY}
     />
+  );
+}
+
+interface FormCaptchaProps {
+  name: string;
+}
+
+export function FormCaptcha({ name }: FormCaptchaProps) {
+  const { setValue, formState } = useFormContext<Record<string, unknown>>();
+
+  function setCaptchaToken(token: string) {
+    setValue(name, token);
+  }
+
+  return (
+    <div>
+      <Captcha setCaptchaToken={setCaptchaToken} />
+      <Typography
+        color={colorPalette.error.main}
+        component="div"
+        variant="helperText"
+      >
+        {formState.errors[name]?.message}
+      </Typography>
+    </div>
   );
 }
