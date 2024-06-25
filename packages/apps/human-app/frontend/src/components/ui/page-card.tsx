1e312b3afb5a42692e2c9f5eeac8dd09727ff124
KacperKoza343 <kacper.koza@blockydevs.com>
2024-06-25 11:50:14 +0200

    fix(app): fix error page and evm address displaying

--- packages/apps/human-app/frontend/src/components/ui/page-card.tsx
+++ packages/apps/human-app/frontend/src/components/ui/page-card.tsx
@@ -255,7 +255,7 @@ export function PageCardError({
   }, []);
 
   return (
-    <Grid container sx={sx}>
+    <Grid container sx={{ ...sx, gap: '2rem' }}>
       {children ? (
         children
       ) : (
