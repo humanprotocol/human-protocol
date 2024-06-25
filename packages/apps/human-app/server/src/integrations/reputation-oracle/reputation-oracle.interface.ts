02df77858f332769f7de36b11f49f12f6b3b1c4e
maciej.nabialek <maciej.nabialek@blockydevs.com>
2024-06-25 12:48:52 +0200

    Fixes after client review

--- packages/apps/human-app/server/src/integrations/reputation-oracle/reputation-oracle.interface.ts
+++ packages/apps/human-app/server/src/integrations/reputation-oracle/reputation-oracle.interface.ts
@@ -1,8 +1,5 @@
 import { SignupWorkerData } from '../../modules/user-worker/model/worker-registration.model';
-import {
-  SigninOperatorData,
-  SignupOperatorData,
-} from '../../modules/user-operator/model/operator-registration.model';
+import { SignupOperatorData } from '../../modules/user-operator/model/operator-registration.model';
 import { SigninWorkerData } from '../../modules/user-worker/model/worker-signin.model';
 import { EmailVerificationData } from '../../modules/email-confirmation/model/email-verification.model';
 import { ResendEmailVerificationData } from '../../modules/email-confirmation/model/resend-email-verification.model';
@@ -11,6 +8,7 @@ import { DisableOperatorData } from '../../modules/disable-operator/model/disabl
 import { RegisterAddressData } from '../../modules/register-address/model/register-address.model';
 import { RestorePasswordData } from '../../modules/password-reset/model/restore-password.model';
 import { TokenRefreshData } from '../../modules/token-refresh/model/token-refresh.model';
+import { SigninOperatorData } from '../../modules/user-operator/model/operator-signin.model';
 class Empty {}
 
 export type RequestDataType =
