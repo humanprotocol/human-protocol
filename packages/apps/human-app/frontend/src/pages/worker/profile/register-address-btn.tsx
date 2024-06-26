c0bf8fe818d7b99ad18eb027e98bba76c2bb63c9
KacperKoza343 <165884432+KacperKoza343@users.noreply.github.com>
2024-06-26 18:06:38 +0200

    fix(app/worker/profile): refetch KVStore data after setting address (#163)

--- packages/apps/human-app/frontend/src/pages/worker/profile/register-address-btn.tsx
+++ packages/apps/human-app/frontend/src/pages/worker/profile/register-address-btn.tsx
@@ -2,19 +2,27 @@ import { t } from 'i18next';
 import { Button } from '@/components/ui/button';
 import { useRegisterAddress } from '@/api/servieces/worker/use-register-address';
 import { useRegisterAddressNotifications } from '@/hooks/use-register-address-notifications';
+import { useGetOnChainRegisteredAddress } from '@/api/servieces/worker/get-on-chain-registered-address';
 
 export function RegisterAddress({ disabled }: { disabled: boolean }) {
   const { onSuccess, onError } = useRegisterAddressNotifications();
+  const {
+    refetch: refetchKVStoreAddressData,
+    isPending: isKVStoreAddressDataPending,
+  } = useGetOnChainRegisteredAddress();
   const { mutate, isPending } = useRegisterAddress({
     onError,
-    onSuccess,
+    onSuccess: () => {
+      void refetchKVStoreAddressData();
+      onSuccess();
+    },
   });
 
   return (
     <Button
       disabled={disabled}
       fullWidth
-      loading={isPending}
+      loading={isPending || isKVStoreAddressDataPending}
       onClick={mutate.bind(undefined, undefined)}
       variant="contained"
     >
