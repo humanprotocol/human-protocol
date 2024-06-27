784b18fd857f372f8c6c597134c5e01b82e73e3d
macnablocky <143715306+macnablocky@users.noreply.github.com>
2024-06-27 14:43:54 +0200

    Adds optional validator to jobsDiscoveryParamsDto.sort_field (#168)
    * Adds optional validator to jobsDiscoveryParamsDto.sort_field
    
    * Adds optional validator to jobsDiscoveryParamsDto.job_type

--- packages/apps/human-app/server/src/modules/jobs-discovery/model/jobs-discovery.model.ts
+++ packages/apps/human-app/server/src/modules/jobs-discovery/model/jobs-discovery.model.ts
@@ -30,11 +30,13 @@ export class JobsDiscoveryParamsDto extends PageableDto {
   @ApiPropertyOptional()
   chain_id?: number;
   @AutoMap()
+  @IsOptional()
   @IsEnum(JobDiscoverySortField)
   @ApiPropertyOptional({ enum: JobDiscoverySortField })
   sort_field?: JobDiscoverySortField;
   @AutoMap()
   @IsString()
+  @IsOptional()
   @ApiPropertyOptional()
   job_type?: string;
   @AutoMap()
