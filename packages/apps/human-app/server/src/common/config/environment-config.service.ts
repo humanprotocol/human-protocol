2249d8b2e604ef97b5a3620f9f3d92e081efcdae
macnablocky <143715306+macnablocky@users.noreply.github.com>
2024-07-05 16:50:52 +0200

    updates default allowed headers (#199)

--- packages/apps/human-app/server/src/common/config/environment-config.service.ts
+++ packages/apps/human-app/server/src/common/config/environment-config.service.ts
@@ -6,7 +6,8 @@ const DEFAULT_CACHE_TTL_USER_STATS = 15 * 60;
 const DEFAULT_CACHE_TTL_ORACLE_DISCOVERY = 24 * 60 * 60;
 const DEFAULT_CACHE_TTL_DAILY_HMT_SPENT = 24 * 60 * 60;
 const DEFAULT_CORS_ALLOWED_ORIGIN = 'http://localhost:5173';
-const DEFAULT_CORS_ALLOWED_HEADERS = 'Content-Type, Accept';
+const DEFAULT_CORS_ALLOWED_HEADERS =
+  'Content-Type,Authorization,X-Requested-With,Accept,Origin';
 const DEFAULT_CACHE_TTL_EXCHANGE_ORACLE_URL = 24 * 60 * 60;
 @Injectable()
 export class EnvironmentConfigService {
