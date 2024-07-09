d9612e9ca73880b5a45bfa0c318d15945d049a34
maya <maria@cvat.ai>
2024-07-09 11:42:25 +0200

    [Recording oracle] Define network based on configuration && update public key in kvstore if key hash is invalid

--- packages/examples/cvat/recording-oracle/run.py
+++ packages/examples/cvat/recording-oracle/run.py
@@ -1,6 +1,6 @@
 import uvicorn
 from human_protocol_sdk.constants import KVStoreKeys
-from human_protocol_sdk.kvstore import KVStoreClient
+from human_protocol_sdk.kvstore import KVStoreClient, KVStoreClientError
 
 from src.chain.web3 import get_web3
 from src.core.config import Config
@@ -8,14 +8,23 @@ from src.core.config import Config
 
 def set_public_key_to_kvstore() -> None:
     if Config.encryption_config.pgp_public_key_url:
-        w3 = get_web3(Config.polygon_amoy.chain_id)
-        kvstore_client = KVStoreClient(w3)
+        for network_config in Config.get_network_configs(only_configured=True):
+            w3 = get_web3(network_config.chain_id)
+            kvstore_client = KVStoreClient(w3)
 
-        if not kvstore_client.get_public_key(Config.polygon_amoy.addr):
-            kvstore_client.set_file_url_and_hash(
-                Config.encryption_config.pgp_public_key_url,
-                key=KVStoreKeys.public_key.value,
-            )
+            is_pub_key_stored = False
+            try:
+                is_pub_key_stored = kvstore_client.get_public_key(network_config.addr)
+            except KVStoreClientError as ex:
+                if "Invalid hash" not in str(ex):
+                    raise
+                is_pub_key_stored = False
+
+            if not is_pub_key_stored:
+                kvstore_client.set_file_url_and_hash(
+                    Config.encryption_config.pgp_public_key_url,
+                    key=KVStoreKeys.public_key.value,
+                )
 
 
 if __name__ == "__main__":
