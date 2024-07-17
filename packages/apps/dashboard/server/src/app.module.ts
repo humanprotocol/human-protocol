eb4339bacde1ced0dc4050a0ff95880f9d183855
Sergey Dzeranov <sergey.dzeranov@gmail.com>
2024-07-17 18:46:24 +0200

    Added `/stats/hcaptcha/daily` and `/stats/hcaptcha/general` endpoints

--- packages/apps/dashboard/server/src/app.module.ts
+++ packages/apps/dashboard/server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { Module } from '@nestjs/common';
 
 import * as Joi from 'joi';
@@ -8,7 +9,6 @@ import { AppController } from './app.controller';
 import { CacheFactoryConfig } from './common/config/cache-factory.config';
 import { CommonConfigModule } from './common/config/config.module';
 import { DetailsModule } from './modules/details/details.module';
-import { RedisModule } from './modules/redis/redis.module';
 import { StatsModule } from './modules/stats/stats.module';
 
 @Module({
@@ -22,12 +22,13 @@ import { StatsModule } from './modules/stats/stats.module';
         HOST: Joi.string().required(),
         PORT: Joi.number().port().default(3000),
         SUBGRAPH_API_KEY: Joi.string().required(),
+        HCAPTCHA_API_KEY: Joi.string().required(),
       }),
     }),
     CacheModule.registerAsync(CacheFactoryConfig),
     CommonConfigModule,
     DetailsModule,
-    RedisModule,
+    ScheduleModule.forRoot(),
     StatsModule,
   ],
   controllers: [AppController],
--- packages/apps/dashboard/server/src/modules/stats/dto/hmt-price.dto.ts
+++ packages/apps/dashboard/server/src/modules/stats/dto/hmt-price.dto.ts
@@ -1,8 +1,8 @@
-import { IsNumber } from 'class-validator';
+import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class HmtPriceDto {
   @ApiProperty({ example: 0.123 })
-  @IsNumber()
+  @IsString()
   public hmtPrice: number;
 }
