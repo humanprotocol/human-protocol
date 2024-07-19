1cc7919f6e27e503509e6a39dfec3057c73152c5
Eugene Voronov <eugene@hmt.ai>
2024-07-19 13:44:40 +0300

    Added comment

--- packages/apps/reputation-oracle/server/src/modules/user/site-key.entity.ts
+++ packages/apps/reputation-oracle/server/src/modules/user/site-key.entity.ts
@@ -4,6 +4,9 @@ import { BaseEntity } from '../../database/base.entity';
 import { UserEntity } from './user.entity';
 import { SiteKeyType } from '../../common/enums';
 
+// TypeORM doesn't natively support partial unique indices.
+// To ensure a user can only have one record in the site_keys table with the type 'hcaptcha',
+// we enforce a partial unique index at the database level.
 @Entity({ schema: NS, name: 'site_keys' })
 @Unique(['siteKey', 'type', 'user'])
 export class SiteKeyEntity extends BaseEntity {
