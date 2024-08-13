7fe1cb1ddb6cd0a6a9ec33c059a56554b15ab524
eugenvoronov <104138627+eugenvoronov@users.noreply.github.com>
2024-08-13 11:07:40 +0300

    Implemented qualification module (#2300)
    * Implemented qualification module
    
    * Used kvStore value, fixed sdk
    
    * Implemented new test cases
    
    * Implemented KVStore get method
    
    * Use constant for kvstore key
    
    ---------
    
    Co-authored-by: portuu3 <adrian.portugues.mas@gmail.com>

---
+++ packages/apps/job-launcher/server/src/modules/qualification/qualification.service.ts
@@ -0,0 +1,52 @@
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { QualificationDto } from './qualification.dto';
+import { firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
+import { Web3ConfigService } from '../../common/config/web3-config.service';
+import { ControlledError } from '../../common/errors/controlled';
+import { ErrorQualification, ErrorWeb3 } from '../../common/constants/errors';
+import { ChainId, KVStoreKeys, KVStoreUtils } from '@human-protocol/sdk';
+
+@Injectable()
+export class QualificationService {
+  private readonly logger = new Logger(QualificationService.name);
+
+  constructor(
+    private httpService: HttpService,
+    private readonly web3ConfigService: Web3ConfigService,
+  ) {}
+
+  public async getQualifications(): Promise<QualificationDto[]> {
+    try {
+      const reputationOracleUrl = await KVStoreUtils.get(
+        ChainId.POLYGON_AMOY,
+        this.web3ConfigService.reputationOracleAddress,
+        KVStoreKeys.url,
+      );
+
+      if (!reputationOracleUrl) {
+        throw new ControlledError(
+          ErrorWeb3.ReputationOracleUrlNotSet,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      const { data } = await firstValueFrom(
+        this.httpService.get<QualificationDto[]>(
+          `${reputationOracleUrl}/qualification`,
+        ),
+      );
+
+      return data;
+    } catch (error) {
+      if (error instanceof ControlledError) {
+        throw error;
+      } else {
+        throw new ControlledError(
+          ErrorQualification.FailedToFetchQualifications,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    }
+  }
+}
