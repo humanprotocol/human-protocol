a4edfeda9113420e75b3c629a62df4548cc5b832
KacperKoza343 <kacper.koza@blockydevs.com>
2024-08-14 14:21:48 +0200

    fix(app/sign-up-page): fix password regex missmatch

--- packages/apps/human-app/frontend/src/shared/helpers/regex.ts
+++ packages/apps/human-app/frontend/src/shared/helpers/regex.ts
@@ -3,9 +3,8 @@ export const password8Chars =
 export const passwordUppercase = /(?=.*[A-Z])/;
 export const passwordLowercase = /(?=.*[a-z])/;
 export const passwordNumeric = /(?=.*[0-9])/;
-export const passwordSpecialCharacter =
-  /(?=.*[$^*.[\]{}()?"!@#%&/\\,><':;|_~`=+-])/;
+export const passwordSpecialCharacter = /[!@#$%^&+=*]/; // allowed characters: @#$%^&+=!*]
 
-// this is sum of all above regular expressions
+// this is sum of all above regular expressions that matches with reputation oracle 'auth.dto.ts'
 export const passwordRegex =
-  /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$^*.[\]{}()?"!@#%&/\\,><':;|_~`=+-])[A-Za-z0-9$^*.[\]{}()?"!@#%&/\\,><':;|_~`=+-]{8,}$/;
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&+=*])(?=.{8,})/;
