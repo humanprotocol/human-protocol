0c4edf6a7754ff7ff475d5fb7ab34721431d3463
eugenvoronov <104138627+eugenvoronov@users.noreply.github.com>
2024-08-15 14:17:09 +0300

    [Job launcher] Get available oracles (#2388)
    * Updated web3 service
    
    * Implemented get available oracles logic
    
    * Updated description
    
    * Updated query params, removed pipe

--- packages/apps/job-launcher/server/src/modules/web3/web3.controller.ts
+++ packages/apps/job-launcher/server/src/modules/web3/web3.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UsePipes } from '@nestjs/common';
 import { Public } from '../../common/decorators';
-import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiResponse, ApiOperation, ApiQuery } from '@nestjs/swagger';
 import { Web3Service } from './web3.service';
 import { ChainId } from '@human-protocol/sdk';
 import { NetworkConfigService } from '../../common/config/network-config.service';
+import { AvailableOraclesDto, GetAvailableOraclesDto } from './web3.dto';
 
 @ApiTags('Web3')
 @Controller('/web3')
@@ -49,5 +50,29 @@ export class Web3Controller {
   @Get('/operator-address')
   getOperatorAddress(): string {
     return this.web3Service.getOperatorAddress();
+  }
+
+  @ApiOperation({
+    summary: 'Get available oracles',
+    description:
+      'Fetch the list of available oracles based on chain id, job type and reputation oracle address.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of available oracles',
+    type: AvailableOraclesDto,
+  })
+  @Public()
+  @Get('/available-oracles')
+  async getAvailableOracles(
+    @Query() query: GetAvailableOraclesDto,
+  ): Promise<AvailableOraclesDto> {
+    const { chainId, jobType, reputationOracleAddress } = query;
+
+    return this.web3Service.getAvailableOracles(
+      chainId,
+      jobType,
+      reputationOracleAddress,
+    );
   }
 }
---
+++ packages/apps/job-launcher/server/src/modules/web3/web3.dto.ts
@@ -0,0 +1,36 @@
+import { ChainId, IOperator } from '@human-protocol/sdk';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class OracleDataDto implements IOperator {
+  address: string;
+  role?: string;
+  url?: string;
+  jobTypes?: string[];
+}
+
+export class AvailableOraclesDto {
+  @ApiProperty({
+    description: 'List of addresses of exchange oracles',
+  })
+  exchangeOracles: string[];
+
+  @ApiProperty({
+    description: 'List of addresses of recording oracles',
+  })
+  recordingOracles: string[];
+}
+
+export class GetAvailableOraclesDto {
+  @ApiProperty({ name: 'chain_id' })
+  @IsString()
+  chainId: ChainId;
+
+  @ApiProperty({ name: 'job_type' })
+  @IsString()
+  jobType: string;
+
+  @ApiProperty({ name: 'reputation_oracle_address' })
+  @IsString()
+  reputationOracleAddress: string;
+}
