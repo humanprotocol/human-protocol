45aa34ecc7050ef727676f83c4b66579532aa15f
Francisco López <francislopez977@gmail.com>
2024-08-19 15:16:25 +0200

    Avoid NoneType object has no attribute get errors

--- packages/sdk/python/human-protocol-sdk/human_protocol_sdk/operator/operator_utils.py
+++ packages/sdk/python/human-protocol-sdk/human_protocol_sdk/operator/operator_utils.py
@@ -197,10 +197,15 @@ class OperatorUtils:
             query=get_leaders_query(filter),
             params={"role": filter.role},
         )
-        leaders_raw = leaders_data["data"]["leaders"]
-
-        if not leaders_raw:
+        if (
+            not leaders_data
+            or "data" not in leaders_data
+            or "leaders" not in leaders_data["data"]
+            or not leaders_data["data"]["leaders"]
+        ):
             return []
+
+        leaders_raw = leaders_data["data"]["leaders"]
 
         job_types = []
         if isinstance(job_types, str):
@@ -283,10 +288,16 @@ class OperatorUtils:
             query=get_leader_query,
             params={"address": leader_address.lower()},
         )
-        leader = leader_data["data"]["leader"]
 
-        if not leader:
+        if (
+            not leader_data
+            or "data" not in leader_data
+            or "leader" not in leader_data["data"]
+            or not leader_data["data"]["leader"]
+        ):
             return None
+
+        leader = leader_data["data"]["leader"]
 
         return LeaderData(
             chain_id=chain_id,
@@ -360,7 +371,13 @@ class OperatorUtils:
             params={"address": address.lower(), "role": role},
         )
 
-        if not reputation_network_data["data"]["reputationNetwork"]:
+        if (
+            not reputation_network_data
+            or "data" not in reputation_network_data
+            or "reputationNetwork" not in reputation_network_data["data"]
+            or "operators" not in reputation_network_data["data"]["reputationNetwork"]
+            or not reputation_network_data["data"]["reputationNetwork"]["operators"]
+        ):
             return []
 
         operators = reputation_network_data["data"]["reputationNetwork"]["operators"]
@@ -417,8 +434,13 @@ class OperatorUtils:
             query=get_reward_added_events_query,
             params={"slasherAddress": slasher.lower()},
         )
-
-        if not reward_added_events_data["data"]["rewardAddedEvents"]:
+        print(reward_added_events_data)
+        if (
+            not reward_added_events_data
+            or "data" not in reward_added_events_data
+            or "rewardAddedEvents" not in reward_added_events_data["data"]
+            or not reward_added_events_data["data"]["rewardAddedEvents"]
+        ):
             return []
 
         reward_added_events = reward_added_events_data["data"]["rewardAddedEvents"]
