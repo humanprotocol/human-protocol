a0b7649c7d2562134c8274d4edaebd4a3cf42ce1
Francisco López <francislopez977@gmail.com>
2024-08-20 10:40:38 +0200

    Fix bytes problem in statistics clients

--- packages/sdk/python/human-protocol-sdk/human_protocol_sdk/gql/statistics.py
+++ packages/sdk/python/human-protocol-sdk/human_protocol_sdk/gql/statistics.py
@@ -51,27 +51,27 @@ fragment EventDayDataFields on EventDayData {
 }
 """
 
-get_hmtoken_statistics_query = """
+hmt_statistics_id = "hmt-statistics-id".encode("utf-8").hex()
+escrow_statistics_id = "escrow-statistics-id".encode("utf-8").hex()
+
+# Inserta los valores hexadecimales en la consulta GraphQL
+get_hmtoken_statistics_query = f"""
 query GetHMTokenStatistics {{
-    hmtokenStatistics(id: "hmt-statistics-id") {{
+    hmtokenStatistics(id: "{hmt_statistics_id}") {{
         ...HMTokenStatisticsFields
     }}
 }}
 {hmtoken_statistics_fragment}
-""".format(
-    hmtoken_statistics_fragment=hmtoken_statistics_fragment
-)
+"""
 
-get_escrow_statistics_query = """
+get_escrow_statistics_query = f"""
 query GetEscrowStatistics {{
-    escrowStatistics(id: "escrow-statistics-id") {{
+    escrowStatistics(id: "{escrow_statistics_id}") {{
         ...EscrowStatisticsFields
     }}
 }}
 {escrow_statistics_fragment}
-""".format(
-    escrow_statistics_fragment=escrow_statistics_fragment
-)
+"""
 
 
 def get_event_day_data_query(filter: StatisticsFilter) -> str:
