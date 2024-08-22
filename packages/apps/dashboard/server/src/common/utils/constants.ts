b18a51f762b364daf38053f4fe7a0b77acefed86
Francisco López <50665615+flopez7@users.noreply.github.com>
2024-08-22 11:13:35 +0200

    [Dashboard][Server] Fetch leaders and plain wallets balances (#2426)
    * Reduce possible chain ids in requests queries
    
    * Remove nullable chainId for details endpoint
    
    * Add getBalance method for details endpoint
    
    * Remove TODOs

--- packages/apps/dashboard/server/src/common/utils/constants.ts
+++ packages/apps/dashboard/server/src/common/utils/constants.ts
@@ -9,3 +9,17 @@ export enum MainnetsId {
   CELO = ChainId.CELO,
   XLAYER = ChainId.XLAYER,
 }
+
+export const LOCALHOST_CHAIN_IDS = [ChainId.LOCALHOST];
+
+export const TESTNET_CHAIN_IDS = [
+  ChainId.BSC_TESTNET,
+  ChainId.POLYGON_AMOY,
+  ChainId.SEPOLIA,
+  ChainId.XLAYER_TESTNET,
+];
+export const MAINNET_CHAIN_IDS = [
+  ChainId.BSC_MAINNET,
+  ChainId.POLYGON,
+  ChainId.MOONBEAM,
+];
