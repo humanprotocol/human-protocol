5dc42d737a16cbe8afdeaae720487c94c432f120
Arseny Boykov <36469655+Bobronium@users.noreply.github.com>
2024-08-22 13:26:24 +0200

    [Exchange Oracle] Fix removal of `LockNotAvailable` in `src.db.errors`

--- packages/examples/cvat/exchange-oracle/src/db/errors.py
+++ packages/examples/cvat/exchange-oracle/src/db/errors.py
@@ -3,5 +3,8 @@ from src.db import engine as db_engine
 if db_engine.driver != "psycopg2":
     raise NotImplementedError
 
+__all__ = ["LockNotAvailable"]
+
+from psycopg2.errors import LockNotAvailable
 
 # These errors can be found, e.g., in the .orig field of sqlalchemy errors
