dfccb37c17391c3741f1e39fcc2f5162698f319c
KacperKoza343 <165884432+KacperKoza343@users.noreply.github.com>
2024-09-11 11:00:51 +0200

    update(dashboard/ui-2024): remove unwanted chain id (#2506)

--- packages/apps/dashboard/ui-2024/src/utils/config/networks.ts
+++ packages/apps/dashboard/ui-2024/src/utils/config/networks.ts
@@ -1,7 +1,7 @@
 import type { Chain } from 'viem/chains';
 import * as chains from 'viem/chains';
 
-const chainIdsList = [1, 56, 97, 137, 1284, 43114, 42220, 196];
+const chainIdsList = [1, 56, 137, 1284, 43114, 42220, 196];
 
 const viemChains = Object.values(chains);
 
