128f2c066582144c308f54cc31aa15d2b08d6752
maya <maria@cvat.ai>
2024-09-11 12:55:04 +0200

    [Exchange oracle] Update integraton/services/test_exchange.py

--- packages/examples/cvat/exchange-oracle/tests/integration/services/test_exchange.py
+++ packages/examples/cvat/exchange-oracle/tests/integration/services/test_exchange.py
@@ -7,19 +7,15 @@ from unittest.mock import patch
 import pytest
 from fastapi import HTTPException
 from pydantic import ValidationError
-from pydantic_core import Url
 
-import src.services.cvat as cvat_service
-from src.core.types import AssignmentStatuses, JobStatuses, ProjectStatuses
+from src.core.types import AssignmentStatuses, JobStatuses, Networks
 from src.db import SessionLocal
+from src.endpoints.serializers import serialize_job
+
+# TODO: test for serialize_assignment, resign_assignment
 from src.models.cvat import Assignment, User
 from src.schemas import exchange as service_api
-from src.services.exchange import (
-    create_assignment,
-    get_available_jobs,
-    get_jobs_by_assignee,
-    serialize_job,
-)
+from src.services.exchange import create_assignment
 
 from tests.utils.db_helper import (
     create_job,
@@ -36,7 +32,7 @@ class ServiceIntegrationTest(unittest.TestCase):
     def tearDown(self):
         self.session.close()
 
-    def test_serialize_task(self):
+    def test_serialize_job(self):
         cvat_id = 1
         escrow_address = "0x86e83d346041E8806e352681f3F14549C0d2BC67"
 
@@ -45,79 +41,28 @@ class ServiceIntegrationTest(unittest.TestCase):
 
         with (
             open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
+            patch("src.endpoints.serializers.get_escrow_manifest") as mock_get_manifest,
         ):
             manifest = json.load(data)
             mock_get_manifest.return_value = manifest
-            data = serialize_job(cvat_project.id)
+            data = serialize_job(cvat_project)
 
-        assert data.id == cvat_project.id
         assert data.escrow_address == escrow_address
-        assert "Task " in data.title
-        assert len(data.title.split("Task ")[1]) <= 10
-        assert isinstance(data.description, str)
-        assert isinstance(data.job_bounty, str)
-        assert isinstance(data.job_time_limit, int)
-        assert isinstance(data.job_size, int)
+        assert isinstance(data.job_description, str)
+        assert data.job_description == manifest["annotation"]["description"]
+        assert isinstance(data.reward_amount, str)
+        assert data.reward_amount == manifest["job_bounty"]
+        assert isinstance(data.reward_token, str)
+        assert data.reward_token == service_api.DEFAULT_TOKEN
         assert data.job_type == cvat_project.job_type
-        assert data.platform == PlatformTypes.CVAT  # noqa: F821
-        assert data.status == cvat_project.status
-        assert data.assignment is None
-        assert isinstance(data, service_api.JobResponse)
-
-    def test_serialize_task_with_assignment(self):
-        cvat_id = 1
-        escrow_address = "0x86e83d346041E8806e352681f3F14549C0d2BC67"
-        user_address = "0x86e83d346041E8806e352681f3F14549C0d2BC68"
-
-        cvat_project, _, cvat_job = create_project_task_and_job(
-            self.session, escrow_address, cvat_id
-        )
-
-        user = User(
-            wallet_address=user_address,
-            cvat_email="test@hmt.ai",
-            cvat_id=1,
-        )
-        self.session.add(user)
-
-        assignment = Assignment(
-            id=str(uuid.uuid4()),
-            user_wallet_address=user_address,
-            cvat_job_id=cvat_job.cvat_id,
-            expires_at=datetime.now() + timedelta(days=1),
-        )
-        self.session.add(assignment)
-        self.session.commit()
-
-        with (
-            open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
-        ):
-            manifest = json.load(data)
-            mock_get_manifest.return_value = manifest
-            data = serialize_job(project_id=cvat_project.id, assignment_id=assignment.id)
-
-        assert data.id == cvat_project.id
-        assert data.escrow_address == escrow_address
-        assert "Task " in data.title
-        assert len(data.title.split("Task ")[1]) <= 10
-        assert isinstance(data.description, str)
-        assert isinstance(data.job_bounty, str)
-        assert isinstance(data.job_time_limit, int)
-        assert isinstance(data.job_size, int)
-        assert data.job_type == cvat_project.job_type
-        assert data.platform == PlatformTypes.CVAT  # noqa: F821
-        assert data.status == cvat_project.status
-        assert data.assignment is not None
-        assert isinstance(data.assignment.assignment_url, Url)
-        assert data.assignment.started_at == assignment.created_at
-        assert data.assignment.finishes_at == assignment.expires_at
+        assert data.status == service_api.JobStatuses.active
+        assert data.chain_id == cvat_project.chain_id
+        assert data.created_at == cvat_project.created_at
         assert isinstance(data, service_api.JobResponse)
 
     def test_serialize_task_invalid_project(self):
-        with pytest.raises(AttributeError):
-            serialize_job(project_id=str(uuid.uuid4()))
+        with pytest.raises(AssertionError):
+            serialize_job(project=str(uuid.uuid4()))
 
     def test_serialize_task_invalid_manifest(self):
         cvat_id = 1
@@ -126,85 +71,10 @@ class ServiceIntegrationTest(unittest.TestCase):
         cvat_project = create_project(self.session, escrow_address, cvat_id)
         self.session.commit()
 
-        with patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest:
+        with patch("src.endpoints.serializers.get_escrow_manifest") as mock_get_manifest:
             mock_get_manifest.return_value = None
             with pytest.raises(ValidationError):
-                serialize_job(project_id=cvat_project.id)
-
-    def test_get_available_tasks(self):
-        cvat_project_1, _, _ = create_project_task_and_job(
-            self.session, "0x86e83d346041E8806e352681f3F14549C0d2BC67", 1
-        )
-        cvat_project_2, _, _ = create_project_task_and_job(
-            self.session, "0x86e83d346041E8806e352681f3F14549C0d2BC68", 2
-        )
-        self.session.commit()
-
-        with (
-            open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
-        ):
-            manifest = json.load(data)
-            mock_get_manifest.return_value = manifest
-            tasks = get_available_jobs()
-
-            assert len(tasks) == 2
-            assert isinstance(tasks[0], service_api.JobResponse)
-            assert isinstance(tasks[1], service_api.JobResponse)
-            assert any(task.id == cvat_project_1.id for task in tasks)
-            assert any(task.id == cvat_project_2.id for task in tasks)
-
-            cvat_service.update_project_status(
-                self.session, cvat_project_2.id, ProjectStatuses.completed
-            )
-            self.session.commit()
-            tasks = get_available_jobs()
-
-            assert len(tasks) == 1
-            assert isinstance(tasks[0], service_api.JobResponse)
-            assert tasks[0].id == cvat_project_1.id
-
-    def test_get_tasks_by_assignee(self):
-        cvat_project_1, _, cvat_job_1 = create_project_task_and_job(
-            self.session, "0x86e83d346041E8806e352681f3F14549C0d2BC67", 1
-        )
-        cvat_project_2, _, cvat_job_2 = create_project_task_and_job(
-            self.session, "0x86e83d346041E8806e352681f3F14549C0d2BC68", 2
-        )
-
-        user_address = "0x86e83d346041E8806e352681f3F14549C0d2BC69"
-        user = User(
-            wallet_address=user_address,
-            cvat_email="test@hmt.ai",
-            cvat_id=1,
-        )
-        self.session.add(user)
-
-        assignment = Assignment(
-            id=str(uuid.uuid4()),
-            user_wallet_address=user_address,
-            cvat_job_id=cvat_job_1.cvat_id,
-            expires_at=datetime.now() + timedelta(days=1),
-        )
-        self.session.add(assignment)
-        self.session.commit()
-
-        with (
-            open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
-        ):
-            manifest = json.load(data)
-            mock_get_manifest.return_value = manifest
-            tasks = get_jobs_by_assignee(user_address)
-
-            assert len(tasks) == 1
-            assert isinstance(tasks[0], service_api.JobResponse)
-            assert tasks[0].id == cvat_project_1.id
-            assert tasks[0].assignment is not None
-
-    def test_get_tasks_by_assignee_invalid_address(self):
-        tasks = get_jobs_by_assignee("invalid_address")
-        assert len(tasks) == 0
+                serialize_job(cvat_project)
 
     def test_create_assignment(self):
         cvat_project_1, _, cvat_job_1 = create_project_task_and_job(
@@ -221,12 +91,14 @@ class ServiceIntegrationTest(unittest.TestCase):
 
         with (
             open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
+            patch("src.endpoints.serializers.get_escrow_manifest") as mock_get_manifest,
             patch("src.services.exchange.cvat_api"),
         ):
             manifest = json.load(data)
             mock_get_manifest.return_value = manifest
-            assignment_id = create_assignment(cvat_project_1.id, user_address)
+            assignment_id = create_assignment(
+                cvat_project_1.escrow_address, cvat_project_1.chain_id, user_address
+            )
 
             assignment = self.session.query(Assignment).filter_by(id=assignment_id).first()
 
@@ -267,12 +139,14 @@ class ServiceIntegrationTest(unittest.TestCase):
 
         with (
             open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
+            patch("src.endpoints.serializers.get_escrow_manifest") as mock_get_manifest,
             patch("src.services.exchange.cvat_api"),
         ):
             manifest = json.load(data)
             mock_get_manifest.return_value = manifest
-            assignment_id = create_assignment(cvat_project.id, user_address)
+            assignment_id = create_assignment(
+                cvat_project.escrow_address, cvat_project.chain_id, user_address
+            )
 
         assignment = self.session.query(Assignment).filter_by(id=assignment_id).first()
 
@@ -287,7 +161,11 @@ class ServiceIntegrationTest(unittest.TestCase):
         self.session.commit()
 
         with pytest.raises(HTTPException):
-            create_assignment(cvat_project_1.id, "invalid_address")
+            create_assignment(
+                cvat_project_1.escrow_address,
+                cvat_project_1.chain_id,
+                "invalid_address",
+            )
 
     def test_create_assignment_invalid_project(self):
         user_address = "0x86e83d346041E8806e352681f3F14549C0d2BC69"
@@ -300,7 +178,7 @@ class ServiceIntegrationTest(unittest.TestCase):
         self.session.commit()
 
         with pytest.raises(HTTPException):
-            create_assignment("1", user_address)
+            create_assignment("1", Networks.localhost.value, user_address)
 
     def test_create_assignment_unfinished_assignment(self):
         _, _, cvat_job = create_project_task_and_job(
@@ -325,14 +203,14 @@ class ServiceIntegrationTest(unittest.TestCase):
 
         with (
             open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
+            patch("src.endpoints.serializers.get_escrow_manifest") as mock_get_manifest,
             patch("src.services.exchange.cvat_api"),
         ):
             manifest = json.load(data)
             mock_get_manifest.return_value = manifest
 
             with pytest.raises(HTTPException):
-                create_assignment("1", user_address)
+                create_assignment("1", Networks.localhost.value, user_address)
 
     def test_create_assignment_no_available_jobs_completed_assignment(self):
         cvat_project, _, cvat_job_1 = create_project_task_and_job(
@@ -372,12 +250,14 @@ class ServiceIntegrationTest(unittest.TestCase):
 
         with (
             open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
+            patch("src.endpoints.serializers.get_escrow_manifest") as mock_get_manifest,
             patch("src.services.exchange.cvat_api"),
         ):
             manifest = json.load(data)
             mock_get_manifest.return_value = manifest
-            assignment_id = create_assignment(cvat_project.id, user_address2)
+            assignment_id = create_assignment(
+                cvat_project.escrow_address, cvat_project.chain_id, user_address2
+            )
 
         assert assignment_id == None
 
@@ -414,12 +294,14 @@ class ServiceIntegrationTest(unittest.TestCase):
 
         with (
             open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
+            patch("src.endpoints.serializers.get_escrow_manifest") as mock_get_manifest,
             patch("src.services.exchange.cvat_api"),
         ):
             manifest = json.load(data)
             mock_get_manifest.return_value = manifest
-            assignment_id = create_assignment(cvat_project.id, user_address2)
+            assignment_id = create_assignment(
+                cvat_project.escrow_address, cvat_project.chain_id, user_address2
+            )
 
         assert assignment_id == None
 
@@ -453,12 +335,14 @@ class ServiceIntegrationTest(unittest.TestCase):
 
         with (
             open("tests/utils/manifest.json") as data,
-            patch("src.services.exchange.get_escrow_manifest") as mock_get_manifest,
+            patch("src.endpoints.serializers.get_escrow_manifest") as mock_get_manifest,
             patch("src.services.exchange.cvat_api"),
         ):
             manifest = json.load(data)
             mock_get_manifest.return_value = manifest
-            assignment_id = create_assignment(cvat_project_1.id, user_address)
+            assignment_id = create_assignment(
+                cvat_project_1.escrow_address, cvat_project_1.chain_id, user_address
+            )
 
             assignment = self.session.query(Assignment).filter_by(id=assignment_id).first()
 
