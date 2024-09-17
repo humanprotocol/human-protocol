2e53b0aca6c97d9bf0ef8f527d0e0e4a87ca82f3
portuu3 <61605646+portuu3@users.noreply.github.com>
2024-09-17 14:27:31 +0200

    Delete strapi CMS for legacy dashboard (#2520)
    * Delete legacy dashboard admin/strapi
    
    * fix lint errors

--- packages/apps/dashboard/admin/config/api.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  rest: {
-    defaultLimit: 100,
-    maxLimit: 1000,
-    withCount: true,
-  },
-};
--- packages/apps/dashboard/admin/src/api/news-item/routes/news-item.ts
+++
@@ -1,7 +0,0 @@
-/**
- * news-item router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::news-item.news-item');
