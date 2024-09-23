47e38a7dd0abcd813c8608f0562229591ebea88d
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-09-23 18:09:43 +0300

    Fix formatting

--- packages/examples/cvat/exchange-oracle/src/endpoints/serializers.py
+++ packages/examples/cvat/exchange-oracle/src/endpoints/serializers.py
@@ -66,7 +66,7 @@ def serialize_job(
             reward_amount=str(manifest.job_bounty) if manifest else None,
             reward_token=(
                 service_api.DEFAULT_TOKEN
-            ), # TODO: this field requires a value to be kept by response_model_exclude_unset=True
+            ),  # TODO: this field requires a value to be kept by response_model_exclude_unset=True
             created_at=project.created_at,
             updated_at=updated_at,
             qualifications=manifest.qualifications,
@@ -144,7 +144,7 @@ def serialize_assignment(
             reward_amount=str(manifest.job_bounty) if manifest else None,
             reward_token=(
                 service_api.DEFAULT_TOKEN
-            ), # TODO: this field requires a value to be kept by response_model_exclude_unset=True
+            ),  # TODO: this field requires a value to be kept by response_model_exclude_unset=True
             url=compose_assignment_url(
                 task_id=assignment.job.cvat_task_id,
                 job_id=assignment.cvat_job_id,
