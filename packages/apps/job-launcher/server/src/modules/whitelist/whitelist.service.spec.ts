cb7153a71884a5acd117fb7b4a0a7e51ab36494a
Francisco López <50665615+flopez7@users.noreply.github.com>
2024-10-08 10:11:44 +0200

    [Job Launcher] Crypto payments for whitelisted users (#2594)
    * Crypto payments for whitelisted users
    
    * UserId not null in whitelist table

---
+++ packages/apps/job-launcher/server/src/modules/whitelist/whitelist.service.spec.ts
@@ -0,0 +1,51 @@
+import { Test } from '@nestjs/testing';
+import { WhitelistService } from './whitelist.service';
+import { WhitelistRepository } from './whitelist.repository';
+import { createMock } from '@golevelup/ts-jest';
+
+describe('WhitelistService', () => {
+  let whitelistService: WhitelistService;
+  let whitelistRepository: WhitelistRepository;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        WhitelistService,
+        {
+          provide: WhitelistRepository,
+          useValue: createMock<WhitelistRepository>(),
+        },
+      ],
+    }).compile();
+
+    whitelistService = moduleRef.get<WhitelistService>(WhitelistService);
+    whitelistRepository =
+      moduleRef.get<WhitelistRepository>(WhitelistRepository);
+  });
+
+  describe('isUserWhitelisted', () => {
+    it('should return true if user is whitelisted', async () => {
+      const userId = 1;
+      jest
+        .spyOn(whitelistRepository, 'findOneByUserId')
+        .mockResolvedValue({ id: 1, user: { id: userId } as any });
+
+      const result = await whitelistService.isUserWhitelisted(userId);
+
+      expect(whitelistRepository.findOneByUserId).toHaveBeenCalledWith(userId);
+      expect(result).toBe(true);
+    });
+
+    it('should return false if user is not whitelisted', async () => {
+      const userId = 2;
+      jest
+        .spyOn(whitelistRepository, 'findOneByUserId')
+        .mockResolvedValue(null);
+
+      const result = await whitelistService.isUserWhitelisted(userId);
+
+      expect(whitelistRepository.findOneByUserId).toHaveBeenCalledWith(userId);
+      expect(result).toBe(false);
+    });
+  });
+});
