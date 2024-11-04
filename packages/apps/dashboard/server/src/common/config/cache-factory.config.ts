9dd7bf25b604c0fb87b0d9fc290d5f196189b74b
Dmitry Nechay <lelekkofe@gmail.com>
2024-11-04 17:59:55 +0300

    refactor: use new redis storage

--- packages/apps/dashboard/server/src/common/config/cache-factory.config.ts
+++ packages/apps/dashboard/server/src/common/config/cache-factory.config.ts
@@ -1,7 +1,15 @@
 import { CacheModuleAsyncOptions } from '@nestjs/common/cache';
 import { ConfigModule } from '@nestjs/config';
+import * as _ from 'lodash';
 import { RedisConfigService } from './redis-config.service';
-import { redisStore } from 'cache-manager-redis-store';
+import { redisStore } from 'cache-manager-redis-yet';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('CacheFactoryRedisStore');
+
+const throttledRedisErrorLog = _.throttle((error) => {
+  logger.error('Redis client network error', error);
+}, 1000 * 5);
 
 export const CacheFactoryConfig: CacheModuleAsyncOptions = {
   isGlobal: true,
@@ -12,7 +20,11 @@ export const CacheFactoryConfig: CacheModuleAsyncOptions = {
         host: configService.redisHost,
         port: configService.redisPort,
       },
+      disableOfflineQueue: true,
     });
+
+    store.client.on('error', throttledRedisErrorLog);
+
     return {
       store: () => store,
     };
