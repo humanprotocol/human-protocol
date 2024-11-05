f020926742dac90800c928a14fe0f52698de55ad
mpblocky <185767042+mpblocky@users.noreply.github.com>
2024-11-05 11:52:50 +0100

    fix: remove zod url check from url schema

--- packages/apps/human-app/frontend/src/shared/helpers/url-domain-validation.ts
+++ packages/apps/human-app/frontend/src/shared/helpers/url-domain-validation.ts
@@ -1,17 +1,14 @@
 import { t } from 'i18next';
 import { z } from 'zod';
 
-export const urlDomainSchema = z
-  .string()
-  .url()
-  .refine(
-    (url) => {
-      try {
-        const newUrl = new URL(url);
-        return newUrl.protocol === 'http:' || newUrl.protocol === 'https:';
-      } catch (e) {
-        return false;
-      }
-    },
-    { message: t('validation.urlValidationError') }
-  );
+export const urlDomainSchema = z.string().refine(
+  (url) => {
+    try {
+      const newUrl = new URL(url);
+      return newUrl.protocol === 'http:' || newUrl.protocol === 'https:';
+    } catch (e) {
+      return false;
+    }
+  },
+  { message: t('validation.urlValidationError') }
+);
