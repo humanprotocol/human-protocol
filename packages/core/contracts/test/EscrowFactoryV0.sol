c2a31d5a9ad197622e445a459b585b58c3ae074d
Francisco López <francislopez977@gmail.com>
2024-11-11 16:30:49 +0100

    Fix failing tests

--- packages/core/contracts/test/EscrowFactoryV0.sol
+++ packages/core/contracts/test/EscrowFactoryV0.sol
@@ -114,5 +114,5 @@ contract EscrowFactoryV0 is OwnableUpgradeable, UUPSUpgradeable {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[44] private __gap;
+    uint256[45] private __gap;
 }
