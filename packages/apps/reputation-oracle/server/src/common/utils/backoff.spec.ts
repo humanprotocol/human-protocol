d92c428f8b7b5a703a59a37294de6de5f10d92df
eugenvoronov <eugene@hmt.ai>
2024-11-19 14:03:43 +0300

    Used constant in backoff unit tests

--- packages/apps/reputation-oracle/server/src/common/utils/backoff.spec.ts
+++ packages/apps/reputation-oracle/server/src/common/utils/backoff.spec.ts
@@ -1,27 +1,28 @@
+import { MOCK_BACKOFF_INTERVAL_SECONDS } from '../../../test/constants';
 import { calculateExponentialBackoffMs } from './backoff';
 
 describe('calculateExponentialBackoffMs', () => {
   it('should return 2 minutes backoff for the first retry (retriesCount = 0)', () => {
     const retriesCount = 0;
     const result = calculateExponentialBackoffMs(retriesCount);
-    expect(result).toBe(120 * 1000); // 2 minutes in milliseconds
+    expect(result).toBe(MOCK_BACKOFF_INTERVAL_SECONDS * 1000); // 2 minutes in milliseconds
   });
 
   it('should return 4 minutes backoff for the second retry (retriesCount = 1)', () => {
     const retriesCount = 1;
     const result = calculateExponentialBackoffMs(retriesCount);
-    expect(result).toBe(2 * 120 * 1000); // 4 minutes in milliseconds
+    expect(result).toBe(2 * MOCK_BACKOFF_INTERVAL_SECONDS * 1000); // 4 minutes in milliseconds
   });
 
   it('should return 8 minutes backoff for the third retry (retriesCount = 2)', () => {
     const retriesCount = 2;
     const result = calculateExponentialBackoffMs(retriesCount);
-    expect(result).toBe(4 * 120 * 1000); // 8 minutes in milliseconds
+    expect(result).toBe(4 * MOCK_BACKOFF_INTERVAL_SECONDS * 1000); // 8 minutes in milliseconds
   });
 
   it('should return 64 minutes backoff for the sixth retry (retriesCount = 5)', () => {
     const retriesCount = 5;
     const result = calculateExponentialBackoffMs(retriesCount);
-    expect(result).toBe(32 * 120 * 1000); // 64 minutes in milliseconds
+    expect(result).toBe(32 * MOCK_BACKOFF_INTERVAL_SECONDS * 1000); // 64 minutes in milliseconds
   });
 });
