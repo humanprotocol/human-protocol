4033f92bf60f71fb5df229ab3becc193289e0210
adrian-oleskiewicz <adrian.oleskiewicz@blockydevs.com>
2024-12-03 11:34:12 +0100

    HDM-31 rollback hook additional state

--- packages/apps/dashboard/ui-2024/src/utils/hooks/use-is-mobile.tsx
+++ packages/apps/dashboard/ui-2024/src/utils/hooks/use-is-mobile.tsx
@@ -1,14 +1,11 @@
 import useMediaQuery from '@mui/material/useMediaQuery';
 
-type Breakpoints = 'mobile' | 'mid';
-
 const breakpoints = {
   mobile: `(max-width: 1100px)`,
-  mid: `(max-width: 1100px)`,
 };
 
-export function useBreakPoints(breakpoint: Breakpoints = 'mobile') {
-  const matchesMobile = useMediaQuery(breakpoints[breakpoint]);
+export function useBreakPoints() {
+  const matchesMobile = useMediaQuery(breakpoints.mobile);
   return {
     mobile: {
       isMobile: matchesMobile,
