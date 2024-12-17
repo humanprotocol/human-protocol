26fcc2a70df3a604392538869b74d1904333daf6
eugenvoronov <eugene@hmt.ai>
2024-12-17 09:18:39 +0300

    Added types

---
+++ packages/apps/dashboard/server/src/common/types/leader.ts
@@ -0,0 +1,9 @@
+import { OrderDirection } from '@human-protocol/sdk';
+import { LeadersOrderBy } from '../enums/leader';
+
+export type GetLeadersPaginationOptions = {
+  orderBy?: LeadersOrderBy;
+  orderDirection?: OrderDirection;
+  first?: number;
+  skip?: number;
+};
