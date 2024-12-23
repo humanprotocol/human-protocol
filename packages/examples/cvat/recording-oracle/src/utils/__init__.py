c7f51e28bfb25a0d625b985e7f495f4f386175aa
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-12-23 16:36:46 +0200

    Refactor grouped()

--- packages/examples/cvat/recording-oracle/src/utils/__init__.py
+++ packages/examples/cvat/recording-oracle/src/utils/__init__.py
@@ -1,5 +1,4 @@
 from collections.abc import Callable, Iterable, Iterator, Mapping, Sequence
-from itertools import groupby
 from typing import TypeVar
 
 _K = TypeVar("_K")
@@ -7,16 +6,16 @@ _V = TypeVar("_V")
 
 
 def grouped(
-    data: Iterator[_V] | Iterable[_V], *, key: Callable[[_V], _K]
+    items: Iterator[_V] | Iterable[_V], *, key: Callable[[_V], _K]
 ) -> Mapping[_K, Sequence[_V]]:
     """
     Returns a mapping with input iterable elements grouped by key, for example:
 
-    [("apple1", "red"), ("apple2", "green"), ("apple3", "red")]
-    key=lambda v: v[1]
-
+    grouped(
+        [("apple1", "red"), ("apple2", "green"), ("apple3", "red")],
+        key=lambda v: v[1]
+    )
     ->
-
     {
         "red": [("apple1", "red"), ("apple3", "red")],
         "green": [("apple2", "green")]
@@ -25,7 +24,9 @@ def grouped(
     Similar to itertools.groupby, but allows reiteration on resulting groups.
     """
 
-    return {
-        group_key: list(group_iter)
-        for group_key, group_iter in groupby(sorted(data, key=key), key=key)
-    }
+    # Can be implemented with itertools.groupby, but it requires extra sorting for input elements
+    grouped_items = {}
+    for item in items:
+        grouped_items.setdefault(key(item), []).append(item)
+
+    return grouped_items
