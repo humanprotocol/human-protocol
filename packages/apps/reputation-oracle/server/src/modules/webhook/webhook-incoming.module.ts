b1ae4fed64d35cdf156645faaae2f9300ec06948
Dmitry Nechay <lelekkofe@gmail.com>
2025-01-03 14:43:02 +0300

    fix: repositories imports

--- packages/apps/reputation-oracle/server/src/modules/webhook/webhook-incoming.module.ts
+++ packages/apps/reputation-oracle/server/src/modules/webhook/webhook-incoming.module.ts
@@ -1,23 +1,15 @@
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { HttpModule } from '@nestjs/axios';
 import { Web3Module } from '../web3/web3.module';
 import { WebhookController } from './webhook.controller';
-import { WebhookIncomingEntity } from './webhook-incoming.entity';
 import { WebhookIncomingRepository } from './webhook-incoming.repository';
 import { EscrowCompletionModule } from '../escrow-completion/escrow-completion.module';
 import { WebhookIncomingService } from './webhook-incoming.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([WebhookIncomingEntity]),
-    ConfigModule,
-    Web3Module,
-    HttpModule,
-    EscrowCompletionModule,
-  ],
+  imports: [ConfigModule, Web3Module, HttpModule, EscrowCompletionModule],
   controllers: [WebhookController],
   providers: [Logger, WebhookIncomingService, WebhookIncomingRepository],
   exports: [WebhookIncomingService],
