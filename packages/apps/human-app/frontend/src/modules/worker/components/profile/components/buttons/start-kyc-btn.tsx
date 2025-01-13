d7addb4a2523dce5d99a22203636b3d9501e10c4
adrian-oleskiewicz <adrian.oleskiewicz@blockydevs.com>
2025-01-13 15:48:37 +0100

    HUM-91 - refactor profile module

---
+++ packages/apps/human-app/frontend/src/modules/worker/components/profile/components/buttons/start-kyc-btn.tsx
@@ -0,0 +1,29 @@
+import { t } from 'i18next';
+import { Button } from '@/shared/components/ui/button';
+import { useAuthenticatedUser } from '@/modules/auth/hooks/use-authenticated-user';
+import { useStartKyc } from '@/modules/worker/components/profile/hooks/use-start-kyc';
+
+export function StartKycButton() {
+  const { user } = useAuthenticatedUser();
+  const { isKYCInProgress, kycStartIsPending, startKYC } = useStartKyc();
+
+  if (isKYCInProgress) {
+    return (
+      <Button disabled fullWidth variant="contained">
+        {t('worker.profile.KYCInProgress')}
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      disabled={user.status !== 'active'}
+      fullWidth
+      loading={kycStartIsPending}
+      onClick={startKYC}
+      variant="contained"
+    >
+      {t('worker.profile.completeKYC')}
+    </Button>
+  );
+}
