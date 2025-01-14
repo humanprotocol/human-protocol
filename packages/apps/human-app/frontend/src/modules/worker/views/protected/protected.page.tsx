9329639e7ce5d9c4c8d2d56e7df9209ecd5ca1a0
adrian-oleskiewicz <adrian.oleskiewicz@blockydevs.com>
2025-01-14 11:40:24 +0100

    HUM-95 - trnslation handling added

--- packages/apps/human-app/frontend/src/modules/worker/views/protected/protected.page.tsx
+++ packages/apps/human-app/frontend/src/modules/worker/views/protected/protected.page.tsx
@@ -1,9 +1,12 @@
 import Box from '@mui/material/Box';
+import { useTranslation } from 'react-i18next';
 
 export function ProtectedPage() {
+  const { t } = useTranslation();
+
   return (
     <Box>
-      <div>ProtectedPage</div>
+      <div>{t('protectedPage.header')}</div>
     </Box>
   );
 }
