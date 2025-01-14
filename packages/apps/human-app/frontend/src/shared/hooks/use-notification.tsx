a9fdece9aaafe8c53f2446890b65d8ad32865f52
adrian-oleskiewicz <adrian.oleskiewicz@blockydevs.com>
2025-01-14 11:29:16 +0100

    HUM-145 - cleanup after dev tests

--- packages/apps/human-app/frontend/src/shared/hooks/use-notification.tsx
+++ packages/apps/human-app/frontend/src/shared/hooks/use-notification.tsx
@@ -7,7 +7,7 @@ import { colorPalette as lightColorPalette } from '@/shared/styles/color-palette
 import { breakpoints } from '@/shared/styles/breakpoints';
 import { useIsMobile } from '@/shared/hooks/use-is-mobile';
 
-const AUTO_HIDE_NOTIFICATION = 66000;
+const AUTO_HIDE_NOTIFICATION = 6000;
 const FONT_COLOR = lightColorPalette.white;
 
 export const useNotification = () => {
