c93903231c861178c75b5867a2db0b106266bef7
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2025-01-23 14:19:52 +0200

    Fix misleading dataset mean quality computation in RO results

--- packages/examples/cvat/recording-oracle/src/handlers/process_intermediate_results.py
+++ packages/examples/cvat/recording-oracle/src/handlers/process_intermediate_results.py
@@ -812,7 +812,7 @@ def process_intermediate_results(  # noqa: PLR0912
 
         db_service.update_gt_stats(session, task.id, gt_stats)
 
-    job_final_result_ids: dict[int, str] = {}
+    job_final_result_ids: dict[str, str] = {}
 
     for job_meta in meta.jobs:
         job = db_service.get_job_by_cvat_id(session, job_meta.job_id)
@@ -903,6 +903,14 @@ def process_intermediate_results(  # noqa: PLR0912
             for r in task_validation_results
         ],
     )
+
+    # Include final results for all jobs
+    job_results: _JobResults = {
+        job.cvat_id: task_validation_results[
+            validation_result_id_to_meta_id[job_final_result_ids[job.id]]
+        ].annotation_quality
+        for job in task_jobs
+    }
 
     return ValidationSuccess(
         job_results=job_results,
