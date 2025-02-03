ab3616ef10dc7ffb773fac2d1a27ba025d382828
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-02-03 10:35:29 +0100

    fix: remove unused exports, add memo to address validation

--- packages/apps/human-app/frontend/src/modules/worker/hooks/use-register-address/index.ts
+++ packages/apps/human-app/frontend/src/modules/worker/hooks/use-register-address/index.ts
@@ -1,3 +1,1 @@
 export { useRegisterAddress } from './use-register-address';
-export { useRegisterAddressMutation } from './use-register-address-mutation';
-export { useRegisterAddressValidation } from './use-register-address-validation';
--- packages/apps/human-app/frontend/src/modules/worker/hooks/use-register-address/use-register-address-validation.ts
+++ packages/apps/human-app/frontend/src/modules/worker/hooks/use-register-address/use-register-address-validation.ts
@@ -1,11 +1,15 @@
+import { useMemo } from 'react';
 import { useWalletConnect } from '@/shared/hooks/use-wallet-connect';
 
 export function useRegisterAddressValidation() {
   const { address } = useWalletConnect();
 
-  const errors = {
-    noAddress: !address,
-  };
+  const errors = useMemo(
+    () => ({
+      noAddress: !address,
+    }),
+    [address]
+  );
 
   return {
     isValid: !Object.values(errors).some(Boolean),
