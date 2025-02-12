e8986b484551331a0d7c60a227dcb473f6bbcd73
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-02-12 09:48:40 +0100

    chore: separate oracle registration flow hook from instructions hook

--- packages/apps/human-app/frontend/src/modules/worker/oracle-registration/hooks/use-oracle-instructions.tsx
+++ packages/apps/human-app/frontend/src/modules/worker/oracle-registration/hooks/use-oracle-instructions.tsx
@@ -1,12 +1,17 @@
 import { useState, useCallback } from 'react';
+import { useGetOracles } from '../../services/oracles';
 
-export function useOracleInstructions() {
+export function useOracleInstructions(oracleAddress?: string) {
   const [hasViewedInstructions, setHasViewedInstructions] = useState(false);
+  const { data: oraclesData } = useGetOracles();
+  const oracleData = oraclesData?.find(
+    (oracle) => oracle.address === oracleAddress
+  );
 
-  const handleInstructionsView = useCallback((instructionsUrl: string) => {
-    window.open(instructionsUrl, '_blank');
+  const handleInstructionsView = useCallback(() => {
+    window.open(oracleData?.registrationInstructions ?? '', '_blank');
     setHasViewedInstructions(true);
-  }, []);
+  }, [oracleData]);
 
   return { hasViewedInstructions, handleInstructionsView };
 }
