ce29d1131da4f2eeff53b5dfb041464c8a1014e7
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-02-12 09:48:40 +0100

    chore: separate oracle registration flow hook from instructions hook

--- packages/apps/human-app/frontend/src/modules/worker/oracle-registration/hooks/use-oracle-registration-flow.tsx
+++ packages/apps/human-app/frontend/src/modules/worker/oracle-registration/hooks/use-oracle-registration-flow.tsx
@@ -1,29 +1,19 @@
 import { useParams } from 'react-router-dom';
-import { useCallback } from 'react';
-import { useGetOracles } from '@/modules/worker/services/oracles';
 import { useOracleInstructions } from './use-oracle-instructions';
 import { useOracleRegistration } from './use-oracle-registration';
 
 export function useOracleRegistrationFlow() {
-  const { address: oracleAddress } = useParams<{ address: string }>();
-  const { data: oraclesData } = useGetOracles();
-  const oracleData = oraclesData?.find(
-    (oracle) => oracle.address === oracleAddress
-  );
+  const { address } = useParams<{ address: string }>();
 
   const { handleRegistration, isRegistrationPending, registrationError } =
-    useOracleRegistration(oracleAddress);
+    useOracleRegistration(address);
 
   const { hasViewedInstructions, handleInstructionsView } =
-    useOracleInstructions();
-
-  const handleInstructionsViewWithData = useCallback(() => {
-    handleInstructionsView(oracleData?.registrationInstructions ?? '');
-  }, [oracleData, handleInstructionsView]);
+    useOracleInstructions(address);
 
   return {
     hasViewedInstructions,
-    handleInstructionsView: handleInstructionsViewWithData,
+    handleInstructionsView,
     handleRegistration,
     isRegistrationPending,
     registrationError,
