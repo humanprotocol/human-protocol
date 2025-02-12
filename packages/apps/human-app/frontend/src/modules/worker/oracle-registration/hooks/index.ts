2c04f442d0eed3076c16b3c7623734695af5570a
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-02-12 09:47:05 +0100

    fix: simplify naming, move use registration from hook to form

--- packages/apps/human-app/frontend/src/modules/worker/oracle-registration/hooks/index.ts
+++ packages/apps/human-app/frontend/src/modules/worker/oracle-registration/hooks/index.ts
@@ -1,3 +1,2 @@
 export * from './use-oracle-registration-flow';
-export * from './use-registration-form';
 export * from './use-is-already-registered';
--- packages/apps/human-app/frontend/src/modules/worker/oracle-registration/registration-form.tsx
+++ packages/apps/human-app/frontend/src/modules/worker/oracle-registration/registration-form.tsx
@@ -1,28 +1,48 @@
 import { Box, Stack } from '@mui/material';
-import { FormProvider } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useParams } from 'react-router-dom';
 import { Button } from '@/shared/components/ui/button';
 import { HCaptchaForm } from '@/shared/components/hcaptcha/h-captcha-form';
-import { useOracleRegistrationFlow, useRegistrationForm } from './hooks';
+import {
+  registrationInExchangeOracleDtoSchema,
+  type RegistrationInExchangeOracleDto,
+} from './sevices';
+import { useOracleRegistrationFlow } from './hooks';
+
+function useRegistrationForm() {
+  const { address: oracleAddress } = useParams<{ address: string }>();
+
+  return useForm<RegistrationInExchangeOracleDto>({
+    defaultValues: {
+      // eslint-disable-next-line camelcase
+      oracle_address: oracleAddress,
+      // eslint-disable-next-line camelcase
+      h_captcha_token: '',
+    },
+    resolver: zodResolver(registrationInExchangeOracleDtoSchema),
+  });
+}
 
 export function RegistrationForm() {
   const { t } = useTranslation();
   const methods = useRegistrationForm();
   const {
     hasViewedInstructions,
-    handleInstructionsView: onInstructionsClick,
-    handleRegistration: onSubmit,
+    handleInstructionsView,
+    handleRegistration,
     isRegistrationPending: isLoading,
     registrationError: error,
   } = useOracleRegistrationFlow();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    void methods.handleSubmit(onSubmit)(event);
+    void methods.handleSubmit(handleRegistration)(event);
   };
 
   return (
     <>
-      <Button onClick={onInstructionsClick} fullWidth variant="contained">
+      <Button onClick={handleInstructionsView} fullWidth variant="contained">
         {t('worker.registrationInExchangeOracle.instructionsButton')}
       </Button>
       <Box>{t('worker.registrationInExchangeOracle.completeMessage')}</Box>
