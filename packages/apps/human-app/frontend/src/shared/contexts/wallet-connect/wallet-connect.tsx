90c5a39b1b5810b9022ffdb39ca39806ca041448
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-02-21 10:56:19 +0100

    fix: relax is ready condition

--- packages/apps/human-app/frontend/src/shared/contexts/wallet-connect/wallet-connect.tsx
+++ packages/apps/human-app/frontend/src/shared/contexts/wallet-connect/wallet-connect.tsx
@@ -91,8 +91,7 @@ export function WalletConnectProvider({
     [web3ProviderMutation.data]
   );
 
-  const isReady =
-    isConnected && address && chainId && web3ProviderMutation.data;
+  const isReady = isConnected && address && chainId;
 
   const contextValue = useMemo(() => {
     return isReady
