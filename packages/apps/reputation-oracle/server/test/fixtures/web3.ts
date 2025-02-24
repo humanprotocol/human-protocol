6c4c04d58c4b5b5c8df25e6f020c5cde41f92b5f
Dmitry Nechay <lelekkofe@gmail.com>
2025-02-24 18:43:06 +0300

    refactor: web3 tests to use fixtures

--- packages/apps/reputation-oracle/server/test/fixtures/web3.ts
+++ packages/apps/reputation-oracle/server/test/fixtures/web3.ts
@@ -2,6 +2,10 @@ import { faker } from '@faker-js/faker';
 import { ChainId } from '@human-protocol/sdk';
 import { ethers } from 'ethers';
 
+export const TEST_PRIVATE_KEY =
+  '0x85dc77260240f78982bdfdfc0a0cb241a85d2f9833468fae7ec362ec7829ce3a';
+export const TEST_ADDRESS = '0x9dfB81606Af98a4776a28Ae0Ae30DA3567ae4B98';
+
 export function generateEthWallet() {
   const wallet = ethers.Wallet.createRandom();
 
