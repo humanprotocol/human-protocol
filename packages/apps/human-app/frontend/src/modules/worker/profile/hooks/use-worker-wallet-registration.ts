26a9b01bb011b4022d6296da0d4ce90fcd79ce75
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-02-26 16:10:33 +0100

    fix: remove unnecessary variable in use worker wallet registration

--- packages/apps/human-app/frontend/src/modules/worker/profile/hooks/use-worker-wallet-registration.ts
+++ packages/apps/human-app/frontend/src/modules/worker/profile/hooks/use-worker-wallet-registration.ts
@@ -1,11 +1,10 @@
-import { useRef, useEffect, useCallback } from 'react';
+import { useCallback } from 'react';
 import { useWalletConnect } from '@/shared/contexts/wallet-connect';
 import { useRegisterAddress } from '@/modules/worker/hooks/use-register-address';
 import { useRegisterAddressNotifications } from '../../hooks/use-register-address-notifications';
 
 export function useWorkerWalletRegistration() {
-  const modalWasOpened = useRef(false);
-  const { isConnected, address, openModal } = useWalletConnect();
+  const { isConnected, openModal } = useWalletConnect();
   const { onSuccess, onError } = useRegisterAddressNotifications();
   const {
     mutate: registerAddressMutation,
@@ -15,21 +14,14 @@ export function useWorkerWalletRegistration() {
     onSuccess,
   });
 
-  useEffect(() => {
-    if (isConnected && modalWasOpened.current) {
-      registerAddressMutation();
-    }
-  }, [address, isConnected, registerAddressMutation]);
-
   const handleConnectWallet = useCallback(() => {
-    modalWasOpened.current = true;
     void openModal();
-  }, [openModal]);
+    registerAddressMutation();
+  }, [openModal, registerAddressMutation]);
 
   return {
     isConnected,
     isRegisterAddressPending,
     handleConnectWallet,
-    modalWasOpened,
   };
 }
