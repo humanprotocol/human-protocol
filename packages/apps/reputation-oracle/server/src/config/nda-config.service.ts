9ea06d1897e3cb469156660a20c322f13720ab45
Dzeranov <sergey.dzeranov@gmail.com>
2025-03-14 11:31:50 +0300

    fix: review comment

--- packages/apps/reputation-oracle/server/src/config/nda-config.service.ts
+++ packages/apps/reputation-oracle/server/src/config/nda-config.service.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NDAConfigService {
-  constructor(private configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {}
 
   /**
    * Latest NDA Url.
