d1743d959cccdfb321ba1e75b979b11d40fbc936
Dmitry Nechay <lelekkofe@gmail.com>
2025-03-14 14:14:38 +0300

    test: createWorkerUser

--- packages/apps/reputation-oracle/server/src/modules/user/user.service.spec.ts
+++ packages/apps/reputation-oracle/server/src/modules/user/user.service.spec.ts
@@ -1,13 +1,18 @@
+import { faker } from '@faker-js/faker';
 import { createMock } from '@golevelup/ts-jest';
 import { Test } from '@nestjs/testing';
+
+import { Web3ConfigService } from '../../config/web3-config.service';
+import { HCaptchaService } from '../../integrations/hcaptcha/hcaptcha.service';
+import * as securityUtils from '../../utils/security';
+
+import { mockWeb3ConfigService } from '../web3/fixtures';
+import { Web3Service } from '../web3/web3.service';
 
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
 import { SiteKeyRepository } from './site-key.repository';
-import { Web3Service } from '../web3/web3.service';
-import { mockWeb3ConfigService } from '../web3/fixtures';
-import { HCaptchaService } from '../../integrations/hcaptcha/hcaptcha.service';
-import { Web3ConfigService } from '../../config/web3-config.service';
+import { Role, UserStatus } from './user.entity';
 
 const mockUserRepository = createMock<UserRepository>();
 const mockSiteKeyRepository = createMock<SiteKeyRepository>();
@@ -51,7 +56,44 @@ describe('UserService', () => {
     userService = moduleRef.get<UserService>(UserService);
   });
 
-  it('created', () => {
-    expect(userService).toBeDefined();
+  describe('isWeb2UserRole', () => {
+    it.each(
+      Object.values(Role).map((role) => ({
+        role,
+        result: role === Role.OPERATOR ? false : true,
+      })),
+    )('should return "$result" for "$role" role', ({ role, result }) => {
+      expect(UserService.isWeb2UserRole(role)).toBe(result);
+    });
+  });
+
+  describe('createWorkerUser', () => {
+    it('should create worker user and return the created entity', async () => {
+      const createUserData = {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      };
+
+      const expectedUserData = {
+        email: createUserData.email,
+        role: Role.WORKER,
+        status: UserStatus.PENDING,
+        password: expect.not.stringMatching(createUserData.password),
+      };
+
+      const result = await userService.createWorkerUser(createUserData);
+
+      expect(mockUserRepository.createUnique).toHaveBeenCalledWith(
+        expectedUserData,
+      );
+      expect(result).toEqual(expectedUserData);
+
+      expect(
+        securityUtils.comparePasswordWithHash(
+          createUserData.password,
+          result.password,
+        ),
+      ).toBe(true);
+    });
   });
 });
