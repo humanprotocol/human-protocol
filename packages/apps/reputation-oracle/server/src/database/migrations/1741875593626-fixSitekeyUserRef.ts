0c5fbc35588f886465f18124292be8afdf57e979
Dmitry Nechay <lelekkofe@gmail.com>
2025-03-18 12:13:20 +0300

    refactor: simplify sitekey migration

--- packages/apps/reputation-oracle/server/src/database/migrations/1741875593626-fixSitekeyUserRef.ts
+++ packages/apps/reputation-oracle/server/src/database/migrations/1741875593626-fixSitekeyUserRef.ts
@@ -5,37 +5,13 @@ export class FixSitekeyUserRef1741875593626 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "hmt"."site_keys" DROP CONSTRAINT "FK_266dc68bd3412cb17b5d927b30c"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "hmt"."site_keys" DROP CONSTRAINT "UQ_7100e356f506f79d28eb2ba3e46"`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "hmt"."site_keys" ALTER COLUMN "user_id" SET NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "hmt"."site_keys" ADD CONSTRAINT "UQ_7100e356f506f79d28eb2ba3e46" UNIQUE ("site_key", "type", "user_id")`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "hmt"."site_keys" ADD CONSTRAINT "FK_266dc68bd3412cb17b5d927b30c" FOREIGN KEY ("user_id") REFERENCES "hmt"."users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "hmt"."site_keys" DROP CONSTRAINT "FK_266dc68bd3412cb17b5d927b30c"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "hmt"."site_keys" DROP CONSTRAINT "UQ_7100e356f506f79d28eb2ba3e46"`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "hmt"."site_keys" ALTER COLUMN "user_id" DROP NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "hmt"."site_keys" ADD CONSTRAINT "UQ_7100e356f506f79d28eb2ba3e46" UNIQUE ("site_key", "type", "user_id")`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "hmt"."site_keys" ADD CONSTRAINT "FK_266dc68bd3412cb17b5d927b30c" FOREIGN KEY ("user_id") REFERENCES "hmt"."users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
   }
 }
