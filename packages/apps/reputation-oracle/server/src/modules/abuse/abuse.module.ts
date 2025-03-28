35917cca82a2a37999033ed0aba3d763e9496728
Francisco López <francislopez977@gmail.com>
2025-03-28 17:50:08 +0100

    fix: add missing AbuseController to the AbuseModule

--- packages/apps/reputation-oracle/server/src/modules/abuse/abuse.module.ts
+++ packages/apps/reputation-oracle/server/src/modules/abuse/abuse.module.ts
@@ -6,10 +6,12 @@ import { AbuseService } from './abuse.service';
 import { Web3Module } from '../web3/web3.module';
 import { ReputationModule } from '../reputation/reputation.module';
 import { WebhookOutgoingModule } from '../webhook/webhook-outgoing.module';
+import { AbuseController } from './abuse.controller';
 
 @Module({
   imports: [HttpModule, Web3Module, ReputationModule, WebhookOutgoingModule],
   providers: [AbuseRepository, AbuseService, AbuseSlackBot],
   exports: [AbuseService],
+  controllers: [AbuseController],
 })
 export class AbuseModule {}
