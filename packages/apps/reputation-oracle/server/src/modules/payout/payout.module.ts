75e0923c5392c98acf98b5aba9e385af6c2d4c1e
Dmitry Nechay <lelekkofe@gmail.com>
2025-04-03 19:53:36 +0300

    refactor: clean storage service from solutions business logic

--- packages/apps/reputation-oracle/server/src/modules/payout/payout.module.ts
+++ packages/apps/reputation-oracle/server/src/modules/payout/payout.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 
+import { EncryptionModuleModule } from '../encryption/encryption.module';
 import { ReputationModule } from '../reputation/reputation.module';
 import { Web3Module } from '../web3/web3.module';
 import { StorageModule } from '../storage/storage.module';
@@ -7,7 +8,12 @@ import { StorageModule } from '../storage/storage.module';
 import { PayoutService } from './payout.service';
 
 @Module({
-  imports: [ReputationModule, Web3Module, StorageModule],
+  imports: [
+    EncryptionModuleModule,
+    ReputationModule,
+    Web3Module,
+    StorageModule,
+  ],
   providers: [PayoutService],
   exports: [PayoutService],
 })
