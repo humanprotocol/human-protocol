99ab64ebf7ae6eb5bf9f861ae512eda89b441dcc
portuu3 <61605646+portuu3@users.noreply.github.com>
2025-04-04 10:21:57 +0200

    add partial status as valid status for completion (#3263)

--- packages/apps/job-launcher/server/src/modules/job/job.service.ts
+++ packages/apps/job-launcher/server/src/modules/job/job.service.ts
@@ -1808,8 +1808,14 @@ export class JobService {
     if (jobEntity.status === JobStatus.COMPLETED) {
       return;
     }
-    if (jobEntity.status !== JobStatus.LAUNCHED) {
-      throw new ControlledError(ErrorJob.NotLaunched, HttpStatus.CONFLICT);
+    if (
+      jobEntity.status !== JobStatus.LAUNCHED &&
+      jobEntity.status !== JobStatus.PARTIAL
+    ) {
+      throw new ControlledError(
+        ErrorJob.InvalidStatusCompletion,
+        HttpStatus.CONFLICT,
+      );
     }
 
     jobEntity.status = JobStatus.COMPLETED;
