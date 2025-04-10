c1ba769a917fd102a461af2f30a38ec5b93add53
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-04-10 16:28:52 +0200

    chore: migrate user profile hooks to use new http client

--- packages/apps/human-app/frontend/src/modules/worker/hooks/use-register-address.ts
+++ packages/apps/human-app/frontend/src/modules/worker/hooks/use-register-address.ts
@@ -1,8 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { t } from 'i18next';
 import { z } from 'zod';
-import { apiClient } from '@/api/api-client';
-import { apiPaths } from '@/api/api-paths';
 import { useAuthenticatedUser } from '@/modules/auth/hooks/use-authenticated-user';
 import { useAccessTokenRefresh } from '@/api/hooks/use-access-token-refresh';
 import {
@@ -11,38 +9,20 @@ import {
 } from '@/api/hooks/use-prepare-signature';
 import type { ResponseError } from '@/shared/types/global.type';
 import { useWalletConnect } from '@/shared/contexts/wallet-connect';
+import { profileService } from '../profile/services/profile.service';
 
 interface RegisterAddressCallbacks {
   onSuccess?: () => void | Promise<void>;
   onError?: (error: ResponseError) => void | Promise<void>;
 }
 
-interface RegisterAddressParams {
-  address: string;
-  signature: string;
-}
-
-const RegisterAddressSuccessSchema = z.unknown();
-
-const registerAddressRequest = async (
-  params: RegisterAddressParams
-): Promise<unknown> => {
-  return apiClient(apiPaths.worker.registerAddress.path, {
-    authenticated: true,
-    withAuthRetry: apiPaths.worker.registerAddress.withAuthRetry,
-    successSchema: RegisterAddressSuccessSchema,
-    options: {
-      method: 'POST',
-      body: JSON.stringify(params),
-    },
-  });
-};
+export const RegisterAddressSuccessSchema = z.unknown();
 
 function useRegisterAddressMutation(callbacks?: RegisterAddressCallbacks) {
   const queryClient = useQueryClient();
   const { user } = useAuthenticatedUser();
   const { refreshAccessTokenAsync } = useAccessTokenRefresh();
-  const { address, signMessage } = useWalletConnect();
+  const { address, chainId, signMessage } = useWalletConnect();
 
   const mutationFn = async () => {
     if (!address) {
@@ -63,7 +43,7 @@ function useRegisterAddressMutation(callbacks?: RegisterAddressCallbacks) {
 
     // wallet address is part of the JWT payload
     // so we need to refresh the token after the address is registered
-    await registerAddressRequest({ address, signature });
+    await profileService.registerAddress({ address, chainId, signature });
     await refreshAccessTokenAsync({ authType: 'web2' });
   };
 
