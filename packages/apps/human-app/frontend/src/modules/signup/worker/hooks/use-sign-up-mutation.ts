0badce2fa2f84c8be80b2b11c8b20b67553202f0
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-04-10 14:47:38 +0200

    chore: migrate sign up to use new http client

--- packages/apps/human-app/frontend/src/modules/signup/worker/hooks/use-sign-up-mutation.ts
+++ packages/apps/human-app/frontend/src/modules/signup/worker/hooks/use-sign-up-mutation.ts
@@ -1,26 +1,30 @@
 import { z } from 'zod';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
-import { apiClient } from '@/api/api-client';
-import { apiPaths } from '@/api/api-paths';
 import { routerPaths } from '@/router/router-paths';
+import { signupService } from '@/modules/signup/services/signup.service';
 import { type SignUpDto } from '../schema';
 
-const signUpSuccessResponseSchema = z.unknown();
-
-async function signUpMutationFn(data: Omit<SignUpDto, 'confirmPassword'>) {
-  await apiClient(apiPaths.worker.signUp.path, {
-    successSchema: signUpSuccessResponseSchema,
-    options: { method: 'POST', body: JSON.stringify(data) },
-  });
-}
+export const SignUpSuccessResponseSchema = z.unknown();
 
 export function useSignUpMutation() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   return useMutation({
-    mutationFn: signUpMutationFn,
+    mutationFn: async (data: SignUpDto) => {
+      try {
+        return await signupService.workerSignUp({
+          email: data.email,
+          password: data.password,
+          // eslint-disable-next-line camelcase
+          h_captcha_token: data.h_captcha_token,
+          confirmPassword: data.confirmPassword,
+        });
+      } catch (error) {
+        throw new Error('Failed to sign up');
+      }
+    },
     onSuccess: async (_, { email }) => {
       navigate(routerPaths.worker.verifyEmail, {
         state: { routerState: { email } },
