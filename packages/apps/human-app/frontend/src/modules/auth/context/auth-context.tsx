cd8b18113363e749cff5ebbea886aa6cba88d36a
mpblocky <185767042+mpblocky@users.noreply.github.com>
2025-04-19 18:30:19 +0200

    chore: cleanup schema in auth context

--- packages/apps/human-app/frontend/src/modules/auth/context/auth-context.tsx
+++ packages/apps/human-app/frontend/src/modules/auth/context/auth-context.tsx
@@ -3,22 +3,16 @@
 import { z } from 'zod';
 import { createAuthProvider } from '@/shared/contexts/generic-auth-context';
 
-const extendableUserDataSchema = z.object({
+const userDataSchema = z.object({
   site_key: z.string().optional().nullable(),
   kyc_status: z.string().optional().nullable(),
   wallet_address: z.string().optional().nullable(),
   status: z.enum(['active', 'pending']),
+  email: z.string(),
+  user_id: z.number(),
+  reputation_network: z.string(),
+  exp: z.number(),
 });
-
-const userDataSchema = z
-  .object({
-    email: z.string(),
-    user_id: z.number(),
-    reputation_network: z.string(),
-    email_notifications: z.boolean().optional(),
-    exp: z.number(),
-  })
-  .merge(extendableUserDataSchema);
 
 export type UserData = z.infer<typeof userDataSchema>;
 
