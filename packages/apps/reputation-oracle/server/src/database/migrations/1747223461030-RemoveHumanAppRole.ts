b8b81926cd9c9825fdd0e64c7dd8f9e96c64491e
Dmitry Nechay <lelekkofe@gmail.com>
2025-05-14 14:57:58 +0300

    feat: add migration to remove human_app role

---
+++ packages/apps/reputation-oracle/server/src/database/migrations/1747223461030-RemoveHumanAppRole.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class RemoveHumanAppRole1747223461030 implements MigrationInterface {
+  name = 'RemoveHumanAppRole1747223461030';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TYPE "hmt"."users_role_enum" RENAME TO "users_role_enum_old"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "hmt"."users_role_enum" AS ENUM('operator', 'worker', 'admin')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "hmt"."users" ALTER COLUMN "role" TYPE "hmt"."users_role_enum" USING "role"::"text"::"hmt"."users_role_enum"`,
+    );
+    await queryRunner.query(`DROP TYPE "hmt"."users_role_enum_old"`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "hmt"."users_role_enum_old" AS ENUM('operator', 'worker', 'human_app', 'admin')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "hmt"."users" ALTER COLUMN "role" TYPE "hmt"."users_role_enum_old" USING "role"::"text"::"hmt"."users_role_enum_old"`,
+    );
+    await queryRunner.query(`DROP TYPE "hmt"."users_role_enum"`);
+    await queryRunner.query(
+      `ALTER TYPE "hmt"."users_role_enum_old" RENAME TO "users_role_enum"`,
+    );
+  }
+}
