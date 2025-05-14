63f64eaafdac3b412d0a7914dd02ca05846ed984
Dmitry Nechay <lelekkofe@gmail.com>
2025-05-14 15:24:00 +0300

    chore: improve decorators comments

--- packages/apps/reputation-oracle/server/src/common/decorators/index.ts
+++ packages/apps/reputation-oracle/server/src/common/decorators/index.ts
@@ -3,13 +3,16 @@ import { UserRole } from '../../modules/user';
 
 /**
  * Decorator for HTTP endpoints to bypass JWT auth guard
- * checks for endpoints that do not need JWT auth.
+ * where JWT auth not needed
  */
 export const Public = Reflector.createDecorator<boolean>({
   key: 'isPublic',
   transform: () => true,
 });
 
+/**
+ * Decorator to specify the list of roles accepted by RolesAuthGuard
+ */
 export const Roles = Reflector.createDecorator<UserRole[]>({
   key: 'roles',
 });
