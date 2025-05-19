a961d6a19257326148c8192862bc0bcbe201c968
Dmitry Nechay <lelekkofe@gmail.com>
2025-05-19 13:24:55 +0300

    refactor: rename cvat docker db

--- scripts/cvat/initdb/create-dbs.sql
+++ scripts/cvat/initdb/create-dbs.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE "reputation-oracle";
-CREATE DATABASE "exchange-oracle";
-CREATE DATABASE "recording-oracle";
+CREATE DATABASE "exchange-oracle-cvat";
+CREATE DATABASE "recording-oracle-cvat";
 CREATE DATABASE "job-launcher";
