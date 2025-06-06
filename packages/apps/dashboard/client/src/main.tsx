b44e69d72888b89574a2de081f03b05c898cf6ef
kirill <kirill@hmt.ai>
2025-06-06 11:13:22 +0300

    feat: placeholder for dark mode and related features

--- packages/apps/dashboard/client/src/main.tsx
+++ packages/apps/dashboard/client/src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
-import theme from './theme';
+import ThemeProvider from '@/providers/ThemeProvider';
 
+import App from './App';
 import '@/assets/styles/main.scss';
 import 'simplebar-react/dist/simplebar.min.css';
 
@@ -19,8 +17,7 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
+  <ThemeProvider>
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
