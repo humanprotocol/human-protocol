bf23d121d5d81407daeb36b6eb99bfbaf4658889
kirill <kirill@hmt.ai>
2025-06-27 16:40:12 +0300

    remove the code, that triggers type error

--- packages/apps/dashboard/client/src/shared/lib/isValidEvmAddress.ts
+++ packages/apps/dashboard/client/src/shared/lib/isValidEvmAddress.ts
@@ -1,5 +1,4 @@
-const isValidEVMAddress = (input: string, mask: string): boolean => {
-  mask.split('').join('');
+const isValidEVMAddress = (input: string): boolean => {
   const evmRegex = /^0x[a-fA-F0-9]{40}$/;
   return evmRegex.test(input);
 };
