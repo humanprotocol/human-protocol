bbf95b6ac81134b49cef9d6121eb0834509a2095
Dmitry Nechay <lelekkofe@gmail.com>
2025-07-07 16:08:06 +0300

    feat: logger package

--- packages/libs/logger/src/index.ts
+++ packages/libs/logger/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello logging');
+import { createPinoLogger as createLogger } from './pino-logger';
+
+export { default as NestLogger } from './nest-logger';
+
+export { LogLevel } from './types';
+export type { Logger, LoggerFactory, LoggerFactoryOptions } from './types';
+
+export { createLogger };
+export default createLogger({ name: 'human-protocol-logger', pretty: true });
