ef2e5090178b04541d9fe4c803966449d0df2550
Dmitry Nechay <lelekkofe@gmail.com>
2025-08-25 15:36:13 +0300

    [HUMAN App] fix: refetch hCaptcha stats once user solved captcha (#3515)

--- packages/apps/human-app/frontend/src/modules/worker/hcaptcha-labeling/hooks/use-solve-hcaptcha-mutation.ts
+++ packages/apps/human-app/frontend/src/modules/worker/hcaptcha-labeling/hooks/use-solve-hcaptcha-mutation.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { ResponseError } from '@/shared/types/global.type';
 import * as hCaptchaLabelingService from '../services/hcaptcha-labeling.service';
 import { type VerifyHCaptchaLabelingBody } from '../types';
@@ -7,14 +7,22 @@ export function useSolveHCaptchaMutation(callbacks: {
   onSuccess: () => void | Promise<void>;
   onError: (error: ResponseError) => void | Promise<void>;
 }) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async (data: VerifyHCaptchaLabelingBody) =>
       hCaptchaLabelingService.verifyHCaptchaLabeling(data),
-    onSuccess: async () => {
-      await callbacks.onSuccess();
+    onSuccess: () => {
+      void callbacks.onSuccess();
+      void queryClient.invalidateQueries({
+        queryKey: ['getHCaptchaUsersStats'],
+      });
+      void queryClient.invalidateQueries({
+        queryKey: ['dailyHmtSpent'],
+      });
     },
-    onError: async (error) => {
-      await callbacks.onError(error);
+    onError: (error) => {
+      void callbacks.onError(error);
     },
   });
 }
