dd865ccd6bb238a6f6d72cbbf51a9e1feab7e903
Dmitry Nechay <lelekkofe@gmail.com>
2025-09-03 12:54:16 +0300

    chore: app-level eslint and prettier

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from '@jest/types';
-
-export default async (): Promise<Config.InitialOptions> => {
-  return {
-    preset: 'ts-jest',
-    projects: ['<rootDir>/packages/**/jest.config.ts'],
-  };
-};
