4a244d8396d1b7bbfa2c1c5c97613a9b5a6ced55
KirillKirill <kirill@hmt.ai>
2025-09-10 14:16:56 +0300

    [Human App] Remove fields that are not used and might cause an error (#3556)

--- packages/apps/human-app/frontend/src/modules/worker/hcaptcha-labeling/schemas.ts
+++ packages/apps/human-app/frontend/src/modules/worker/hcaptcha-labeling/schemas.ts
@@ -11,19 +11,12 @@ export const dailyHmtSpentResponseSchema = z.object({
 
 export const hcaptchaUserStatsResponseSchema = z.object({
   balance: z.object({
-    available: z.number(),
-    estimated: z.number(),
     recent: z.number(),
     total: z.number(),
   }),
   served: z.number(),
   solved: z.number(),
-  verified: z.number(),
   currentDateStats: z.object({
-    billing_units: z.number(),
-    bypass: z.number(),
-    served: z.number(),
     solved: z.number(),
   }),
-  currentEarningsStats: z.number(),
 });
