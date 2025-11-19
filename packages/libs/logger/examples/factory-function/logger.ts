4900240518a387a0f1edb117d05597c5430fb776
Dmitry Nechay <lelekkofe@gmail.com>
2025-11-19 20:05:52 +0300

    chore: bump pino; update examples

--- packages/libs/logger/examples/factory-function/logger.ts
+++ packages/libs/logger/examples/factory-function/logger.ts
@@ -1,19 +1,29 @@
-import { createLogger, LogLevel } from '../../src';
+import { createLogger, isLogLevel, LogLevel } from '../../src';
 
 const ENV_NAME = process.env.NODE_ENV || 'development';
 const isDevelopment = ENV_NAME === 'development';
 const isTest = process.env.NODE_ENV === 'test';
 
+const LOG_LEVEL_OVERRIDE = process.env.LOG_LEVEL;
+
+let logLevel = LogLevel.INFO;
+if (isLogLevel(LOG_LEVEL_OVERRIDE)) {
+  logLevel = LOG_LEVEL_OVERRIDE;
+} else if (isDevelopment) {
+  logLevel = LogLevel.DEBUG;
+}
+
 const defaultExampleLogger = createLogger(
   {
     name: 'DefaultLogger',
-    level: isDevelopment ? LogLevel.DEBUG : LogLevel.INFO,
-    pretty: isDevelopment,
+    level: logLevel,
+    pretty: isDevelopment || process.env.LOG_PRETTY === 'true',
     disabled: isTest,
   },
   {
     environment: ENV_NAME,
     service: 'logger-examples',
+    version: 'ff-ex-v1',
   },
 );
 
