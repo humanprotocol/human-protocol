d0b9343d6641d7b1a2eb4b76d363f4601353591a
flopez7 <francisco@hmt.ai>
2026-03-19 18:16:43 +0100

    remove GCV example package and related files

--- packages/examples/gcv/src/dto/storage.ts
+++
@@ -1,8 +0,0 @@
-import { AWSRegions, StorageProviders } from '../enums/storage';
-
-export class StorageDataDto {
-  public provider: StorageProviders;
-  public region: AWSRegions | null;
-  public bucketName: string;
-  public path: string;
-}
