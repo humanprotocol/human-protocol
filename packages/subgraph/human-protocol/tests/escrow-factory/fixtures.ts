6122ba6232ae22b7d5767203d3081804166526b6
Francisco López <50665615+flopez7@users.noreply.github.com>
2026-03-23 09:56:33 +0100

    [Subgraph] Update launcher references in Escrow functions  (#3837)
    Co-authored-by: flopez7 <francisco@hmt.ai>

--- packages/subgraph/human-protocol/tests/escrow-factory/fixtures.ts
+++ packages/subgraph/human-protocol/tests/escrow-factory/fixtures.ts
@@ -1,7 +1,10 @@
 import { newMockEvent } from 'matchstick-as/assembly/index';
 import { ethereum, BigInt, Address, dataSource } from '@graphprotocol/graph-ts';
 
-import { Launched } from '../../generated/EscrowFactory/EscrowFactory';
+import {
+  Launched,
+  LaunchedV2,
+} from '../../generated/EscrowFactory/EscrowFactory';
 import { generateUniqueHash } from '../../tests/utils';
 
 export function createLaunchedEvent(
@@ -38,6 +41,50 @@ export function createLaunchedEvent(
 
   newLaunchedEvent.parameters.push(tokenParam);
   newLaunchedEvent.parameters.push(escrowParam);
+
+  return newLaunchedEvent;
+}
+
+export function createLaunchedV2Event(
+  factory: Address,
+  launcher: Address,
+  token: Address,
+  escrow: Address,
+  jobRequesterId: string,
+  timestamp: BigInt
+): LaunchedV2 {
+  const newLaunchedEvent = changetype<LaunchedV2>(newMockEvent());
+  newLaunchedEvent.transaction.hash = generateUniqueHash(
+    escrow.toString(),
+    timestamp,
+    newLaunchedEvent.transaction.nonce
+  );
+
+  newLaunchedEvent.block.timestamp = timestamp;
+  newLaunchedEvent.transaction.from = launcher;
+  newLaunchedEvent.transaction.to = Address.fromString(
+    dataSource.address().toHexString()
+  );
+  newLaunchedEvent.address = factory;
+
+  newLaunchedEvent.parameters = [];
+
+  const tokenParam = new ethereum.EventParam(
+    'token',
+    ethereum.Value.fromAddress(token)
+  );
+  const escrowParam = new ethereum.EventParam(
+    'escrow',
+    ethereum.Value.fromAddress(escrow)
+  );
+  const jobRequesterIdParam = new ethereum.EventParam(
+    'jobRequesterId',
+    ethereum.Value.fromString(jobRequesterId)
+  );
+
+  newLaunchedEvent.parameters.push(tokenParam);
+  newLaunchedEvent.parameters.push(escrowParam);
+  newLaunchedEvent.parameters.push(jobRequesterIdParam);
 
   return newLaunchedEvent;
 }
