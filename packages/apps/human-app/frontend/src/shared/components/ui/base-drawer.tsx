ef88919f2581735b74616799820352e76066213c
KirillKirill <kirill@hmt.ai>
2026-07-31 13:53:53 +0300

    chore: add base modal and base drawer components for reusing them

---
+++ packages/apps/human-app/frontend/src/shared/components/ui/base-drawer.tsx
@@ -0,0 +1,66 @@
+import { useCallback, type PropsWithChildren } from 'react';
+import { Drawer, IconButton, type SxProps, type Theme } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  isLoading?: boolean;
+  sx?: SxProps<Theme>;
+  closeButtonSx?: SxProps<Theme>;
+};
+
+export function BaseDrawer({
+  open,
+  onClose,
+  isLoading = false,
+  sx,
+  closeButtonSx,
+  children,
+}: PropsWithChildren<Props>) {
+  const handleClose = useCallback(() => {
+    if (isLoading) return;
+    onClose();
+  }, [isLoading, onClose]);
+
+  return (
+    <Drawer
+      open={open}
+      onClose={handleClose}
+      anchor="bottom"
+      slotProps={{
+        backdrop: {
+          sx: {
+            backdropFilter: 'blur(7px)',
+            background: 'rgba(0, 0, 0, 0.3)',
+          },
+        },
+        paper: {
+          elevation: 0,
+          sx: {
+            py: 2,
+            borderTopLeftRadius: '16px',
+            borderTopRightRadius: '16px',
+            border: 'none',
+            height: '75dvh',
+            ...sx,
+          },
+        },
+      }}
+    >
+      <IconButton
+        onClick={handleClose}
+        sx={{
+          position: 'absolute',
+          top: 16,
+          right: 16,
+          p: 0,
+          ...closeButtonSx,
+        }}
+      >
+        <CloseIcon sx={{ color: 'neutral.100' }} />
+      </IconButton>
+      {children}
+    </Drawer>
+  );
+}
---
+++ packages/apps/human-app/frontend/src/shared/components/ui/modal/base-modal.tsx
@@ -0,0 +1,89 @@
+import { type PropsWithChildren, useCallback } from 'react';
+
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  IconButton,
+  Modal,
+  Paper,
+  type SxProps,
+  type Theme,
+} from '@mui/material';
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  isLoading?: boolean;
+  sx?: SxProps<Theme>;
+  closeButtonSx?: SxProps<Theme>;
+};
+
+export function BaseModal({
+  open,
+  onClose,
+  isLoading = false,
+  sx,
+  closeButtonSx,
+  children,
+}: PropsWithChildren<Props>) {
+  const handleClose = useCallback(() => {
+    if (isLoading) return;
+    onClose();
+  }, [isLoading, onClose]);
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        mx: 0,
+      }}
+      slotProps={{
+        backdrop: {
+          sx: {
+            backdropFilter: 'blur(7px)',
+            background: 'rgba(0, 0, 0, 0.3)',
+          },
+        },
+      }}
+    >
+      <Paper
+        elevation={0}
+        sx={{
+          py: 6,
+          px: 4,
+          width: 640,
+          maxHeight: '700px',
+          overflowY: 'hidden',
+          borderRadius: '20px',
+          position: 'relative',
+          boxShadow: 'none',
+          ...sx,
+        }}
+      >
+        <IconButton
+          disabled={isLoading}
+          onClick={handleClose}
+          sx={{
+            p: 0,
+            color: 'neutral.100',
+            position: 'absolute',
+            top: 48,
+            right: 32,
+            '&:hover': {
+              bgcolor: 'unset',
+            },
+            ...closeButtonSx,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        {children}
+      </Paper>
+    </Modal>
+  );
+}
+
+export default BaseModal;
