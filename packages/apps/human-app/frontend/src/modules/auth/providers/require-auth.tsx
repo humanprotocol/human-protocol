ec5d4c5154479d64fbfb9ce414b4ea3ed5b16563
KirillKirill <kirill@hmt.ai>
2026-08-07 15:02:54 +0300

    fix: improve RequireAuth code

--- packages/apps/human-app/frontend/src/modules/auth/providers/require-auth.tsx
+++ packages/apps/human-app/frontend/src/modules/auth/providers/require-auth.tsx
@@ -1,13 +1,12 @@
-import type { ReactNode } from 'react';
+import { createContext, type ReactNode } from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
-import { createContext } from 'react';
+
 import { useAuth } from '@/modules/auth/hooks/use-auth';
 import { routerPaths } from '@/router/router-paths';
 import { PageCardLoader } from '@/shared/components/ui/page-card';
 import { type AuthenticatedUserContextType } from '@/shared/contexts/generic-auth-context';
 import { type UserData } from '../context/auth-context';
 import { useIsUserVerified } from '@/shared/hooks';
-import { KycStatus } from '@/modules/worker/profile/types/profile-types';
 
 export const AuthenticatedUserContext =
   createContext<AuthenticatedUserContextType<UserData> | null>(null);
@@ -17,16 +16,11 @@ export function RequireAuth({ children }: { children: ReactNode }) {
   const location = useLocation();
   const isUserVerified = useIsUserVerified();
 
-  const isKycApproved = auth.user?.kyc_status === KycStatus.APPROVED;
-  const isWalletConnected = auth.user?.wallet_address !== null;
-
-  const isUserReady = isKycApproved && isWalletConnected;
-
   if (auth.status === 'loading') {
     return <PageCardLoader />;
   }
 
-  if (!auth.user || !isUserReady) {
+  if (!auth.user) {
     return (
       <Navigate replace state={{ from: location }} to={routerPaths.homePage} />
     );
